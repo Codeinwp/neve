0e39c145b89da76db7143a70d5303f0c96cafc4d
abaicus <baicusandrei@me.com>
2018-09-06 13:19:32 +0300

    Lowercase file names.

--- Autoloader.php
+++ Autoloader.php
@@ -124,8 +124,10 @@ class Autoloader {
 			// replace namespace separators with directory separators
 			// in the relative class name, append with .php
 			$file = $base_dir
-					. str_replace( '\\', '/', $relative_class )
-					. '.php';
+			        . str_replace( '\\', '/', $relative_class )
+			        . '.php';
+
+			$file = strtolower( $file );
 
 			// if the mapped file exists, require it
 			if ( $this->require_file( $file ) ) {
