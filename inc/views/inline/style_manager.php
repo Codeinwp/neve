cc7e20401e3e8c2fb995492704385f4fac87c2dc
Andrei Baicus <baicusandrei@me.com>
2018-09-25 11:11:42 +0300

    Delete customizer generated CSS on theme update so it can be regenerated.

--- inc/views/inline/style_manager.php
+++ inc/views/inline/style_manager.php
@@ -104,6 +104,7 @@ class Style_Manager extends Base_View {
 
 		add_action( 'wp_enqueue_scripts', array( $this, 'maybe_enqueue' ) );
 		add_action( 'customize_save_after', array( $this, 'wipe_customizer_css_file' ), 0 );
+		add_action( 'after_switch_theme', array( $this, 'wipe_customizer_css_file' ), 0 );
 
 		if ( ! is_admin() && ! is_customize_preview() ) {
 			add_action( 'shutdown', array( $this, 'generate_customizer_css_file' ), PHP_INT_MAX );
