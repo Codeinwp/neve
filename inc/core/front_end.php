c283b6453a00084b546c4e9226793a4e23fd07a3
Andrei Baicus <baicusandrei@me.com>
2018-10-23 12:21:32 +0300

    fix: add amp pair mode to theme support

--- inc/core/front_end.php
+++ inc/core/front_end.php
@@ -51,7 +51,7 @@ class Front_End {
 		add_theme_support( 'align-wide' );
 		add_theme_support( 'fl-theme-builder-headers' );
 		add_theme_support( 'fl-theme-builder-footers' );
-		add_theme_support( 'amp' );
+		add_theme_support( 'amp', array( 'paired' => true ) );
 
 		register_nav_menus(
 			array(
