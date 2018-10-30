f036ba259280a8e1ceab6707cf32cdbd24afc462
coti <stefan@themeisle.com>
2018-10-30 12:37:31 +0200

    chore: Welcome notice updated
    Strings within the welcome notice can be translated.
    
    #119

--- inc/core/admin.php
+++ inc/core/admin.php
@@ -173,7 +173,23 @@ class Admin {
 		$name       = $theme_args->__get( 'Name' );
 		$slug       = $theme_args->__get( 'stylesheet' );
 
-		$notice = apply_filters( 'ti_about_welcome_notice_filter', ( '<p>' . sprintf( 'Welcome! Thank you for choosing %1$s! To fully take advantage of the best our theme can offer please make sure you visit our %2$swelcome page%3$s.', $name, '<a href="' . esc_url( admin_url( 'themes.php?page=' . $slug . '-welcome' ) ) . '">', '</a>' ) . '</p><p><a href="' . esc_url( admin_url( 'themes.php?page=' . $slug . '-welcome&onboarding=yes#sites_library' ) ) . '" class="button" style="text-decoration: none;">' . sprintf( 'Get started with %s', $name ) . '</a></p>' ) );
+		$about_page_url          = '<a href="' . esc_url( admin_url( 'themes.php?page=' . $slug . '-welcome' ) ) . '">';
+		$about_page_url         .= esc_html__( 'welcome page.', 'neve' );
+		$about_page_url         .= '</a>';
+		$getting_started_button  = '<a href="' . esc_url( admin_url( 'themes.php?page=' . $slug . '-welcome&onboarding=yes#sites_library' ) ) . '" class="button" style="text-decoration: none;">';
+		$getting_started_button .= esc_html__( 'Get started with ', 'neve' ) . $name;
+		$getting_started_button .= '</a>';
+
+		$notice  = '';
+		$notice .= '<p>';
+		$notice .= esc_html__( 'Welcome! Thank you for choosing ', 'neve' );
+		$notice .= $name;
+		$notice .= esc_html__( '! To fully take advantage of the best our theme can offer please make sure you visit our ', 'neve' );
+		$notice .= $about_page_url;
+		$notice .= '</p>';
+		$notice .= '<p>';
+		$notice .= $getting_started_button;
+		$notice .= '</p>';
 
 		echo wp_kses_post( $notice );
 	}
