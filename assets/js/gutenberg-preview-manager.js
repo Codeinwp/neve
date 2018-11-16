40cc90a87438e9a2e1a5e9dec8380f05d0b69b89
Andrei Baicus <baicusandrei@me.com>
2018-11-16 11:23:34 +0200

    feat: gutenberg integration [wip]

--- assets/js/gutenberg-preview-manager.js
+++ assets/js/gutenberg-preview-manager.js
@@ -2,13 +2,49 @@
 
 (function ( $ ) {
 	$.neveGutenberg = {
+		data: neveGutenbergHelper,
+
 		init: function () {
-			console.log( neveGutenbergHelper );
+			this.addClasses();
+			this.handleSidebar();
+			this.handleTitleToggle();
+			console.log( this.data );
+		},
+
+		addClasses: function () {
+			var editor = $( '.editor-styles-wrapper' );
+			var wrapMarkup = '<div class="neve-gtb-flow-wrap"></div>';
+			$( editor ).addClass( 'neve-gtb' );
+			$( editor ).find( '.editor-writing-flow' ).wrap( wrapMarkup );
+		},
+
+		handleSidebar: function () {
+			var wrap = $( '.neve-gtb-flow-wrap' );
+			$( wrap ).prepend( this.getSidebarMarkup( 'left' ) );
+			$( wrap ).append( this.getSidebarMarkup( 'right' ) );
+		},
+
+		getSidebarMarkup: function ( position ) {
+			var sidebarClass = '';
+			if ( this.data.neve_meta_sidebar !== position ) {
+				sidebarClass = ' sidebar-hidden ';
+			}
+			return '<div class="neve-gtb-sidebar ' + sidebarClass + position + '"><h2>'+ this.data.strings.sidebar +'</h2></div>';
+		},
+
+		handleTitleToggle: function () {
+			if ( this.data.neve_meta_disable_title === 'on' ) {
+				$( '.neve-gtb' ).find( '.editor-post-title' ).hide();
+			}
+
+			$( '#neve-page-settings #neve_meta_disable_title' ).on( 'change', function () {
+				$( '.neve-gtb' ).find( '.editor-post-title' ).toggle();
+			} );
 		},
 
 	};
 })( jQuery );
 
-jQuery( document ).ready( function () {
+jQuery( window ).load( function () {
 	jQuery.neveGutenberg.init();
 } );
