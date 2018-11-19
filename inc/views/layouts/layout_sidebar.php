4289b1fc97e910fb26db5714d81a35460f6de3db
cristian-ungureanu <cristian@themeisle.com>
2018-11-19 12:20:38 +0200

    refactor: improve php code

--- inc/views/layouts/layout_sidebar.php
+++ inc/views/layouts/layout_sidebar.php
@@ -73,36 +73,36 @@ class Layout_Sidebar extends Base_View {
 
 		if ( $advanced_options === false ) {
 			$sidebar_setup['theme_mod'] = 'neve_default_sidebar_layout';
-		} else {
-			switch ( $context ) {
-				case 'blog-archive':
-					$sidebar_setup['theme_mod'] = 'neve_blog_archive_sidebar_layout';
-					break;
-				case 'single-post':
-					$sidebar_setup['theme_mod'] = 'neve_single_post_sidebar_layout';
-					if ( class_exists( 'WooCommerce' ) && is_product() ) {
-						$sidebar_setup['theme_mod'] = 'neve_single_product_sidebar_layout';
-					}
-					break;
-				case 'single-page':
-					$sidebar_setup['theme_mod'] = 'neve_other_pages_sidebar_layout';
-					break;
-				case 'shop':
-					if ( class_exists( 'WooCommerce' ) ) {
-						$sidebar_setup['sidebar_slug'] = 'shop-sidebar';
-						if ( is_woocommerce() ) {
-							$sidebar_setup['theme_mod'] = 'neve_shop_archive_sidebar_layout';
-						}
-						if ( is_product() ) {
-							$sidebar_setup['theme_mod'] = 'neve_single_product_sidebar_layout';
-						}
-					}
-					break;
-				default:
-					$sidebar_setup['theme_mod'] = 'neve_other_pages_sidebar_layout';
-			}
+			return $sidebar_setup;
 		}
 
+		switch ( $context ) {
+			case 'blog-archive':
+				$sidebar_setup['theme_mod'] = 'neve_blog_archive_sidebar_layout';
+				break;
+			case 'single-post':
+				$sidebar_setup['theme_mod'] = 'neve_single_post_sidebar_layout';
+				if ( class_exists( 'WooCommerce' ) && is_product() ) {
+					$sidebar_setup['theme_mod'] = 'neve_single_product_sidebar_layout';
+				}
+				break;
+			case 'single-page':
+				$sidebar_setup['theme_mod'] = 'neve_other_pages_sidebar_layout';
+				break;
+			case 'shop':
+				if ( class_exists( 'WooCommerce' ) ) {
+					$sidebar_setup['sidebar_slug'] = 'shop-sidebar';
+					if ( is_woocommerce() ) {
+						$sidebar_setup['theme_mod'] = 'neve_shop_archive_sidebar_layout';
+					}
+					if ( is_product() ) {
+						$sidebar_setup['theme_mod'] = 'neve_single_product_sidebar_layout';
+					}
+				}
+				break;
+			default:
+				$sidebar_setup['theme_mod'] = 'neve_other_pages_sidebar_layout';
+		}
 		return $sidebar_setup;
 	}
 
