48472974a45572c9d95d185c07b3bf056effae6d
Andrei Baicus <baicusandrei@me.com>
2018-12-10 17:14:06 +0200

    fix: alt attribute for the author gravatar image. #267

--- inc/views/partials/post_meta.php
+++ inc/views/partials/post_meta.php
@@ -48,7 +48,7 @@ class Post_Meta extends Base_View {
 					$author_email = get_the_author_meta( 'user_email' );
 					$avatar_url   = get_avatar_url( $author_email );
 					$markup      .= '<li class="meta author vcard">';
-					$markup      .= '<img class="photo" src="' . esc_url( $avatar_url ) . '" />&nbsp;<span class="author-name fn">' . wp_kses_post( get_the_author_posts_link() ) . '</span>';
+					$markup      .= '<img class="photo" alt="'. get_the_author() .'" src="' . esc_url( $avatar_url ) . '" />&nbsp;<span class="author-name fn">' . wp_kses_post( get_the_author_posts_link() ) . '</span>';
 					$markup      .= '</li>';
 					break;
 				case 'date':
