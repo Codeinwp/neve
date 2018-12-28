0ec596f7893dd24c31d63270bdac66a908d7c969
selul <marius.cristea@vertistudio.com>
2018-12-28 14:27:15 +0200

    feat: adds new themeisle sdk, remove incomptible modules on deploy.

---
+++ grunt/clean.js
@@ -0,0 +1,9 @@
+/* jshint node:true */
+// https://github.com/gruntjs/grunt-contrib-clean
+module.exports = {
+
+		wporg: [
+			'vendor/codeinwp/themeisle-sdk/src/Modules/Dashboard_widget.php',
+			'vendor/codeinwp/themeisle-sdk/src/Modules/Licenser.php'
+		]
+};
