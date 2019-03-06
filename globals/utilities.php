395005a163f2b83199cd276665a87a0fa65217c0
coti <stefan@themeisle.com>
2019-03-06 14:20:13 +0200

    chore: Compatibility with Neve Hooks plugin
    Retrieve hooks location and name for Neve Hooks plugin.

--- globals/utilities.php
+++ globals/utilities.php
@@ -14,3 +14,26 @@
 function neve_is_amp() {
 	return function_exists( 'is_amp_endpoint' ) && is_amp_endpoint();
 }
+
+/**
+ * Get hooks by location
+ *
+ * @return array $hooks - hooks locations and name
+ */
+function neve_hooks() {
+
+	$hooks = array(
+		'header'  => array(
+			'neve_before_header_hook',
+			'neve_before_navbar_toggle_hook',
+			'neve_after_navbar_toggle_hook',
+			'neve_after_header_hook'
+		),
+		'footer'  => array(
+			'neve_before_footer_hook',
+			'neve_after_footer_hook',
+		),
+	);
+
+	return apply_filters( 'neve_hooks_list', $hooks );
+}
