98297c4a40c4bac23540a7c0f7c91efeb5242596
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2019-03-07 16:33:08 +0200

    chore: build scripts

--- inc/core/front_end.php
+++ inc/core/front_end.php
@@ -197,7 +197,7 @@ class Front_End {
 
 		);
 
-		/* s$this->add_gutenberg_starter_sites(); */
+		/* $this->add_gutenberg_starter_sites(); */
 
 		return apply_filters( 'neve_filter_onboarding_data', $this->onboarding_config );
 	}
