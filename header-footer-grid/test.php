3d151a5d431cbab0d40cdd72d1fde5808df040f0
selul <marius.cristea@vertistudio.com>
2019-03-19 16:30:25 +0200

    removes timestamp switch

--- header-footer-grid/test.php
+++ header-footer-grid/test.php
@@ -30,11 +30,6 @@ add_theme_support(
 );
 require_once "functions-template.php";
 /**
- * Define timestamp which will be used as refference when we enable the HFG functionality.
- * If the theme is installed after this, they would see the HFG by default.
- */
-define( 'NEVE_TOGGLE_HFG_TIMESTAMP', 1552487483 );
-/**
  * Disable search icon in menu as HFG has a search icon item already.
  */
 add_filter( 'neve_should_search_in_menu', '__return_false' );
@@ -42,20 +37,12 @@ add_filter( 'neve_should_search_in_menu', '__return_false' );
 add_filter(
 	'hfg_active', function () {
 	return true;
-//	return ! ( isset( $_GET['builder_off'] ) && $_GET['builder_off'] === 'true' );
-//	if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
-//		return true;
-//	}
-//
-//	return NEVE_TOGGLE_HFG_TIMESTAMP < get_option( 'neve_install', time() );
 }
 );
-if ( apply_filters( 'hfg_active',false ) === true ) {
-	//remove_all_actions( 'neve_do_footer' );
+if ( apply_filters( 'hfg_active', false ) === true ) {
 	add_action( 'neve_do_footer', function () {
 		do_action( 'hfg_footer_render' );
 	} );
-	//remove_all_actions( 'neve_do_header' );
 	add_action( 'neve_do_header', function () {
 		do_action( 'hfg_header_render' );
 	} );
