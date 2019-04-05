93a7109eeb68eba90846a6b7bd2d9ee84053ce23
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2019-04-05 19:16:34 +0300

    chore: remove some more strings + version bump

--- header-footer-grid/Core/Components/Nav.php
+++ header-footer-grid/Core/Components/Nav.php
@@ -83,19 +83,19 @@ class Nav extends Abstract_Component {
 					'choices' => array(
 						'style-plain'         => array(
 							'url'  => Settings::get_instance()->url . '/assets/images/customizer/menu_style_1.svg',
-							'name' => __( 'Plain', 'neve' ),
+							'name' => '',
 						),
 						'style-full-height'   => array(
 							'url'  => Settings::get_instance()->url . '/assets/images/customizer/menu_style_2.svg',
-							'name' => __( 'Full Height', 'neve' ),
+							'name' => '',
 						),
 						'style-border-bottom' => array(
 							'url'  => Settings::get_instance()->url . '/assets/images/customizer/menu_style_3.svg',
-							'name' => __( 'Bottom Border', 'neve' ),
+							'name' => '',
 						),
 						'style-border-top'    => array(
 							'url'  => Settings::get_instance()->url . '/assets/images/customizer/menu_style_4.svg',
-							'name' => __( 'Top Border', 'neve' ),
+							'name' => '',
 						),
 					),
 				]
