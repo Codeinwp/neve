20ff297b585d551f36d38354ae039b91af29fa07
Bogdan Preda <bogdan.preda@themeisle.com>
2019-04-19 17:05:29 +0300

    chore: added class based on theme mod Codeinwp/neve-pro-addon#72

--- header-footer-grid/templates/header-wrapper.php
+++ header-footer-grid/templates/header-wrapper.php
@@ -11,7 +11,12 @@
 namespace HFG;
 
 use HFG\Core\Builder\Header as HeaderBuilder;
+
+$transparent_header_class = '';
+if ( get_theme_mod( 'neve_transparent_header', false ) ) {
+	$transparent_header_class = ' neve-tansparent-header';
+}
 ?>
-<div id="header-grid"  class="<?php echo esc_attr( get_builder( HeaderBuilder::BUILDER_NAME )->get_property( 'panel' ) ); ?> site-header">
+<div id="header-grid"  class="<?php echo esc_attr( get_builder( HeaderBuilder::BUILDER_NAME )->get_property( 'panel' ) ) . esc_attr( $transparent_header_class ); ?> site-header">
 	<?php render_builder( HeaderBuilder::BUILDER_NAME ); ?>
 </div>
