3bf76241f22e07a1aa75b0e488956ca73d7a5205
Andrei Baicus <baicusandrei@me.com>
2019-06-28 14:08:44 +0300

    ci: setting up backstop

---
+++ bin/visual-regression.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+export DOCKER_FILE=docker-compose.travis.yml
+
+#npm install
+#npm run-script build
+#composer install --no-dev
+
+
+docker-compose -f $DOCKER_FILE up -d
+sleep 10
+docker-compose -f $DOCKER_FILE run --rm cli wp core install --url=http://localhost:8055 --title=SandboxSite --admin_user=admin --admin_email=admin@admin.com
+docker-compose -f $DOCKER_FILE run --rm -u root cli chmod 0777 -R /var/www/html
+docker-compose -f $DOCKER_FILE run --rm cli wp site empty --yes
+docker-compose -f $DOCKER_FILE run --rm cli wp theme mod remove --all
+docker-compose -f $DOCKER_FILE run --rm cli wp theme activate neve
+docker-compose -f $DOCKER_FILE run --rm cli wp themeisle-si import $1
+
+
+npm run backstop:reference -- --filter=$1 || export BUILD_EXIT=1
+#npm run backstop:test -- --filter=$1 || export BUILD_EXIT=1
+
+#docker-compose -f $DOCKER_FILE down
+
