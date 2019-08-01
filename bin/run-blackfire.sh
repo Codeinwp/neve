2458ae7572c2f4111c15cb34c5dc58486c0473fc
selul <marius.cristea@vertistudio.com>
2019-08-01 12:57:10 +0300

    build: adds basic blackfire tests

--- bin/run-blackfire.sh
+++ bin/run-blackfire.sh
@@ -33,14 +33,14 @@ fi
 
 BLACKFIRE_TEST_NAME="Local test for $(hostname)";
 
-if [ -z "$TRAVIS_JOB_NUMBER" ]
+if [ -n "$TRAVIS_JOB_NUMBER" ]
 then
       BLACKFIRE_TEST_NAME="Travis job: #$TRAVIS_JOB_NUMBER";
 fi
 
 cd blackfire;
 
-docker-compose up -d
+docker-compose up -d --build
 sleep 10
 
 docker-compose run --user root --rm cli wp core install  --url=http://wordpress --title=BlackfireTest --admin_email=admin@admin.com --admin_user=admin --admin_password=admin  --skip-email --allow-root
@@ -48,14 +48,20 @@ docker-compose run --user root --rm cli wp theme activate neve --allow-root
 docker-compose run --rm --user root cli wp config set WP_SITEURL '"http://" . $_SERVER["HTTP_HOST"]'  --type=constant --add --raw --allow-root
 docker-compose run --rm --user root cli wp config set WP_HOME '"http://" . $_SERVER["HTTP_HOST"]'  --type=constant --add --raw --allow-root
 
-BLACKFIRE_OUTPUT=$(docker-compose exec blackfire blackfire --samples 10 --title "$BLACKFIRE_TEST_NAME" curl http://wordpress)
+BLACKFIRE_OUTPUT=$(docker-compose exec blackfire blackfire --samples 10 --title "$BLACKFIRE_TEST_NAME:home" curl http://wordpress)
+BLACKFIRE_OUTPUT="$BLACKFIRE_OUTPUT"$(docker-compose exec blackfire blackfire --samples 10 --title "$BLACKFIRE_TEST_NAME:sample page" curl http://wordpress/sample-page)
+BLACKFIRE_OUTPUT="$BLACKFIRE_OUTPUT"$(docker-compose exec blackfire blackfire --samples 10 --title "$BLACKFIRE_TEST_NAME:archive" curl http://wordpress/category/uncategorized/)
 
 echo "$BLACKFIRE_OUTPUT"
-
-docker-compose down
 
 if [[ $BLACKFIRE_OUTPUT == *"failing"* ]]; then
   echo "BLACKFIRE test is failing...";
   exit 1;
 fi
+
+docker-compose down
+
 exit 0;
+
+
+
