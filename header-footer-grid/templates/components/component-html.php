b2906e0810af6295577a70d162a7cb7116e4ab83
cristian-ungureanu <cristian@themeisle.com>
2019-08-13 18:04:56 +0300

    fix: allow input in html header footer component

--- header-footer-grid/templates/components/component-html.php
+++ header-footer-grid/templates/components/component-html.php
@@ -14,40 +14,9 @@ use HFG\Core\Components\CustomHtml;
 
 $content = component_setting( CustomHtml::CONTENT_ID );
 $content = apply_filters( 'neve_top_bar_content', $content );
-
-global $allowedposttags;
-$tags_input_included = array_merge(
-	$allowedposttags,
-	array(
-		'input' => array(
-			'accept'       => true,
-			'autocomplete' => true,
-			'autofocus'    => true,
-			'checked'      => true,
-			'class'        => true,
-			'disabled'     => true,
-			'id'           => true,
-			'height'       => true,
-			'min'          => true,
-			'max'          => true,
-			'minlenght'    => true,
-			'maxlength'    => true,
-			'name'         => true,
-			'pattern'      => true,
-			'placeholder'  => true,
-			'readony'      => true,
-			'required'     => true,
-			'size'         => true,
-			'src'          => true,
-			'step'         => true,
-			'type'         => true,
-			'value'        => true,
-			'width'        => true,
-		),
-	)
-);
+$allowed_post_tags   = wp_kses_allowed_html( 'header_footer_grid' );
 
 ?>
 <div class="nv-html-content">
-	<?php echo wp_kses( balanceTags( $content, true ), $tags_input_included ); ?>
+	<?php echo wp_kses( balanceTags( $content, true ), $allowed_post_tags ); ?>
 </div>
