23afc28d88a3b9a50422327c9f282e7fbf703c1b
Andrei Baicus <baicusandrei@me.com>
2019-08-22 10:01:03 +0300

    fix: duplicated ids for fallback menus in navwalker [#694]

--- inc/views/nav_walker.php
+++ inc/views/nav_walker.php
@@ -180,7 +180,7 @@ class Nav_Walker extends \Walker_Nav_Menu {
 	public static function fallback() {
 		$fallback_args = array(
 			'depth'      => - 1,
-			'menu_id'    => 'nv-primary-navigation',
+			'menu_id'    => 'nv-primary-navigation' . '-' . \HFG\current_row( \HFG\Core\Builder\Header::BUILDER_NAME ),
 			'menu_class' => 'primary-menu-ul',
 			'container'  => 'ul',
 			'before'     => '',
