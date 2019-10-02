bc5de81e63dd89786402265dce67dcdb45010926
Andrei Baicus <baicusandrei@me.com>
2019-10-02 10:09:38 +0300

    test: container customizer settings + improve row background test [closes Codeinwp/neve-pro-addon#299]

--- cypress/integration/customizer/container-settings.spec.js
+++ cypress/integration/customizer/container-settings.spec.js
@@ -6,8 +6,9 @@ describe( 'Container Settings', function() {
 		'pageUrl': null,
 		'postUrl': null
 	};
+
 	it( 'Create new page named "' + setup.title + '".', function() {
-		cy.insertPost(setup.title, setup.content, 'page');
+		cy.insertPost( setup.title, setup.content, 'page' );
 		cy.get( '.post-publish-panel__postpublish-header a' ).
 				contains( setup.title ).
 				should( 'have.attr', 'href' ).
@@ -16,60 +17,117 @@ describe( 'Container Settings', function() {
 				} );
 	} );
 
-	// it( 'Create new post named "' + setup.title + '".', function() {
-	// 	cy.insertPost( setup.title, setup.content, 'post', true );
-	// 	cy.get( '.post-publish-panel__postpublish-header a' ).
-	// 			contains( setup.title ).
-	// 			should( 'have.attr', 'href' ).
-	// 			then( (href) => {
-	// 				setup.postUrl = href;
-	// 			} );
-	// } );
+	it( 'Create new post named "' + setup.title + '".', function() {
+		cy.insertPost( setup.title, setup.content, 'post', true );
+		cy.get( '.post-publish-panel__postpublish-header a' ).
+				contains( setup.title ).
+				should( 'have.attr', 'href' ).
+				then( (href) => {
+					setup.postUrl = href;
+				} );
+	} );
 
-		it( 'Setup customizer container settings.', function() {
-			cy.login( '/wp-admin/customize.php' );
-			aliasRestRoutes();
-			cy.get( '#accordion-panel-neve_layout' ).click();
-			cy.get( '#accordion-section-neve_container' ).click();
-			cy.get( '#customize-control-neve_container_width' ).as( 'control' );
+	it( 'Setup customizer container width.', function() {
+		cy.login( '/wp-admin/customize.php' );
+		aliasRestRoutes();
+		cy.get( '#accordion-panel-neve_layout' ).click();
+		cy.get( '#accordion-section-neve_container' ).click();
+		cy.get( '#customize-control-neve_container_width' ).as( 'control' );
 
-			let devices = { 'desktop': 1200, 'tablet': 960, 'mobile': 680 };
-			Object.keys( devices ).map( (device) => {
-				console.log( devices[device] );
-				cy.get( '@control' ).
-						find( '.responsive-switchers .preview-' + device ).
-						click();
-				cy.get( '@control' ).find( '.control-wrap.' + device + ' .range-slider-value' ).
-						invoke( 'val', devices[device] ).
-						trigger( 'change' );
-			} );
-
-			// cy.get('.customize-control-neve_default_container_style select').select('full-width');
-			// cy.get('.customize-control-neve_blog_archive_container_style select').select('full-width');
-			// cy.get('.customize-control-neve_single_post_container_style').select('full-width');
-
-			cy.get( '#save' ).click();
-			cy.wait( '@customizerSave' ).then( (req) => {
-				expect( req.response.body.success ).to.be.true;
-				expect( req.status ).to.equal( 200 );
-			} );
+		let devices = { 'desktop': 1200, 'tablet': 960, 'mobile': 680 };
+		Object.keys( devices ).map( (device) => {
+			cy.get( '@control' ).
+					find( '.responsive-switchers .preview-' + device ).
+					click();
+			cy.get( '@control' ).
+					find( '.control-wrap.' + device + ' .range-slider-value' ).
+					invoke( 'val', devices[device] ).
+					trigger( 'change' );
 		} );
 
+		cy.get( '#save' ).click();
+		cy.wait( '@customizerSave' ).then( (req) => {
+			expect( req.response.body.success ).to.be.true;
+			expect( req.status ).to.equal( 200 );
+		} );
+	} );
+
 	it( 'Container width on front end.', function() {
-		cy.navigate(setup.pageUrl);
-		cy.get('.single-page-container').as('container');
-		cy.get('@container').should('have.css', 'max-width').
-				and('eq', '1200px');
+		cy.navigate( setup.pageUrl );
+		cy.get( '.single-page-container' ).as( 'container' );
+		cy.get( '@container' ).should( 'have.css', 'max-width' ).
+				and( 'eq', '1200px' );
 
 		cy.viewport( 768, 1024 );//iPad
-		cy.get('@container').should('have.css', 'max-width').
-				and('eq', '960px');
+		cy.get( '@container' ).should( 'have.css', 'max-width' ).
+				and( 'eq', '960px' );
 
 		cy.viewport( 375, 812 ); //iPhoneX
-		cy.get('@container').should('have.css', 'max-width').
-				and('eq', '680px');
+		cy.get( '@container' ).should( 'have.css', 'max-width' ).
+				and( 'eq', '680px' );
+	} );
+
+	it( 'Setup customizer container style.', function() {
+		cy.login( '/wp-admin/customize.php' );
+		aliasRestRoutes();
+		cy.get( '#accordion-panel-neve_layout' ).click();
+		cy.get( '#accordion-section-neve_container' ).click();
+		cy.get( '#_customize-input-neve_default_container_style' ).
+				select( 'full-width' );
+		cy.get( '#_customize-input-neve_blog_archive_container_style' ).
+				select( 'full-width' );
+		cy.get( '#_customize-input-neve_single_post_container_style' ).
+				select( 'full-width' );
+		cy.get( '#save' ).click();
+		cy.wait( '@customizerSave' ).then( (req) => {
+			expect( req.response.body.success ).to.be.true;
+			expect( req.status ).to.equal( 200 );
+		} );
+	} );
+
+	it( 'Container style on front end.', function() {
+		cy.navigate( setup.pageUrl );
+		cy.get( '.single-page-container' ).as( 'container' );
+		cy.get( '@container' ).should( 'have.class', 'container-fluid' );
+		cy.navigate( setup.postUrl );
+		cy.get( '.single-post-container' ).as( 'container' );
+		cy.get( '@container' ).should( 'have.class', 'container-fluid' );
+	} );
+
+
+	it( 'Go back to defaults.', function() {
+		cy.login( '/wp-admin/customize.php' );
+		aliasRestRoutes();
+		cy.get( '#accordion-panel-neve_layout' ).click();
+		cy.get( '#accordion-section-neve_container' ).click();
+		cy.get( '#customize-control-neve_container_width' ).as( 'control' );
+		cy.get( '#_customize-input-neve_default_container_style' ).
+				select( 'contained' );
+		cy.get( '#_customize-input-neve_blog_archive_container_style' ).
+				select( 'contained' );
+		cy.get( '#_customize-input-neve_single_post_container_style' ).
+				select( 'contained' );
+
+		cy.get( '#customize-control-neve_container_width' ).as( 'control' );
+
+		let devices = ['desktop', 'tablet', 'mobile'];
+		devices.map( (device) => {
+			cy.get( '@control' ).
+					find( '.responsive-switchers .preview-' + device ).
+					click();
+			cy.get( '@control' ).
+					find( '.control-wrap.' + device + ' .range-reset-slider' ).
+					should( 'be.visible' ).
+					click();
+		} );
+		cy.get( '#save' ).click();
+		cy.wait( '@customizerSave' ).then( (req) => {
+			expect( req.response.body.success ).to.be.true;
+			expect( req.status ).to.equal( 200 );
+		} );
 	} );
 } );
+
 
 function aliasRestRoutes() {
 	let home = Cypress.config().baseUrl;
