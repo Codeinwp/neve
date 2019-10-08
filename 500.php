f98f0cd929f0f68d01d69dd585b63dcda0c80fe3
cristian-ungureanu <cristian@themeisle.com>
2019-10-08 23:06:58 +0300

    chore: fix offline and server error page style for pwa

--- 500.php
+++ 500.php
@@ -1,8 +1,27 @@
 <?php
 /**
- * 500 template.
+ * The template for displaying the 500 server error page
  *
  * @package Neve
- */
+ */ ?>
+	<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<?php wp_head(); ?>
+	</head>
+
+<body <?php body_class(); ?> <?php echo wp_kses( apply_filters( 'neve_body_data_attrs', '' ), array( '[class]' => true ) ); ?>>
+<?php wp_body_open(); ?>
+<?php
 do_action( 'neve_do_server_error' );
+
+wp_footer(); 
+?>
+
+</body>
+
+</html>
