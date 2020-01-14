c6711519dd847307b0927ba465fea5c780c684ae
selul <marius.cristea@vertistudio.com>
2020-01-14 20:20:40 +0200

    build: modern js

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,39 +1,5 @@
 /* jshint node:true */
 // https://github.com/gruntjs/grunt-contrib-uglify
 module.exports = {
-	scripts: {
-		files: {
-			'./assets/js/metabox.min.js': [ './assets/js/metabox.js' ],
-		},
-		options: {
-			compress: true,
-			mangle: true,
-			sourceMap: false
-		},
-	},
 
-	customizerControls: {
-		files: {
-			'./assets/js/customizer-controls.min.js': [ './assets/js/admin/src/customizer-controls.js', './inc/customizer/controls/js/*.js', './inc/addons/customizer/controls/js/*.js' ],
-			'./assets/js/customizer-preview.min.js': [ './assets/js/admin/src/customizer-preview.js', './inc/customizer/options/js/*.js', './inc/addons/customizer/options/js/*.js' ],
-		},
-		options: {
-			compress: true,
-			mangle: true,
-			sourceMap: false
-		},
-	},
-
-	unMinified: {
-		files: {
-			'./assets/js/customizer-controls.js': [ './inc/customizer/controls/js/*.js', './inc/addons/customizer/controls/js/*.js' ],
-			'./assets/js/customizer-preview.js': [ './assets/js/admin/src/customizer-controls.js', './inc/customizer/options/js/*.js', './inc/addons/customizer/options/js/*.js' ],
-		},
-		options: {
-			compress: false,
-			mangle: false,
-			sourceMap: true,
-			beautify: true
-		},
-	}
 };
