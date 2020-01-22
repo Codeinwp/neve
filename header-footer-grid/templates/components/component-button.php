979c60438d2c7a609c29a8953adc9d668bac190d
cristian-ungureanu <cristian@themeisle.com>
2020-01-22 16:01:30 +0200

    fix: second component getting the translation from the first component

--- header-footer-grid/templates/components/component-button.php
+++ header-footer-grid/templates/components/component-button.php
@@ -13,9 +13,8 @@ namespace HFG;
 use HFG\Core\Components\Button;
 
 $text        = parse_dynamic_tags( component_setting( Button::TEXT_ID ) );
-$text        = apply_filters( 'neve_translate_single_string', $text );
 $button_link = parse_dynamic_tags( component_setting( Button::LINK_ID ) );
-$button_link = apply_filters( 'neve_translate_single_string', $button_link )
+
 ?>
 <div class="component-wrap">
 	<a href="<?php echo esc_url( $button_link ); ?>"
