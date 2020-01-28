26b86ce68aa8e2e93c9a3c65116bf370785c009a
abaicus <baicusandrei@me.com>
2020-01-28 12:25:35 +0200

    style: seamless add to cart adaptations [ref: Codeinwp/neve-pro-addon#442]

--- inc/compatibility/woocommerce.php
+++ inc/compatibility/woocommerce.php
@@ -80,6 +80,7 @@ class Woocommerce {
 			.woocommerce .checkout_coupon button.button,
 			.woocommerce #review_form #respond input#submit,
 			.woocommerce .price_slider_amount button.button:not(.nv-sidebar-toggle),
+			.woocommerce .woocommerce-mini-cart__buttons.buttons a.button.wc-forward:not(.checkout),
 			.woocommerce .button.button-secondary.more-details',
 		'hover'            => '
 			,#comments input[type=submit]:hover,
@@ -90,6 +91,7 @@ class Woocommerce {
 			.woocommerce .checkout_coupon button.button:hover,
 			.woocommerce #review_form #respond input#submit:hover,
 			.woocommerce .price_slider_amount button.button:hover,
+			.woocommerce .woocommerce-mini-cart__buttons.buttons a.button.wc-forward:not(.checkout):hover,
 			.woocommerce .button.button-secondary.more-details:hover',
 		'no-padding'       => '
 			,.woocommerce ul[id^="nv-primary-navigation"] .woocommerce-mini-cart__buttons.buttons a.button.wc-forward:not(.checkout),
