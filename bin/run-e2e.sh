58cf465fd7a0d6187cfa3284b7546553c7f2a08d
Andrei Baicus <baicusandrei@me.com>
2020-02-20 14:07:31 +0200

    ci: moving to GitHub actions [WIP]

--- bin/run-e2e.sh
+++ bin/run-e2e.sh
@@ -35,9 +35,6 @@ docker-compose -f $DOCKER_FILE run --rm wordpress bash wp core update-db
 docker-compose -f $DOCKER_FILE run --rm wordpress bash wp option admin_email_lifespan 1763469022
 
 export BUILD_INFO_MESSAGE="${BUILD_SPEC} \n ${COMMIT_INFO_MESSAGE}"
-# Run Cypress
-#npm run cypress:open
-npm run cypress:run || exit 1
 
 # Bring stack down.
 docker-compose -f $DOCKER_FILE down
