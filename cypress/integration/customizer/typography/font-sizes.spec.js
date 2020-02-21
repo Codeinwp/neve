e2372a1cb92e8d54dc832bc2b508ee3a958f8766
Andrei Baicus <baicusandrei@me.com>
2020-02-21 14:19:04 +0200

    ci: moving to GitHub actions [WIP]

--- cypress/integration/customizer/typography/font-sizes.spec.js
+++ cypress/integration/customizer/typography/font-sizes.spec.js
@@ -122,92 +122,92 @@ describe( 'Typography Control', function() {
 		// } );
 	} );
 
-	// it( 'Test Typography on Front End', function() {
-	// 	cy.visit( '/markup-html-tags-and-formatting/' );
-	//
-	// 	// Test text transform.
-	// 	cy.get( 'body' ).
-	// 			should( 'have.css', 'text-transform' ).
-	// 			and( 'match',
-	// 					new RegExp( setup.general.transform.toLowerCase(), 'g' ) );
-	//
-	// 	// Test font weight.
-	// 	cy.get( 'body' ).
-	// 			should( 'have.css', 'font-weight' ).
-	// 			and( 'match',
-	// 					new RegExp( setup.general.weight, 'g' ) );
-	//
-	// 	let deviceMap = {
-	// 		'desktop': {
-	// 			height: 1080, width: 1920
-	// 		},
-	// 		'tablet': {
-	// 			height: 1024, width: 768
-	// 		},
-	// 		'mobile': {
-	// 			height: 667, width: 375
-	// 		}
-	// 	};
-	//
-	// 	Object.keys( deviceMap ).map( device => {
-	// 		// Change viewport.
-	// 		cy.viewport( deviceMap[device].width, deviceMap[device].height );
-	//
-	// 		// Test font size.
-	// 		cy.get( 'body' ).
-	// 				should( 'have.css', 'font-size' ).
-	// 				and( 'match',
-	// 						new RegExp( setup.general.fontSize[device] + 'px', 'g' ) );
-	//
-	// 		// Test line height.
-	// 		cy.get( 'body' ).
-	// 				should( 'have.css', 'line-height' ).
-	// 				and( 'match',
-	// 						new RegExp( setup.general.lineHeight[device], 'g' ) );
-	//
-	// 		// Test letter spacing.
-	// 		cy.get( 'body' ).
-	// 				should( 'have.css', 'letter-spacing' ).
-	// 				and( 'match',
-	// 						new RegExp( setup.general.letterSpacing[device], 'g' ) );
-	// 	} );
-	// } );
-	//
-	// it( 'Test Typography inside the Editor', function() {
-	// 	cy.visit( '/markup-html-tags-and-formatting/' );
-	// 	cy.get( '#wp-admin-bar-edit > a' ).click();
-	//
-	// 	cy.get( '.editor-styles-wrapper .wp-block p' ).
-	// 			first().
-	// 			as( 'editorBody' );
-	// 	// Test text transform.
-	// 	cy.get( '@editorBody' ).
-	// 			should( 'have.css', 'text-transform' ).
-	// 			and( 'match',
-	// 					new RegExp( setup.general.transform.toLowerCase(), 'g' ) );
-	//
-	// 	// Test font weight.
-	// 	cy.get( '@editorBody' ).
-	// 			should( 'have.css', 'font-weight' ).
-	// 			and( 'match',
-	// 					new RegExp( setup.general.weight, 'g' ) );
-	//
-	// 	// Test font size.
-	// 	cy.get( '@editorBody' ).
-	// 			should( 'have.css', 'font-size' ).
-	// 			and( 'match',
-	// 					new RegExp( setup.general.fontSize['desktop'] + 'px', 'g' ) );
-	//
-	// 	// Test line height.
-	// 	cy.get( '@editorBody' ).
-	// 			should( 'have.css', 'line-height' ).
-	// 			and( 'match',
-	// 					new RegExp( setup.general.lineHeight['desktop'], 'g' ) );
-	//
-	// 	// Test letter spacing.
-	// 	cy.get( '@editorBody' ).
-	// 			should( 'have.css', 'letter-spacing' ).
-	// 			and( 'match',
-	// 					new RegExp( setup.general.letterSpacing['desktop'], 'g' ) );
-	// } );
+	it( 'Test Typography on Front End', function() {
+		cy.visit( '/markup-html-tags-and-formatting/' );
+
+		// Test text transform.
+		cy.get( 'body' ).
+				should( 'have.css', 'text-transform' ).
+				and( 'match',
+						new RegExp( setup.general.transform.toLowerCase(), 'g' ) );
+
+		// Test font weight.
+		cy.get( 'body' ).
+				should( 'have.css', 'font-weight' ).
+				and( 'match',
+						new RegExp( setup.general.weight, 'g' ) );
+
+		let deviceMap = {
+			'desktop': {
+				height: 1080, width: 1920
+			},
+			'tablet': {
+				height: 1024, width: 768
+			},
+			'mobile': {
+				height: 667, width: 375
+			}
+		};
+
+		Object.keys( deviceMap ).map( device => {
+			// Change viewport.
+			cy.viewport( deviceMap[device].width, deviceMap[device].height );
+
+			// Test font size.
+			cy.get( 'body' ).
+					should( 'have.css', 'font-size' ).
+					and( 'match',
+							new RegExp( setup.general.fontSize[device] + 'px', 'g' ) );
+
+			// Test line height.
+			cy.get( 'body' ).
+					should( 'have.css', 'line-height' ).
+					and( 'match',
+							new RegExp( setup.general.lineHeight[device], 'g' ) );
+
+			// Test letter spacing.
+			cy.get( 'body' ).
+					should( 'have.css', 'letter-spacing' ).
+					and( 'match',
+							new RegExp( setup.general.letterSpacing[device], 'g' ) );
+		} );
+	} );
+
+	it( 'Test Typography inside the Editor', function() {
+		cy.visit( '/markup-html-tags-and-formatting/' );
+		cy.get( '#wp-admin-bar-edit > a' ).click();
+
+		cy.get( '.editor-styles-wrapper .wp-block p' ).
+				first().
+				as( 'editorBody' );
+		// Test text transform.
+		cy.get( '@editorBody' ).
+				should( 'have.css', 'text-transform' ).
+				and( 'match',
+						new RegExp( setup.general.transform.toLowerCase(), 'g' ) );
+
+		// Test font weight.
+		cy.get( '@editorBody' ).
+				should( 'have.css', 'font-weight' ).
+				and( 'match',
+						new RegExp( setup.general.weight, 'g' ) );
+
+		// Test font size.
+		cy.get( '@editorBody' ).
+				should( 'have.css', 'font-size' ).
+				and( 'match',
+						new RegExp( setup.general.fontSize['desktop'] + 'px', 'g' ) );
+
+		// Test line height.
+		cy.get( '@editorBody' ).
+				should( 'have.css', 'line-height' ).
+				and( 'match',
+						new RegExp( setup.general.lineHeight['desktop'], 'g' ) );
+
+		// Test letter spacing.
+		cy.get( '@editorBody' ).
+				should( 'have.css', 'letter-spacing' ).
+				and( 'match',
+						new RegExp( setup.general.letterSpacing['desktop'], 'g' ) );
+	} );
 } );
