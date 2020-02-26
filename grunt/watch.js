6a49feb52fe62ac8e6278bc303369a85cf93724f
Andrei Baicus <baicusandrei@me.com>
2020-02-26 16:55:13 +0200

    ci: moving to GitHub actions [WIP] - drops grunt-theme-fleet; - cleanup grunt tasks; - move pot file generation to wp-cli in grunt container; - regenerates pot; - regenerates scripts; - unifies build tasks in grunt and npm;

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,13 +1,6 @@
 /* jshint node:true */
 // https://github.com/gruntjs/grunt-contrib-watch
 module.exports = {
-	customizerJs: {
-		files: [
-			'inc/customizer/controls/js/*.js',
-			'inc/customizer/options/js/*.js',
-		],
-		tasks: [ 'uglify:customizerControls', 'uglify:unMinified' ]
-	},
 	sass: {
 		files: [ 'assets/scss/**/*.scss', 'header-footer-grid/assets/scss/**/*.scss' ],
 		tasks: [ 'sass:style', 'cssmin' ]
