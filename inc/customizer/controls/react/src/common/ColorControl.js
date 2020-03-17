f2d189c09fdcacb70e2016fd33f4357ad51ef571
abaicus <baicusandrei@me.com>
2020-03-17 14:27:16 +0200

    chore: adds unset state for color controls

--- inc/customizer/controls/react/src/common/ColorControl.js
+++ inc/customizer/controls/react/src/common/ColorControl.js
@@ -1,13 +1,15 @@
 /* global wp */
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
 
 const { ColorPicker, Button, Dropdown } = wp.components
-const { Component } = wp.element
+const { Component, Fragment } = wp.element
+const { __ } = wp.i18n
 
-class NumberControl extends Component {
+class ColorControl extends Component {
   render() {
     const { label, selectedColor } = this.props
-
+    let toggle = null
     return (
       <div className='neve-control-header neve-color-component'>
         {
@@ -18,20 +20,36 @@ class NumberControl extends Component {
         }
 
         <Dropdown
-          renderToggle={( { isOpen, onToggle } ) => (
-            <Button
-              className='is-secondary is-button'
-              onClick={onToggle}
-              aria-expanded={isOpen}
-              style={{ backgroundColor: selectedColor }}
-            />
-          )}
+          renderToggle={( { isOpen, onToggle } ) => {
+            toggle = onToggle
+            return (
+              <Button
+                className={classnames(['is-secondary is-button', { 'is-empty': !selectedColor }])}
+                onClick={onToggle}
+                aria-expanded={isOpen}
+                style={{ backgroundColor: selectedColor }}
+              />
+            )
+          }}
           renderContent={() => (
-            <ColorPicker
-              color={selectedColor}
-              onChangeComplete={( value ) => { this.props.onChange(value.hex) }}
-              disableAlpha
-            />
+            <Fragment>
+              <ColorPicker
+                color={selectedColor}
+                onChangeComplete={( value ) => { this.props.onChange(value.hex) }}
+                disableAlpha
+              />
+              {selectedColor &&
+                <Button
+                  className='clear'
+                  isPrimary
+                  onClick={() => {
+                    this.props.onChange('')
+                    toggle()
+                  }}
+                >
+                  {__( 'Clear', 'neve' )}
+                </Button>}
+            </Fragment>
           )}
         />
       </div>
@@ -39,10 +57,10 @@ class NumberControl extends Component {
   }
 }
 
-NumberControl.propTypes = {
+ColorControl.propTypes = {
   label: PropTypes.string,
   onChange: PropTypes.func.isRequired,
   selectedColor: PropTypes.string.isRequired
 }
 
-export default NumberControl
+export default ColorControl
