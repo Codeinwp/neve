a68c1d2170cbce130383083961dafc823ef527b0
abaicus <baicusandrei@me.com>
2020-03-19 13:14:06 +0200

    ci: trigger deploy [skip ci]

--- bin/dist.sh
+++ bin/dist.sh
@@ -5,6 +5,9 @@ export BUILD_VERSION
 BUILD_NAME=$(node -pe "require('./package.json').name")
 export BUILD_NAME
 
+echo "::set-env name=BUILD_NAME::$BUILD_NAME"
+echo "::set-env name=BUILD_VERSION::$BUILD_VERSION"
+
 if [ ! -d "dist" ]; then
   mkdir "dist"
 fi
@@ -18,3 +21,4 @@ rsync -rc --exclude-from ".distignore" "./" "dist/$BUILD_NAME"
 cd dist
 zip -r "../artifact/$BUILD_NAME" "./$BUILD_NAME/"
 cd -
+
