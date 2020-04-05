d0f2132fe122cd8dee378c77a6e22565dbf6caf3
selul <marius.cristea@vertistudio.com>
2020-04-05 13:39:39 +0300

    build: optimize e2e setup and remove redundant files

---
+++ bin/cli-setup.sh
@@ -0,0 +1,27 @@
+wp  --allow-root core install --url=http://localhost:8080 --title=SandboxSite --admin_user=admin --admin_password=admin --admin_email=admin@admin.com
+mkdir /var/www/html/wp-content/uploads
+chmod 0777 -R /var/www/html/wp-content/
+rm -rf /var/www/html/wp-content/plugins/akismet
+
+
+#Setup core
+wp --allow-root core update
+wp --allow-root core update-db
+wp --allow-root option update admin_email_lifespan 1763469022
+#Setup Environment
+
+wp --allow-root theme is-installed neve && wp --allow-root theme update neve ||  wp --allow-root theme install neve
+wp --allow-root plugin is-installed wordpress-importer && wp --allow-root plugin update wordpress-importer ||  wp --allow-root plugin install wordpress-importer
+wp --allow-root plugin is-installed amp && wp --allow-root plugin update amp ||  wp --allow-root plugin install amp
+wp --allow-root plugin activate --all
+wp --allow-root theme activate neve
+
+wp --allow-root option patch update amp-options theme_support 'transitional'
+
+##Setup core content.
+curl -O https://wpcom-themes.svn.automattic.com/demo/theme-unit-test-data.xml
+wp --allow-root import ./theme-unit-test-data.xml --authors=skip  --skip=image_resize
+
+#Generic setup
+wp --allow-root rewrite structure /%postname%/
+wp --allow-root menu location assign all-pages primary
