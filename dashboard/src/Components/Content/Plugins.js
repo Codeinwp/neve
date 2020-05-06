06d68e573125bcce794e77b575c451a32c37586c
abaicus <baicusandrei@me.com>
2020-05-06 14:57:29 +0300

    chore: add store state so that plugin status stays consistent on tab switch cc: @rodica-andronache

--- dashboard/src/Components/Content/Plugins.js
+++ dashboard/src/Components/Content/Plugins.js
@@ -1,24 +1,28 @@
 /* global neveDash */
 import PluginCard from '../PluginCard';
 
+const {withSelect} = wp.data;
 const {Fragment} = wp.element;
 
-const Header = () => {
-  const {plugins} = neveDash;
+const Header = ({plugins}) => {
+	if (! plugins) {
+		return null;
+	}
 
-  if ( ! plugins ) {
-    return null;
-  }
-
-  return (
-    <Fragment>
-      {
-        Object.keys( plugins ).map( (slug) => {
-          return <PluginCard key={slug} slug={slug} data={plugins[slug]}/>;
-        } )
-      }
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			{
+				Object.keys(plugins).map((slug) => {
+					return <PluginCard key={slug} slug={slug} data={plugins[slug]}/>;
+				})
+			}
+		</Fragment>
+	);
 };
 
-export default Header;
+export default withSelect(select => {
+	const {getPlugins} = select('neve-dashboard');
+	return {
+		plugins: getPlugins()
+	};
+})(Header);
