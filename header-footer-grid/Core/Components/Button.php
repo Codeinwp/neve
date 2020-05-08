2b8622297a5d30d79373176ad35d37df0f4886d6
selul <marius.cristea@vertistudio.com>
2020-05-08 17:26:12 +0300

    fix button border default value on hfg components, ref:  https://app.diffy.website#/shared/MjkzMzMxMjM1/diffs/29333/single/0/1440

--- header-footer-grid/Core/Components/Button.php
+++ header-footer-grid/Core/Components/Button.php
@@ -24,9 +24,9 @@ use Neve\Core\Styles\Dynamic_Selector;
 class Button extends Abstract_Component {
 
 	const COMPONENT_ID = 'button_base';
-	const LINK_ID      = 'link_setting';
-	const TEXT_ID      = 'text_setting';
-	const STYLE_ID     = 'style_setting';
+	const LINK_ID = 'link_setting';
+	const TEXT_ID = 'text_setting';
+	const STYLE_ID = 'style_setting';
 
 	/**
 	 * Default spacing value
@@ -164,9 +164,18 @@ class Button extends Abstract_Component {
 			Dynamic_Selector::KEY_RULES    => [
 				Config::CSS_PROP_BACKGROUND_COLOR => $id . '.background',
 				Config::CSS_PROP_COLOR            => $id . '.text',
-				Config::CSS_PROP_BORDER_RADIUS    => $id . '.borderRadius',
-				Config::CSS_PROP_CUSTOM_BTN_TYPE  => $id . '.type',
-				Config::CSS_PROP_BORDER_WIDTH     => $id . '.borderWidth',
+				Config::CSS_PROP_BORDER_RADIUS    => [
+					Dynamic_Selector::META_KEY     => $id . '.borderRadius',
+					Dynamic_Selector::META_DEFAULT => 3
+				],
+				Config::CSS_PROP_CUSTOM_BTN_TYPE  => [
+					Dynamic_Selector::META_KEY     => $id . '.type',
+					Dynamic_Selector::META_DEFAULT => 'fill',
+				],
+				Config::CSS_PROP_BORDER_WIDTH     => [
+					Dynamic_Selector::META_KEY     => $id . '.borderWidth',
+					Dynamic_Selector::META_DEFAULT => 1
+				],
 			],
 		];
 		$css_array[] = [
