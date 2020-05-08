d03e38dab30e01505974cc3dc19b204110c352cc
selul <marius.cristea@vertistudio.com>
2020-05-08 20:02:22 +0300

    fix default colors for gutenberg helper classes, ref: https://app.diffy.website#/shared/MjkzMzMxMjM1/diffs/29333/single/5/1440

--- inc/core/styles/generator.php
+++ inc/core/styles/generator.php
@@ -90,48 +90,56 @@ class Generator {
 				Config::CSS_PROP_COLOR => [
 					Dynamic_Selector::META_KEY       => Config::MODS_LINK_HOVER_COLOR,
 					Dynamic_Selector::META_IMPORTANT => true,
+					Dynamic_Selector::META_DEFAULT => '#0366d6'
 				],
 			],
 			'.has-neve-link-color-color'                  => [
 				Config::CSS_PROP_COLOR => [
 					Dynamic_Selector::META_KEY       => Config::MODS_LINK_COLOR,
 					Dynamic_Selector::META_IMPORTANT => true,
+					Dynamic_Selector::META_DEFAULT => '#0366d6'
 				],
 			],
 			'.has-neve-text-color-color'                  => [
 				Config::CSS_PROP_COLOR => [
 					Dynamic_Selector::META_KEY       => Config::MODS_TEXT_COLOR,
 					Dynamic_Selector::META_IMPORTANT => true,
+					Dynamic_Selector::META_DEFAULT => '#404248'
 				],
 			],
 			'.has-neve-button-color-color'                => [
 				Config::CSS_PROP_COLOR => [
 					Dynamic_Selector::META_KEY       => Config::MODS_BUTTON_PRIMARY_STYLE . '.background',
 					Dynamic_Selector::META_IMPORTANT => true,
+					Dynamic_Selector::META_DEFAULT => '#0366d6'
 				],
 			],
 			'.has-neve-link-color-background-color'       => [
 				Config::CSS_PROP_BACKGROUND_COLOR => [
 					Dynamic_Selector::META_KEY       => Config::MODS_LINK_COLOR,
 					Dynamic_Selector::META_IMPORTANT => true,
+					Dynamic_Selector::META_DEFAULT => '#0366d6'
 				],
 			],
 			'.has-neve-link-hover-color-background-color' => [
 				Config::CSS_PROP_BACKGROUND_COLOR => [
 					Dynamic_Selector::META_KEY       => Config::MODS_LINK_HOVER_COLOR,
 					Dynamic_Selector::META_IMPORTANT => true,
+					Dynamic_Selector::META_DEFAULT => '#0366d6'
 				],
 			],
 			'.has-neve-button-color-background-color'     => [
 				Config::CSS_PROP_BACKGROUND_COLOR => [
 					Dynamic_Selector::META_KEY       => Config::MODS_BUTTON_PRIMARY_STYLE . '.background',
 					Dynamic_Selector::META_IMPORTANT => true,
+					Dynamic_Selector::META_DEFAULT => '#0366d6'
 				],
 			],
 			'.has-neve-text-color-background-color'       => [
 				Config::CSS_PROP_BACKGROUND_COLOR => [
 					Dynamic_Selector::META_KEY       => Config::MODS_TEXT_COLOR,
 					Dynamic_Selector::META_IMPORTANT => true,
+					Dynamic_Selector::META_DEFAULT => '#404248'
 				],
 			],
 		];
