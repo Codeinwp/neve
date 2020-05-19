5a970f2be1c7de05ddc0e9506d16702ac5497fd8
abaicus <baicusandrei@me.com>
2020-05-19 16:05:55 +0300

    chore: adds rest api check on starter-sites component initialization [Codeinwp/neve-pro-addon#695]

--- dashboard/src/Components/Content/StarterSites.js
+++ dashboard/src/Components/Content/StarterSites.js
@@ -1,8 +1,23 @@
 /* global neveDash */
 import Onboarding from '../Onboarding/Main';
+import {get} from '../../utils/rest';
+
+const {useEffect, useState} = wp.element;
 
 const StarterSites = () => {
-	return <Onboarding/>;
+	const [ restApiState, setRestApiState ] = useState(true);
+
+	useEffect(() => {
+		get(neveDash.onboarding.root, true).then(r => {
+			if ( ! r.ok || 200 !== r.status ) {
+				setRestApiState(false);
+			}
+		});
+	}, []);
+
+	return (
+		restApiState ? <Onboarding/> : <div className="no-rest card"><p dangerouslySetInnerHTML={{__html: neveDash.onboarding.i18n.rest_not_working}}></p></div>
+		);
 };
 
 export default StarterSites;
