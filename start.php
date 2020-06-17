f0da45bac95c37d4df7884b2e0733b142aecdea8
abaicus <baicusandrei@me.com>
2020-06-17 12:56:18 +0300

    chore: use SDK filters for license / tiers [Codeinwp/neve-pro-addon#763]

--- start.php
+++ start.php
@@ -9,6 +9,21 @@
  * Run theme functionality
  */
 function neve_run() {
+	define(
+		'NEVE_FILE_TREE',
+		[
+			1 => 1,
+			2 => 1,
+			3 => 2,
+			4 => 2,
+			5 => 3,
+			6 => 3,
+			7 => 1,
+			8 => 2,
+			9 => 3,
+		] 
+	);
+
 	$vendor_file = trailingslashit( get_template_directory() ) . 'vendor/autoload.php';
 	if ( is_readable( $vendor_file ) ) {
 		require_once $vendor_file;
@@ -29,6 +44,26 @@ function neve_run() {
 	}
 
 	if ( class_exists( '\\Neve_Pro\\Core\\Loader' ) ) {
+		if ( defined( 'NEVE_PRO_BASEFILE' ) ) {
+			$slug  = basename( dirname( NEVE_PRO_BASEFILE ) );
+			$slug  = str_replace( '-', '_', strtolower( trim( $slug ) ) );
+			$value = get_option( $slug . '_license_data' );
+
+			if ( ! empty( $value ) ) {
+				add_filter(
+					'product_neve_license_status',
+					function ( $v ) use ( $value ) {
+						return isset( $value->license ) ? $value->license : 'free';
+					} 
+				);
+				add_filter(
+					'product_neve_license_plan',
+					function( $v ) use ( $value ) {
+						return isset( $value->plan ) ? $value->plan : -1;
+					} 
+				);
+			}
+		}
 		/**
 		 * Legacy code, compatibility with old pro version.
 		 */
