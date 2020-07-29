5ba913c92d954a7139656ec1d5e5dd17d9ea887d
cristian-ungureanu <cristian@themeisle.com>
2020-07-29 20:03:58 +0300

    chore: add shortcut for sidebar

--- inc/admin/metabox/src/components/Sidebar.js
+++ inc/admin/metabox/src/components/Sidebar.js
@@ -2,9 +2,38 @@
 const { PluginSidebar, PluginSidebarMoreMenuItem } = wp.editPost;
 const { __ } = wp.i18n;
 
+
 import { MetaFieldsManager } from './MetaFieldsManager';
+const { useShortcut } = wp.keyboardShortcuts;
+const { useCallback } = wp.element;
 
 const Sidebar = () => {
+
+	wp.data.dispatch( 'core/keyboard-shortcuts' ).registerShortcut( {
+		name: 'neve/open-meta-sidebar',
+		category: 'block',
+		description: __( 'Open Neve meta sidebar', 'neve' ),
+		keyCombination: {
+			modifier: 'ctrl',
+			character: 'n'
+		}
+	} );
+
+	useShortcut(
+		'neve/open-meta-sidebar',
+		useCallback(
+			() => {
+				const sidebarStatus = wp.data.select( 'core/edit-post' ).isPluginSidebarOpened( 'meta-sidebar/neve-meta-sidebar' );
+				if ( true === sidebarStatus ) {
+					wp.data.dispatch( 'core/edit-post' ).closeGeneralSidebar( 'meta-sidebar/neve-meta-sidebar' );
+				} else {
+					wp.data.dispatch( 'core/edit-post' ).openGeneralSidebar( 'meta-sidebar/neve-meta-sidebar' );
+				}
+			},
+			[]
+		)
+	);
+
 	return (
 		<>
 			<PluginSidebarMoreMenuItem
