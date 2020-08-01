d2c64ee9b86cfc08e7fe9f4ea4b70ccb210047ae
selul <marius.cristea@vertistudio.com>
2020-08-01 18:08:21 +0300

    optimize e2e

--- bin/install-theme-check.sh
+++ bin/install-theme-check.sh
@@ -1,4 +1,5 @@
 php -d memory_limit=1024M "$(which wp)" package install anhskohbo/wp-cli-themecheck --allow-root
 wp  --allow-root core install --url=http://localhost:8080 --title=SandboxSite --admin_user=admin --admin_password=admin --admin_email=admin@admin.com
 wp plugin install theme-check --activate --allow-root
+wp --allow-root theme install /var/www/html/wp-content/neve.zip
 wp themecheck --theme=neve --no-interactive --allow-root
