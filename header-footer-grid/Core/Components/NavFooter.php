4d47df557ea55635af01fd990377218f921655ed
abaicus <baicusandrei@me.com>
2020-08-18 16:21:04 +0300

    chore: smaller control titles for min instead of minimum

--- header-footer-grid/Core/Components/NavFooter.php
+++ header-footer-grid/Core/Components/NavFooter.php
@@ -174,7 +174,7 @@ class NavFooter extends Abstract_Component {
 				'id'                 => self::ITEM_HEIGHT,
 				'group'              => $this->get_class_const( 'COMPONENT_ID' ),
 				'tab'                => SettingsManager::TAB_LAYOUT,
-				'label'              => __( 'Items Minimum Height (px)', 'neve' ),
+				'label'              => __( 'Items Min Height (px)', 'neve' ),
 				'sanitize_callback'  => [ $this, 'sanitize_responsive_int_json' ],
 				'transport'          => 'post' . $this->get_class_const( 'COMPONENT_ID' ),
 				'default'            => $this->get_default_for_responsive_from_intval( self::ITEM_HEIGHT, 25 ),
