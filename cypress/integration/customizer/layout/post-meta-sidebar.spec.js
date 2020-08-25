36fe03f043f34a4f04b081dfb4b3b385a8fb532f
cristian-ungureanu <cristian@themeisle.com>
2020-08-26 01:21:52 +0300

    chore: add cypress test

--- cypress/integration/customizer/layout/post-meta-sidebar.spec.js
+++ cypress/integration/customizer/layout/post-meta-sidebar.spec.js
@@ -12,69 +12,69 @@ describe('Single post meta sidebar', function () {
 		});
 	});
 
-	// it('Default meta box settings on front end.', function () {
-	// 	cy.visit(postSetup.url);
-	//
-	// 	cy.get('.nv-sidebar-wrap').should('have.class', 'nv-right').and('be.visible');
-	// 	cy.get('.single-post-container').should('not.have.class', 'container-fluid').and('be.visible');
-	// 	cy.get('.nv-single-post-wrap').should('have.css', 'max-width').and('eq', '70%');
-	// 	cy.get('.entry-title').should('exist').and('contain', postSetup.title);
-	// 	cy.get('.hfg_header').should('exist');
-	// 	cy.get('footer.site-footer').should('exist');
-	// 	cy.get('.nv-thumb-wrap > img').should('exist').and('be.visible').and('have.attr', 'src').then(($href) => {
-	// 		expect($href).to.contain('/wp-content/uploads/');
-	// 	});
-	// 	cy.get('.nv-content-wrap').should('contain', postSetup.content);
-	// });
-	//
-	// it('Check sidebar layout', function () {
-	// 	cy.login(postSetup.url);
-	// 	cy.get('#wp-admin-bar-edit a').click();
-	// 	cy.clearWelcome();
-	//
-	// 	openNeveSidebar();
-	//
-	// 	let sidebarControl = getControl('neve_meta_sidebar');
-	// 	sidebarControl.find('.components-radio-control__input[value="full-width"]').parent().click();
-	// 	cy.updatePost();
-	// 	cy.visit(postSetup.url);
-	// 	cy.get('.nv-sidebar-wrap').should('not.exist');
-	// 	cy.get('#wp-admin-bar-edit a').click();
-	//
-	// 	sidebarControl = getControl('neve_meta_sidebar');
-	// 	sidebarControl.find('.components-radio-control__input[value="left"]').parent().click();
-	// 	cy.updatePost();
-	// 	cy.visit(postSetup.url);
-	// 	cy.get('.nv-sidebar-wrap').should('exist').and('have.class', 'nv-left');
-	// 	cy.get('#wp-admin-bar-edit a').click();
-	//
-	// 	sidebarControl = getControl('neve_meta_sidebar');
-	// 	sidebarControl.find('.components-radio-control__input[value="right"]').parent().click();
-	// 	cy.updatePost();
-	// 	cy.visit(postSetup.url);
-	// 	cy.get('.nv-sidebar-wrap').should('exist').and('have.class', 'nv-right');
-	// });
-	//
-	// it('Check container layout', function () {
-	// 	cy.login(postSetup.url);
-	// 	cy.get('#wp-admin-bar-edit a').click();
-	// 	cy.clearWelcome();
-	//
-	// 	openNeveSidebar();
-	//
-	// 	let sidebarControl = getControl('neve_meta_container');
-	// 	sidebarControl.find('.components-button').contains('Contained').click();
-	// 	cy.updatePost();
-	// 	cy.visit(postSetup.url);
-	// 	cy.get('.single-post-container').should('have.class', 'container').and('be.visible');
-	// 	cy.get('#wp-admin-bar-edit a').click();
-	//
-	// 	sidebarControl = getControl('neve_meta_container');
-	// 	sidebarControl.find('.components-button').contains('Full Width').click();
-	// 	cy.updatePost();
-	// 	cy.visit(postSetup.url);
-	// 	cy.get('.single-post-container').should('not.have.class', 'container').and('be.visible');
-	// });
+	it('Default meta box settings on front end.', function () {
+		cy.visit(postSetup.url);
+
+		cy.get('.nv-sidebar-wrap').should('have.class', 'nv-right').and('be.visible');
+		cy.get('.single-post-container').should('not.have.class', 'container-fluid').and('be.visible');
+		cy.get('.nv-single-post-wrap').should('have.css', 'max-width').and('eq', '70%');
+		cy.get('.entry-title').should('exist').and('contain', postSetup.title);
+		cy.get('.hfg_header').should('exist');
+		cy.get('footer.site-footer').should('exist');
+		cy.get('.nv-thumb-wrap > img').should('exist').and('be.visible').and('have.attr', 'src').then(($href) => {
+			expect($href).to.contain('/wp-content/uploads/');
+		});
+		cy.get('.nv-content-wrap').should('contain', postSetup.content);
+	});
+
+	it('Check sidebar layout', function () {
+		cy.login(postSetup.url);
+		cy.get('#wp-admin-bar-edit a').click();
+		cy.clearWelcome();
+
+		openNeveSidebar();
+
+		let sidebarControl = getControl('neve_meta_sidebar');
+		sidebarControl.find('.components-radio-control__input[value="full-width"]').parent().click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('.nv-sidebar-wrap').should('not.exist');
+		cy.get('#wp-admin-bar-edit a').click();
+
+		sidebarControl = getControl('neve_meta_sidebar');
+		sidebarControl.find('.components-radio-control__input[value="left"]').parent().click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('.nv-sidebar-wrap').should('exist').and('have.class', 'nv-left');
+		cy.get('#wp-admin-bar-edit a').click();
+
+		sidebarControl = getControl('neve_meta_sidebar');
+		sidebarControl.find('.components-radio-control__input[value="right"]').parent().click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('.nv-sidebar-wrap').should('exist').and('have.class', 'nv-right');
+	});
+
+	it('Check container layout', function () {
+		cy.login(postSetup.url);
+		cy.get('#wp-admin-bar-edit a').click();
+		cy.clearWelcome();
+
+		openNeveSidebar();
+
+		let sidebarControl = getControl('neve_meta_container');
+		sidebarControl.find('.components-button').contains('Contained').click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('.single-post-container').should('have.class', 'container').and('be.visible');
+		cy.get('#wp-admin-bar-edit a').click();
+
+		sidebarControl = getControl('neve_meta_container');
+		sidebarControl.find('.components-button').contains('Full Width').click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('.single-post-container').should('not.have.class', 'container').and('be.visible');
+	});
 
 	it('Check container width', function () {
 		cy.login(postSetup.url);
@@ -92,98 +92,98 @@ describe('Single post meta sidebar', function () {
 
 		cy.get('.nv-single-post-wrap').should('have.css', 'max-width').and('eq', '60%');
 	})
-	//
-	// it('Check title alignment', function () {
-	// 	cy.login(postSetup.url);
-	// 	cy.get('#wp-admin-bar-edit a').click();
-	// 	cy.clearWelcome();
-	//
-	// 	openNeveSidebar();
-	//
-	// 	cy.get('.neve_meta_title_alignment .nv-align-center').click();
-	// 	cy.updatePost();
-	// 	cy.visit(postSetup.url);
-	// 	cy.get('h1.entry-title').should('have.class', 'has-text-align-center').and('have.css', 'text-align').and('eq', 'center');
-	// 	cy.get('#wp-admin-bar-edit a').click();
-	//
-	// 	cy.get('.neve_meta_title_alignment .nv-align-right').click();
-	// 	cy.updatePost();
-	// 	cy.visit(postSetup.url);
-	// 	cy.get('h1.entry-title').should('have.class', 'has-text-align-right').and('have.css', 'text-align').and('eq', 'right');
-	// 	cy.get('#wp-admin-bar-edit a').click();
-	//
-	// 	cy.get('.neve_meta_title_alignment .nv-align-left').click();
-	// 	cy.updatePost();
-	// 	cy.visit(postSetup.url);
-	// 	cy.get('h1.entry-title').should('have.class', 'has-text-align-left').and('have.css', 'text-align').and('eq', 'left');
-	// 	cy.get('#wp-admin-bar-edit a').click();
-	// });
-	//
-	// it('Check author avatar', function () {
-	// 	cy.login(postSetup.url);
-	// 	cy.get('#wp-admin-bar-edit a').click();
-	// 	cy.clearWelcome();
-	//
-	// 	openNeveSidebar();
-	//
-	// 	const enableContentWidth = cy.get('.components-toggle-control__label').contains('Author Avatar');
-	// 	activateCheckbox(enableContentWidth);
-	// 	cy.updatePost();
-	//
-	// 	cy.visit(postSetup.url);
-	// 	cy.get('.nv-meta-list .author .photo').should('exist');
-	// });
-	//
-	// it('Check post elements', function () {
-	// 	cy.login(postSetup.url);
-	// 	cy.get('#wp-admin-bar-edit a').click();
-	// 	cy.clearWelcome();
-	//
-	// 	openNeveSidebar();
-	//
-	// 	toggleElements(false);
-	// 	cy.get('.components-toggle-control__label').contains('Author Avatar').should('not.exist');
-	// 	cy.updatePost();
-	// 	cy.visit(postSetup.url);
-	//
-	// 	cy.get('h1.title').should('not.exist');
-	// 	cy.get('.nv-thumb-wrap').should('not.exist');
-	// 	cy.get('.nv-tags-list').should('not.exist');
-	// 	cy.get('.nv-meta-list').should('not.exist');
-	// 	cy.get('.nv-content-wrap').should('not.exist');
-	// 	cy.get('#comments').should('not.exist');
-	// 	cy.get('.nv-post-navigation').should('not.exist');
-	//
-	// 	cy.get('#wp-admin-bar-edit a').click();
-	// 	toggleElements(true);
-	// 	cy.get('.components-toggle-control__label').contains('Author Avatar').should('exist');
-	// 	cy.updatePost();
-	// 	cy.visit(postSetup.url);
-	//
-	// 	cy.get('h1.title').should('exist');
-	// 	cy.get('.nv-thumb-wrap').should('exist');
-	// 	cy.get('.nv-tags-list').should('exist');
-	// 	cy.get('.nv-meta-list').should('exist');
-	// 	cy.get('.nv-content-wrap').should('exist');
-	// 	cy.get('#comments').should('exist');
-	// 	cy.get('.nv-post-navigation').should('exist');
-	// 	cy.visit(postSetup.url);
-	//
-	// 	cy.get('#wp-admin-bar-edit a').click();
-	// 	const headerToggle = cy.get('.components-toggle-control__label').contains('Disable Header');
-	// 	activateCheckbox(headerToggle);
-	// 	cy.updatePost();
-	// 	cy.visit(postSetup.url);
-	// 	cy.get('.hfg_header').should('not.exist');
-	// 	cy.get('#wp-admin-bar-edit a').click();
-	//
-	// 	const footerToggle = cy.get('.components-toggle-control__label').contains('Disable Footer');
-	// 	activateCheckbox(footerToggle);
-	// 	cy.updatePost();
-	// 	cy.visit(postSetup.url);
-	// 	cy.get('footer.site-footer').should('not.exist');
-	//
-	// });
+
+	it('Check title alignment', function () {
+		cy.login(postSetup.url);
+		cy.get('#wp-admin-bar-edit a').click();
+		cy.clearWelcome();
+
+		openNeveSidebar();
+
+		cy.get('.neve_meta_title_alignment .nv-align-center').click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('h1.entry-title').should('have.class', 'has-text-align-center').and('have.css', 'text-align').and('eq', 'center');
+		cy.get('#wp-admin-bar-edit a').click();
+
+		cy.get('.neve_meta_title_alignment .nv-align-right').click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('h1.entry-title').should('have.class', 'has-text-align-right').and('have.css', 'text-align').and('eq', 'right');
+		cy.get('#wp-admin-bar-edit a').click();
+
+		cy.get('.neve_meta_title_alignment .nv-align-left').click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('h1.entry-title').should('have.class', 'has-text-align-left').and('have.css', 'text-align').and('eq', 'left');
+		cy.get('#wp-admin-bar-edit a').click();
+	});
+
+	it('Check author avatar', function () {
+		cy.login(postSetup.url);
+		cy.get('#wp-admin-bar-edit a').click();
+		cy.clearWelcome();
+
+		openNeveSidebar();
+
+		const enableContentWidth = cy.get('.components-toggle-control__label').contains('Author Avatar');
+		activateCheckbox(enableContentWidth);
+		cy.updatePost();
+
+		cy.visit(postSetup.url);
+		cy.get('.nv-meta-list .author .photo').should('exist');
+	});
+
+	it('Check post elements', function () {
+		cy.login(postSetup.url);
+		cy.get('#wp-admin-bar-edit a').click();
+		cy.clearWelcome();
+
+		openNeveSidebar();
+
+		toggleElements(false);
+		cy.get('.components-toggle-control__label').contains('Author Avatar').should('not.exist');
+		cy.updatePost();
+		cy.visit(postSetup.url);
+
+		cy.get('h1.title').should('not.exist');
+		cy.get('.nv-thumb-wrap').should('not.exist');
+		cy.get('.nv-tags-list').should('not.exist');
+		cy.get('.nv-meta-list').should('not.exist');
+		cy.get('.nv-content-wrap').should('not.exist');
+		cy.get('#comments').should('not.exist');
+		cy.get('.nv-post-navigation').should('not.exist');
+
+		cy.get('#wp-admin-bar-edit a').click();
+		toggleElements(true);
+		cy.get('.components-toggle-control__label').contains('Author Avatar').should('exist');
+		cy.updatePost();
+		cy.visit(postSetup.url);
+
+		cy.get('h1.title').should('exist');
+		cy.get('.nv-thumb-wrap').should('exist');
+		cy.get('.nv-tags-list').should('exist');
+		cy.get('.nv-meta-list').should('exist');
+		cy.get('.nv-content-wrap').should('exist');
+		cy.get('#comments').should('exist');
+		cy.get('.nv-post-navigation').should('exist');
+		cy.visit(postSetup.url);
+
+		cy.get('#wp-admin-bar-edit a').click();
+		const headerToggle = cy.get('.components-toggle-control__label').contains('Disable Header');
+		activateCheckbox(headerToggle);
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('.hfg_header').should('not.exist');
+		cy.get('#wp-admin-bar-edit a').click();
+
+		const footerToggle = cy.get('.components-toggle-control__label').contains('Disable Footer');
+		activateCheckbox(footerToggle);
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('footer.site-footer').should('not.exist');
+
+	});
 
 })
 
