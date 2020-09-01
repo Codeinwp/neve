b06efe080b67e49555d2f1be276990034b5446a0
selul <marius.cristea@vertistudio.com>
2020-09-01 17:35:42 +0300

    testing

--- bin/envs/amp/start.sh
+++ bin/envs/amp/start.sh
@@ -1,3 +1,4 @@
 wp --allow-root plugin is-installed amp && wp --allow-root plugin update amp ||  wp --allow-root plugin install amp
 wp --allow-root plugin activate amp
+wp --allow-root post generate --count=10
 wp --allow-root option patch update amp-options theme_support 'transitional'
