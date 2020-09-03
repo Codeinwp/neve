44b99c7a34059eb0e74d778e5370457b7e79c143
selul <marius.cristea@vertistudio.com>
2020-09-03 12:47:31 +0300

    another test

--- bin/envs/init.sh
+++ bin/envs/init.sh
@@ -20,7 +20,7 @@ docker-compose -f $DOCKER_FILE up -d
 
 # Wait for mysql container to be ready.
 while docker-compose -f $DOCKER_FILE run  --rm -u root cli wp --allow-root db check ; [ $? -ne 0 ];  do
-	  echo "Waiting for db to be ready ... "
+	  echo "Waiting for db to be ready... "
     sleep 1
 done
 
