37b708b175268241028fab61e15b74716dfd0d6e
selul <marius.cristea@vertistudio.com>
2020-09-03 10:36:29 +0300

    fix modern editor tests

---
+++ bin/envs/classic-editor/start.sh
@@ -0,0 +1,2 @@
+wp --allow-root plugin is-installed classic-editor && wp --allow-root plugin update classic-editor ||  wp --allow-root plugin install classic-editor
+wp --allow-root plugin activate classic-editor
