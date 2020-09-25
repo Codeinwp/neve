a45328128e04f6f92b7499edb7b3ac533099651c
selul <marius.cristea@vertistudio.com>
2020-09-25 17:32:12 +0300

    fix ss names for starter sites

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -212,7 +212,7 @@ function setTypographyControl( controlSelector, values ) {
  * Capture and compare the fullpage snapshots.
  *
  */
-Cypress.Commands.add("captureDocument", (generalMaskAndClip = true) => {
+Cypress.Commands.add("captureDocument", (generalMaskAndClip = true, screenShotName = false) => {
 	if(generalMaskAndClip){
 		let maskElement = '.elementor-element-31b9e15, .pikaday__display--pikaday, .elementor-widget-video, label[for="vscf_captcha"],.elementor-widget-google_maps,.pikaday__display,.elementor-video-iframe';
 		let clipElement = '.widget_top_rated_products, .wpcf7-quiz-label, .eaw-typed-text,.product .related.products,.captcha_img,.elementor-element-38f7ff1e,.elementor-widget-container[style*="will-change"], .particles-js-canvas-el,.product_list_widget li + li, .exclusive.products';
@@ -220,7 +220,7 @@ Cypress.Commands.add("captureDocument", (generalMaskAndClip = true) => {
 	}
 	cy.scrollTo('bottom',{ ensureScrollable: false });
 	cy.wait(2000);
-	cy.percySnapshot();
+	cy.percySnapshot(screenShotName);
 });
 /**
  * Capture and compare the fullpage snapshots.
