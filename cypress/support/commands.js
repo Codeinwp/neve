f977c9e290d1552f2d1e7ff050814f0507df4229
selul <marius.cristea@vertistudio.com>
2020-09-25 22:11:40 +0300

    fix scroll to bottom

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,6 @@
 import 'cypress-file-upload';
 import '@percy/cypress';
+let scrollToBottom = require("scroll-to-bottomjs");
 Cypress.Cookies.defaults({
 	preserve: /wordpress_.*/
 });
@@ -218,8 +219,8 @@ Cypress.Commands.add("captureDocument", (generalMaskAndClip = true, screenShotNa
 		let clipElement = '.widget_top_rated_products, .wpcf7-quiz-label, .eaw-typed-text,.product .related.products,.captcha_img,.elementor-element-38f7ff1e,.elementor-widget-container[style*="will-change"], .particles-js-canvas-el,.product_list_widget li + li, .exclusive.products';
 		cy.maskAndClip(maskElement,clipElement);
 	}
-	cy.scrollTo('bottom',{ ensureScrollable: false });
-	cy.wait(2000);
+	cy.window().then(cyWindow => scrollToBottom({ remoteWindow: cyWindow }));
+	cy.wait(5000);
 	cy.percySnapshot(screenShotName);
 });
 /**
