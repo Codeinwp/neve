558639c0046033d14f1f65d90325ebc6d2f30ff2
abaicus <baicusandrei@me.com>
2020-10-08 13:48:16 +0300

    chore: fix rtlcss generation on build

--- inc/customizer/controls/react/bundle/controls.asset.php
+++ inc/customizer/controls/react/bundle/controls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '878cfe27df38cfa1f9b24b820a80ba89');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '5801309cd5a3dc755fa603f15381a2f4');
