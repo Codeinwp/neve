68007893ab2b1a786e5dc579859b78ffbc6ec3ac
cristian-ungureanu <cristian@themeisle.com>
2020-10-09 15:09:44 +0300

    chore: fix underlined widgets in elementor

--- inc/customizer/controls/react/bundle/controls.asset.php
+++ inc/customizer/controls/react/bundle/controls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '5801309cd5a3dc755fa603f15381a2f4');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'be86494d7564b6edf3987bb05c20f5eb');
