012fd11d59cb277e31f3d37f91289381adfcfe9e
abaicus <baicusandrei@me.com>
2020-10-12 10:56:41 +0300

    build: scripts

--- inc/customizer/controls/react/bundle/controls.asset.php
+++ inc/customizer/controls/react/bundle/controls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '8aca5f514bbd0c1ad93cf9d1946dd316');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '5801309cd5a3dc755fa603f15381a2f4');
