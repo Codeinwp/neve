b112af895c5671f5e08ccb972c40253a532700a8
abaicus <baicusandrei@me.com>
2020-11-16 11:21:56 +0200

    chore: adds documentation link for global colors

--- inc/customizer/controls/react/bundle/controls.asset.php
+++ inc/customizer/controls/react/bundle/controls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '5f7e7aef460b418533e769059eec49f3');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '2bfcf6f973541575496ebaf36e9190b4');
