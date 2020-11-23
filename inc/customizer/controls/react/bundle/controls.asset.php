d196aa4ec639a084dfaf4214e0f660d62902d0fa
abaicus <baicusandrei@me.com>
2020-11-23 11:26:16 +0200

    chore: rtlcss

--- inc/customizer/controls/react/bundle/controls.asset.php
+++ inc/customizer/controls/react/bundle/controls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '3fd500605feb8154f641627de20ad936');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '2bfcf6f973541575496ebaf36e9190b4');
