95b22879bb1abfdf3ab65e8397136bc5e096a129
abaicus <baicusandrei@me.com>
2020-12-03 12:03:29 +0200

    chore: fix issue with old core background color applying to body

--- inc/core/front_end.php
+++ inc/core/front_end.php
@@ -61,7 +61,6 @@ class Front_End {
 		add_filter( 'embed_oembed_html', array( $this, 'wrap_oembeds' ), 10, 3 );
 		add_filter( 'video_embed_html', array( $this, 'wrap_jetpack_oembeds' ), 10, 1 );
 		add_filter( 'themeisle_gutenberg_templates', array( $this, 'add_gutenberg_templates' ) );
-		add_filter( 'theme_mod_background_color', '__return_empty_string' );
 
 		$this->add_amp_support();
 		$nav_menus_to_register = apply_filters(
