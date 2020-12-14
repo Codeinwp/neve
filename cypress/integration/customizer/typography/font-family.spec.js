53d709fc25670cbdd1226d45909e5977041dd773
abaicus <baicusandrei@me.com>
2020-12-14 16:00:55 +0200

    chore: fix font-family.spec.js

--- cypress/integration/customizer/typography/font-family.spec.js
+++ cypress/integration/customizer/typography/font-family.spec.js
@@ -1,125 +1,113 @@
 const fonts = {
 	general: 'Arapey',
-	headings: 'Allerta Stencil'
+	headings: 'Allerta Stencil',
 };
 
 function aliasRestRoutes() {
-	let home = Cypress.config().baseUrl;
-	cy.server().
-			route( 'POST', home + '/wp-admin/admin-ajax.php' ).
-			as( 'customizerSave' );
+	const home = Cypress.config().baseUrl;
+	cy.server()
+		.route( 'POST', home + '/wp-admin/admin-ajax.php' )
+		.as( 'customizerSave' );
 }
 
 /**
  * Set font family inside i
+ *
  * @param controlSelector
  * @param fontFamily
  */
-function setFontFamilyControl(controlSelector, fontFamily) {
-	cy.get( controlSelector ).
-			as( 'control' );
+function setFontFamilyControl( controlSelector, fontFamily ) {
+	cy.get( controlSelector ).as( 'control' );
 
-	cy.get( '@control' ).
-			should( 'be.visible' ).
-			and( 'contain', 'Abc' ).
-			and( 'contain', 'Font Family' );
+	cy.get( '@control' )
+		.should( 'be.visible' )
+		.and( 'contain', 'Abc' )
+		.and( 'contain', 'Font Family' );
 
-	cy.get( '@control' ).
-			find( '.neve-typeface-control button' ).
-			click();
+	cy.get( '@control' ).find( '.neve-typeface-control button' ).click();
 
-	cy.get( '@control' ).
-			find( '.popover-content' ).
-			should( 'be.visible' );
+	cy.get( '@control' ).find( '.popover-content' ).should( 'be.visible' );
 
 	// Scroll popover to bottom and make sure that we're seeing load more.
-	cy.get( '@control' ).find( '.neve-fonts-list li' ).then( $el => {
-		const count = $el.length;
-		cy.get( '@control' ).find( '.neve-fonts-list' ).scrollTo( 'bottom' );
-		cy.get( '@control' ).find( 'li.load-more' ).should( 'be.visible' );
-		// Wait to load more.
-		cy.wait( 2000 );
-		// Make sure we have more fonts loaded.
-		cy.get( '@control' ).
-				find( '.neve-fonts-list li' ).
-				its( 'length' ).
-				should( 'be.gte', count );
-	} );
+	cy.get( '@control' )
+		.find( '.neve-fonts-list li' )
+		.then( ( $el ) => {
+			const count = $el.length;
+			cy.get( '@control' )
+				.find( '.neve-fonts-list' )
+				.scrollTo( 'bottom' );
+			cy.get( '@control' ).find( 'li.load-more' ).should( 'be.visible' );
+			// Wait to load more.
+			cy.wait( 2000 );
+			// Make sure we have more fonts loaded.
+			cy.get( '@control' )
+				.find( '.neve-fonts-list li' )
+				.its( 'length' )
+				.should( 'be.gte', count );
+		} );
 	// Search for fontFamily.
-	cy.get( '@control' ).
-			find( '.search .components-text-control__input' ).
-			type( fontFamily );
+	cy.get( '@control' )
+		.find( '.search .components-text-control__input' )
+		.type( fontFamily );
 
 	// Check that we found exactly 3 fonts.
-	cy.get( '@control' ).
-			find( '.neve-fonts-list li a' ).
-			should( 'have.length', 2 ).
-			and( 'contain', fontFamily );
+	cy.get( '@control' )
+		.find( '.neve-fonts-list li a' )
+		.should( 'have.length', 2 )
+		.and( 'contain', fontFamily );
 
 	// Click on fontFamily font.
-	cy.get( '@control' ).
-			find( '.neve-fonts-list li a' ).
-			contains( fontFamily ).
-			click();
+	cy.get( '@control' )
+		.find( '.neve-fonts-list li a' )
+		.contains( fontFamily )
+		.click();
 }
 
-describe( 'Font Family', function() {
-	const typographyPanel = 'neve_typography'
-	const generalSection = 'neve_typography_general'
-	const headingsSection = 'neve_typography_headings'
-
-	it( 'Sets up customizer Font Family', function() {
-		cy.login();
-		cy.visit( '/wp-admin/customize.php' );
-
-		// Setup general.
-		cy.window().then((win) => {
-			win.wp.customize.panel(typographyPanel).focus()
-			win.wp.customize.section(generalSection).focus()
-		});
-		setFontFamilyControl( '#customize-control-neve_body_font_family', fonts.general );
-
-
-		cy.window().then((win) => {
-			win.wp.customize.panel(typographyPanel).focus()
-			win.wp.customize.section(headingsSection).focus()
-		});
-		setFontFamilyControl( '#customize-control-neve_headings_font_family', fonts.headings );
-
-		aliasRestRoutes();
-		cy.get( '#save' ).click();
-		cy.wait( '@customizerSave' ).then( (req) => {
-			expect( req.response.body.success ).to.be.true;
-			expect( req.status ).to.equal( 200 );
-			cy.wait( 2000 );
+describe( 'Font Family', function () {
+	const BEFORE = () => {
+		cy.setCustomizeSettings( {
+			neve_body_font_family: fonts.general,
+			neve_headings_font_family: fonts.headings,
 		} );
-	} );
+	};
 
-	it( 'Test Font Family on Front End', function() {
+	const AFTER = () => {
+		cy.setCustomizeSettings( {
+			neve_body_font_family: '',
+			neve_headings_font_family: '',
+		} );
+	};
+
+	before( () => BEFORE() );
+	after( () => AFTER() );
+
+	it( 'Font Family on Front End', function () {
 		cy.visit( '/markup-html-tags-and-formatting/' );
 
-		cy.get( 'body' ).
-				should( 'have.css', 'font-family' ).
-				and( 'match', new RegExp( fonts.general, 'g' ) );
+		cy.get( 'body' )
+			.should( 'have.css', 'font-family' )
+			.and( 'match', new RegExp( fonts.general, 'g' ) );
 
-		cy.get( 'h1, h2, h3, h4, h5, h6' ).
-				should( 'have.css', 'font-family' ).
-				and( 'match', new RegExp( fonts.headings, 'g' ) );
+		cy.get( 'h1, h2, h3, h4, h5, h6' )
+			.should( 'have.css', 'font-family' )
+			.and( 'match', new RegExp( fonts.headings, 'g' ) );
 	} );
 
-	it( 'Test Font Family inside the Editor', function() {
+	it( 'Test Font Family inside the Editor', function () {
 		cy.visit( '/markup-html-tags-and-formatting/' );
 		cy.get( '#wp-admin-bar-edit > a' ).click();
 		cy.clearWelcome();
-		cy.get( '.editor-styles-wrapper .block-editor-writing-flow p' ).
-				as( 'body' );
-		cy.get( '@body' ).should( 'have.css', 'font-family' ).
-				and( 'match', new RegExp( fonts.general, 'g' ) );
+		cy.get( '.editor-styles-wrapper .block-editor-writing-flow p' ).as(
+			'body'
+		);
+		cy.get( '@body' )
+			.should( 'have.css', 'font-family' )
+			.and( 'match', new RegExp( fonts.general, 'g' ) );
 
-		['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].forEach( function(heading) {
-			cy.get( `.editor-styles-wrapper ${heading}` ).
-					should( 'have.css', 'font-family' ).
-					and( 'match', new RegExp( fonts.headings, 'g' ) );
+		[ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ].forEach( function ( heading ) {
+			cy.get( `.editor-styles-wrapper ${ heading }` )
+				.should( 'have.css', 'font-family' )
+				.and( 'match', new RegExp( fonts.headings, 'g' ) );
 		} );
 	} );
 } );
