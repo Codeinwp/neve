b9434964b777fe6e27daaf9cd62345c90cfaf6e1
abaicus <baicusandrei@me.com>
2020-12-14 17:23:42 +0200

    chore: fix font-sizes.spec.js

--- cypress/integration/customizer/typography/font-family.spec.js
+++ cypress/integration/customizer/typography/font-family.spec.js
@@ -3,66 +3,6 @@ const fonts = {
 	headings: 'Allerta Stencil',
 };
 
-function aliasRestRoutes() {
-	const home = Cypress.config().baseUrl;
-	cy.server()
-		.route( 'POST', home + '/wp-admin/admin-ajax.php' )
-		.as( 'customizerSave' );
-}
-
-/**
- * Set font family inside i
- *
- * @param controlSelector
- * @param fontFamily
- */
-function setFontFamilyControl( controlSelector, fontFamily ) {
-	cy.get( controlSelector ).as( 'control' );
-
-	cy.get( '@control' )
-		.should( 'be.visible' )
-		.and( 'contain', 'Abc' )
-		.and( 'contain', 'Font Family' );
-
-	cy.get( '@control' ).find( '.neve-typeface-control button' ).click();
-
-	cy.get( '@control' ).find( '.popover-content' ).should( 'be.visible' );
-
-	// Scroll popover to bottom and make sure that we're seeing load more.
-	cy.get( '@control' )
-		.find( '.neve-fonts-list li' )
-		.then( ( $el ) => {
-			const count = $el.length;
-			cy.get( '@control' )
-				.find( '.neve-fonts-list' )
-				.scrollTo( 'bottom' );
-			cy.get( '@control' ).find( 'li.load-more' ).should( 'be.visible' );
-			// Wait to load more.
-			cy.wait( 2000 );
-			// Make sure we have more fonts loaded.
-			cy.get( '@control' )
-				.find( '.neve-fonts-list li' )
-				.its( 'length' )
-				.should( 'be.gte', count );
-		} );
-	// Search for fontFamily.
-	cy.get( '@control' )
-		.find( '.search .components-text-control__input' )
-		.type( fontFamily );
-
-	// Check that we found exactly 3 fonts.
-	cy.get( '@control' )
-		.find( '.neve-fonts-list li a' )
-		.should( 'have.length', 2 )
-		.and( 'contain', fontFamily );
-
-	// Click on fontFamily font.
-	cy.get( '@control' )
-		.find( '.neve-fonts-list li a' )
-		.contains( fontFamily )
-		.click();
-}
-
 describe( 'Font Family', function () {
 	const BEFORE = () => {
 		cy.setCustomizeSettings( {
