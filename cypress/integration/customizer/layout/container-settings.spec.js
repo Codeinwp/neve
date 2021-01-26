774afeed04f6b88bd7702ecb428b4c08274edbb3
Augusto Silva <guto_slv@live.com>
2021-01-26 00:45:53 -0300

    refactor(tests): refactors customizer layout specs
    - Refactors set up functions into a before hook
    - Removes unnecessary after hooks

--- cypress/integration/customizer/layout/container-settings.spec.js
+++ cypress/integration/customizer/layout/container-settings.spec.js
@@ -1,54 +1,53 @@
-describe('Container Settings', function () {
-
+/* eslint-disable array-callback-return */
+describe('Container Settings', () => {
 	const setup = {
-		'pageTitle': 'Container on Page',
-		'postTitle': 'Container on Post',
-		'content': 'Container Test Content',
-		'pageUrl': null,
-		'postUrl': null
+		pageTitle: 'Container on Page',
+		postTitle: 'Container on Post',
+		content: 'Container Test Content',
+		pageUrl: null,
+		postUrl: null,
 	};
 
-	it('Create new page named "' + setup.pageTitle + '".', function () {
+	it('Create new page named "' + setup.pageTitle + '".', () => {
 		cy.insertPost(setup.pageTitle, setup.content, 'page');
-		cy.get('.post-publish-panel__postpublish-header a').
-		contains(setup.pageTitle).
-		should('have.attr', 'href').
-		then((href) => {
-			setup.pageUrl = href;
-		});
+		cy.get('.post-publish-panel__postpublish-header a')
+			.contains(setup.pageTitle)
+			.should('have.attr', 'href')
+			.then((href) => {
+				setup.pageUrl = href;
+			});
 	});
 
-	it('Create new post named "' + setup.postTitle + '".', function () {
+	it('Create new post named "' + setup.postTitle + '".', () => {
 		cy.insertPost(setup.postTitle, setup.content, 'post', true);
-		cy.get('.post-publish-panel__postpublish-header a').
-		contains(setup.postTitle).
-		should('have.attr', 'href').
-		then((href) => {
-			setup.postUrl = href;
-		});
+		cy.get('.post-publish-panel__postpublish-header a')
+			.contains(setup.postTitle)
+			.should('have.attr', 'href')
+			.then((href) => {
+				setup.postUrl = href;
+			});
 	});
 
-	it('Setup customizer container width.', function () {
-
+	it('Setup customizer container width.', () => {
 		cy.goToCustomizer();
 		cy.aliasRestRoutes();
 		cy.get('#accordion-panel-neve_layout').click();
 		cy.get('#accordion-section-neve_container').click();
 		cy.get('#customize-control-neve_container_width').as('control');
 
-		let devices = {
-			'desktop': 1200,
-			'tablet': 960,
-			'mobile': 680
+		const devices = {
+			desktop: 1200,
+			tablet: 960,
+			mobile: 680,
 		};
 		Object.keys(devices).map((device) => {
-			cy.get('@control').
-			find('button.' + device).
-			click();
-			cy.get('@control').
-			find('input[type=number]').
-			type('{selectall}').
-			type(devices[device]);
+			cy.get('@control')
+				.find('button.' + device)
+				.click();
+			cy.get('@control')
+				.find('input[type=number]')
+				.type('{selectall}')
+				.type(devices[device]);
 		});
 
 		cy.get('#save').click();
@@ -58,33 +57,34 @@ describe('Container Settings', function () {
 		});
 	});
 
-	it('Container width on front end.', function () {
+	it('Container width on front end.', () => {
 		cy.visit(setup.pageUrl);
 		cy.get('.single-page-container').as('container');
-		cy.get('@container').should('have.css', 'max-width').
-		and('eq', '1200px');
+		cy.get('@container')
+			.should('have.css', 'max-width')
+			.and('eq', '1200px');
 
 		cy.viewport(768, 1024); //iPad
-		cy.get('@container').should('have.css', 'max-width').
-		and('eq', '960px');
+		cy.get('@container').should('have.css', 'max-width').and('eq', '960px');
 
 		cy.viewport(375, 812); //iPhoneX
-		cy.get('@container').should('have.css', 'max-width').
-		and('eq', '680px');
+		cy.get('@container').should('have.css', 'max-width').and('eq', '680px');
 	});
 
-	it('Setup customizer container style.', function () {
-
+	it('Setup customizer container style.', () => {
 		cy.goToCustomizer();
 		cy.aliasRestRoutes();
 		cy.get('#accordion-panel-neve_layout').click();
 		cy.get('#accordion-section-neve_container').click();
-		cy.get('#_customize-input-neve_default_container_style').
-		select('full-width');
-		cy.get('#_customize-input-neve_blog_archive_container_style').
-		select('full-width');
-		cy.get('#_customize-input-neve_single_post_container_style').
-		select('full-width');
+		cy.get('#_customize-input-neve_default_container_style').select(
+			'full-width'
+		);
+		cy.get('#_customize-input-neve_blog_archive_container_style').select(
+			'full-width'
+		);
+		cy.get('#_customize-input-neve_single_post_container_style').select(
+			'full-width'
+		);
 		cy.get('#save').click();
 		cy.wait('@customizerSave').then((req) => {
 			expect(req.response.body.success).to.be.true;
@@ -92,7 +92,7 @@ describe('Container Settings', function () {
 		});
 	});
 
-	it('Container style on front end.', function () {
+	it('Container style on front end.', () => {
 		cy.visit(setup.pageUrl);
 		cy.get('.single-page-container').as('container');
 		cy.get('@container').should('have.class', 'container-fluid');
@@ -101,33 +101,34 @@ describe('Container Settings', function () {
 		cy.get('@container').should('have.class', 'container-fluid');
 	});
 
-
-	it('Go back to defaults.', function () {
-
+	it('Go back to defaults.', () => {
 		cy.goToCustomizer();
 		cy.aliasRestRoutes();
 		cy.get('#accordion-panel-neve_layout').click();
 		cy.get('#accordion-section-neve_container').click();
 		cy.get('#customize-control-neve_container_width').as('control');
-		cy.get('#_customize-input-neve_default_container_style').
-		select('contained');
-		cy.get('#_customize-input-neve_blog_archive_container_style').
-		select('contained');
-		cy.get('#_customize-input-neve_single_post_container_style').
-		select('contained');
+		cy.get('#_customize-input-neve_default_container_style').select(
+			'contained'
+		);
+		cy.get('#_customize-input-neve_blog_archive_container_style').select(
+			'contained'
+		);
+		cy.get('#_customize-input-neve_single_post_container_style').select(
+			'contained'
+		);
 
 		cy.get('#customize-control-neve_container_width').as('control');
 
-		let devices = ['desktop', 'tablet', 'mobile'];
+		const devices = ['desktop', 'tablet', 'mobile'];
 		devices.map((device) => {
-			cy.get('@control').
-			find('button.' + device).
-			click();
-			cy.get('@control').
-			find('button').
-			contains('Reset').
-			should('be.visible').
-			click();
+			cy.get('@control')
+				.find('button.' + device)
+				.click();
+			cy.get('@control')
+				.find('button')
+				.contains('Reset')
+				.should('be.visible')
+				.click();
 		});
 		cy.get('#save').click();
 		cy.wait('@customizerSave').then((req) => {
