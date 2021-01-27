8e9ff4137240487b1e9aa2d9e1b485f57c982749
Augusto Silva <guto_slv@live.com>
2021-01-26 22:14:43 -0300

    style: fix dashboard specs style to the ESLint standard

--- cypress/integration/dashboard/general.spec.js
+++ cypress/integration/dashboard/general.spec.js
@@ -1,48 +1,59 @@
+/* eslint-disable array-callback-return */
 describe('General UI', () => {
-	const TABS = ['start', 'plugins', 'help', 'changelog', 'free-pro', 'starter-sites']
+	const TABS = [
+		'start',
+		'plugins',
+		'help',
+		'changelog',
+		'free-pro',
+		'starter-sites',
+	];
 
 	beforeEach(() => {
-		cy.login()
-		cy.visit('/wp-admin/themes.php?page=neve-welcome')
-		cy.server().route('GET', '/wp-json/wp/v2/settings/').as('getSettings')
-		cy.server().route('POST', '/wp-json/wp/v2/settings/').as('saveSettings')
-	})
+		cy.login();
+		cy.visit('/wp-admin/themes.php?page=neve-welcome');
+		cy.server().route('GET', '/wp-json/wp/v2/settings/').as('getSettings');
+		cy.server()
+			.route('POST', '/wp-json/wp/v2/settings/')
+			.as('saveSettings');
+	});
 
 	it('Placeholder Loading', () => {
-		cy.get('.mock-dash').should('exist.and.be.visible')
+		cy.get('.mock-dash').should('exist.and.be.visible');
 		cy.wait('@getSettings').then((req) => {
-			cy.get('.mock-dash').should('not.exist.and.be.visible')
-			expect(req.status).to.equal(200)
-		})
-	})
+			cy.get('.mock-dash').should('not.exist.and.be.visible');
+			expect(req.status).to.equal(200);
+		});
+	});
 
 	it('Test Logger Flag', () => {
-		cy.get('.sidebar-section input').as('loggerToggle')
-		cy.get('@loggerToggle').should('not.be.checked')
-		cy.get('@loggerToggle').check()
+		cy.get('.sidebar-section input').as('loggerToggle');
+		cy.get('@loggerToggle').should('not.be.checked');
+		cy.get('@loggerToggle').check();
 		cy.wait('@saveSettings').then((req) => {
-			expect(req.status).to.equal(200)
-			expect(req.response.body['neve_logger_flag']).to.equal('yes')
-		})
+			expect(req.status).to.equal(200);
+			expect(req.response.body.neve_logger_flag).to.equal('yes');
+		});
 
-		cy.reload()
-		cy.get('@loggerToggle').should('be.checked')
-
-	})
+		cy.reload();
+		cy.get('@loggerToggle').should('be.checked');
+	});
 
 	it('URL Hash Tab Focus', () => {
-		TABS.map(hash => {
-			cy.reload()
-			cy.visit(`/wp-admin/themes.php?page=neve-welcome#${hash}`)
-			cy.get(`nav.navigation a.${hash}`).should('have.class', 'active')
-			cy.get('.tab-content').should('have.class', hash)
-		})
-	})
+		TABS.map((hash) => {
+			cy.reload();
+			cy.visit(`/wp-admin/themes.php?page=neve-welcome#${hash}`);
+			cy.get(`nav.navigation a.${hash}`).should('have.class', 'active');
+			cy.get('.tab-content').should('have.class', hash);
+		});
+	});
 
 	it('Tabs Navigation', () => {
-		TABS.map(slug => {
-			cy.get(`nav.navigation a.${slug}`).click().should('have.class', 'active')
-			cy.get('.tab-content').should('have.class', slug)
-		})
-	})
-})
+		TABS.map((slug) => {
+			cy.get(`nav.navigation a.${slug}`)
+				.click()
+				.should('have.class', 'active');
+			cy.get('.tab-content').should('have.class', slug);
+		});
+	});
+});
--- cypress/integration/dashboard/useful-plugins.spec.js
+++ cypress/integration/dashboard/useful-plugins.spec.js
@@ -1,38 +1,43 @@
 describe('Useful Plugins Tab - Install Optimole', () => {
-
 	beforeEach(() => {
-		cy.login()
-		cy.visit('/wp-admin/themes.php?page=neve-welcome#plugins')
-		cy.server()
-		cy.route('POST', '/wp-admin/admin-ajax.php').as('adminAjax')
-		cy.route('POST', '/wp-admin/upload.php?page=optimole').as('optimoleActivation')
-		cy.get('.tab-content.plugins').as('pluginsTab')
-	})
+		cy.login();
+		cy.visit('/wp-admin/themes.php?page=neve-welcome#plugins');
+		cy.server();
+		cy.route('POST', '/wp-admin/admin-ajax.php').as('adminAjax');
+		cy.route('POST', '/wp-admin/upload.php?page=optimole').as(
+			'optimoleActivation'
+		);
+		cy.get('.tab-content.plugins').as('pluginsTab');
+	});
 
 	it('Installs Plugins', () => {
-		cy.get('@pluginsTab').find('.plugin.card').should('have.length.greaterThan', 4)
-		cy.get('@pluginsTab').find('.plugin.optimole-wp button').then((btn) => {
-			if (btn.text().includes('Install')) {
-				btn.trigger("click");
-				cy.wait('@adminAjax').then((req) => {
-					expect(req.status).to.equal(200)
-					expect(req.response.body.success).to.equal(true)
-				})
-			}
-		});
-	})
+		cy.get('@pluginsTab')
+			.find('.plugin.card')
+			.should('have.length.greaterThan', 4);
+		cy.get('@pluginsTab')
+			.find('.plugin.optimole-wp button')
+			.then((btn) => {
+				if (btn.text().includes('Install')) {
+					btn.trigger('click');
+					cy.wait('@adminAjax').then((req) => {
+						expect(req.status).to.equal(200);
+						expect(req.response.body.success).to.equal(true);
+					});
+				}
+			});
+	});
 
 	it('Activates Plugins', () => {
-		cy.get('@pluginsTab').find('.plugin.optimole-wp button').as('button')
-		cy.get('@button').contains('Activate').click()
-		cy.get('@button').should('contain', 'Deactivate')
+		cy.get('@pluginsTab').find('.plugin.optimole-wp button').as('button');
+		cy.get('@button').contains('Activate').click();
+		cy.get('@button').should('contain', 'Deactivate');
 
-		cy.reload()
-		cy.get('@button').should('contain', 'Deactivate').click()
-		cy.get('@button').should('contain', 'Activate')
+		cy.reload();
+		cy.get('@button').should('contain', 'Deactivate').click();
+		cy.get('@button').should('contain', 'Activate');
 
-		cy.reload()
-		cy.get('@button').should('contain', 'Activate').click()
-		cy.get('@button').should('contain', 'Deactivate')
-	})
-})
+		cy.reload();
+		cy.get('@button').should('contain', 'Activate').click();
+		cy.get('@button').should('contain', 'Deactivate');
+	});
+});
