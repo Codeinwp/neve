c7b16d99f1bf444f39251e52985cf66a52b31e99
Augusto Silva <guto_slv@live.com>
2021-01-26 22:55:14 -0300

    test: fix customizer hfg padding control spec

--- cypress/integration/customizer/hfg/hfg-padding-control.spec.js
+++ cypress/integration/customizer/hfg/hfg-padding-control.spec.js
@@ -2,13 +2,14 @@ describe('Header Builder Padding Control', () => {
 	before(() => {
 		// Login to wp and redirect to customizer.
 		cy.goToCustomizer();
+		cy.aliasRestRoutes();
 
 		// Open customizer panel.
 		cy.get('#accordion-panel-hfg_header').should('be.visible').click();
 
 		// Check if Logo and Site title component is visible and click it.
 		cy.get(
-			'.hfg--builder-show .hfg--panel-desktop .hfg--row-main .grid-stack-item[title="Logo & Site Identity"]',
+			'.hfg--builder-show .hfg--panel-desktop .hfg--row-main .grid-stack-item[title="Logo & Site Identity"]'
 		)
 			.should('be.visible')
 			.click();
@@ -25,47 +26,47 @@ describe('Header Builder Padding Control', () => {
 		cy.get('#customize-control-logo_component_padding .top-input')
 			.click()
 			.type(
-				'{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}',
+				'{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}'
 			)
 			.trigger('change');
 
 		// Check if linked values
-		// cy.get('#customize-control-logo_component_padding .top-input').should(
-		// 	'have.value',
-		// 	'30',
-		// );
-		// cy.get('#customize-control-logo_component_padding  .bottom-input').should(
-		// 	'have.value',
-		// 	'30',
-		// );
-		// cy.get('#customize-control-logo_component_padding .left-input').should(
-		// 	'have.value',
-		// 	'30',
-		// );
-		// cy.get('#customize-control-logo_component_padding .right-input').should(
-		// 	'have.value',
-		// 	'30',
-		// );
+		cy.get('#customize-control-logo_component_padding .top-input').should(
+			'have.value',
+			'30'
+		);
+		cy.get(
+			'#customize-control-logo_component_padding  .bottom-input'
+		).should('have.value', '30');
+		cy.get('#customize-control-logo_component_padding .left-input').should(
+			'have.value',
+			'30'
+		);
+		cy.get('#customize-control-logo_component_padding .right-input').should(
+			'have.value',
+			'30'
+		);
 
 		// Click on Reset button
-		cy.get('#customize-control-logo_component_padding button.reset').click();
+		cy.get(
+			'#customize-control-logo_component_padding button.reset'
+		).click();
 
 		// Check if Reset button works
 		cy.get('#customize-control-logo_component_padding .top-input').should(
 			'have.value',
-			'10',
+			'10'
 		);
 		cy.get('#customize-control-logo_component_padding .right-input').should(
 			'have.value',
-			'0',
+			'0'
 		);
-		cy.get('#customize-control-logo_component_padding .bottom-input').should(
-			'have.value',
-			'10',
-		);
+		cy.get(
+			'#customize-control-logo_component_padding .bottom-input'
+		).should('have.value', '10');
 		cy.get('#customize-control-logo_component_padding .left-input').should(
 			'have.value',
-			'0',
+			'0'
 		);
 
 		// Unlink values
@@ -87,11 +88,10 @@ describe('Header Builder Padding Control', () => {
 	});
 
 	it('Sets up Padding for the Logo Component', () => {
-		cy.aliasRestRoutes();
 		cy.wait('@customizerSave').then((req) => {
 			expect(req.response.body.success).to.be.true;
 			expect(req.status).to.equal(200);
-			checkFrontEnd();
+			cy.checkPaddingFrontEnd();
 		});
 	});
 });
