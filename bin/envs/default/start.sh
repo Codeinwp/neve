7675dbff24a9e8aed33af602c1f053d3b0f28280
Augusto Silva <guto_slv@live.com>
2021-01-29 01:03:02 -0300

    test: adds necessary plugins to default env
    - Adds template patterns collection to default env to set up the env for starter site import tests

--- bin/envs/default/start.sh
+++ bin/envs/default/start.sh
@@ -1,3 +1,5 @@
 #Generic setup
 wp --allow-root media import /tmp/repo/neve/cypress/fixtures/image.jpg
 wp --allow-root rewrite structure /%postname%/
+wp --allow-root plugin is-installed templates-patterns-collection  && wp --allow-root plugin update templates-patterns-collection  ||  wp --allow-root plugin install templates-patterns-collection 
+wp --allow-root plugin activate templates-patterns-collection 
