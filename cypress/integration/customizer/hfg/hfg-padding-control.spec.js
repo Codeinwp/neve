59b40815a676cbf91745e7ed2d90dbd1d67cf691
Augusto Silva <augusto@themeisle.com>
2021-02-11 16:11:27 -0300

    style: changes the style to reflect eslint update

--- cypress/integration/customizer/hfg/hfg-padding-control.spec.js
+++ cypress/integration/customizer/hfg/hfg-padding-control.spec.js
@@ -1,97 +1,71 @@
-describe("Header Builder Padding Control", () => {
-  before(() => {
-    // Login to wp and redirect to customizer.
-    cy.goToCustomizer();
-    cy.aliasRestRoutes();
+describe('Header Builder Padding Control', () => {
+	before(() => {
+		// Login to wp and redirect to customizer.
+		cy.goToCustomizer();
+		cy.aliasRestRoutes();
 
-    // Open customizer panel.
-    cy.get("#accordion-panel-hfg_header").should("be.visible").click();
+		// Open customizer panel.
+		cy.get('#accordion-panel-hfg_header').should('be.visible').click();
 
-    // Check if Logo and Site title component is visible and click it.
-    cy.get(
-      '.hfg--builder-show .hfg--panel-desktop .hfg--row-main .grid-stack-item[title="Logo & Site Identity"]'
-    )
-      .should("be.visible")
-      .click();
+		// Check if Logo and Site title component is visible and click it.
+		cy.get(
+			'.hfg--builder-show .hfg--panel-desktop .hfg--row-main .grid-stack-item[title="Logo & Site Identity"]',
+		)
+			.should('be.visible')
+			.click();
 
-    // Switch to layout tab.
-    cy.get('#customize-control-logo_tabs [data-tab="layout"]')
-      .should("be.visible")
-      .click();
+		// Switch to layout tab.
+		cy.get('#customize-control-logo_tabs [data-tab="layout"]').should('be.visible').click();
 
-    // Link values
-    cy.get("#customize-control-logo_component_padding button.link").click();
+		// Link values
+		cy.get('#customize-control-logo_component_padding button.link').click();
 
-    // Increase Top Padding value
-    cy.get("#customize-control-logo_component_padding .top-input")
-      .click()
-      .type(
-        "{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}"
-      )
-      .trigger("change");
+		// Increase Top Padding value
+		cy.get('#customize-control-logo_component_padding .top-input')
+			.click()
+			.type(
+				'{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}',
+			)
+			.trigger('change');
 
-    // Check if linked values
-    cy.get("#customize-control-logo_component_padding .top-input").should(
-      "have.value",
-      "30"
-    );
-    cy.get("#customize-control-logo_component_padding  .bottom-input").should(
-      "have.value",
-      "30"
-    );
-    cy.get("#customize-control-logo_component_padding .left-input").should(
-      "have.value",
-      "30"
-    );
-    cy.get("#customize-control-logo_component_padding .right-input").should(
-      "have.value",
-      "30"
-    );
+		// Check if linked values
+		cy.get('#customize-control-logo_component_padding .top-input').should('have.value', '30');
+		cy.get('#customize-control-logo_component_padding  .bottom-input').should('have.value', '30');
+		cy.get('#customize-control-logo_component_padding .left-input').should('have.value', '30');
+		cy.get('#customize-control-logo_component_padding .right-input').should('have.value', '30');
 
-    // Click on Reset button
-    cy.get("#customize-control-logo_component_padding button.reset").click();
+		// Click on Reset button
+		cy.get('#customize-control-logo_component_padding button.reset').click();
 
-    // Check if Reset button works
-    cy.get("#customize-control-logo_component_padding .top-input").should(
-      "have.value",
-      "10"
-    );
-    cy.get("#customize-control-logo_component_padding .right-input").should(
-      "have.value",
-      "0"
-    );
-    cy.get("#customize-control-logo_component_padding .bottom-input").should(
-      "have.value",
-      "10"
-    );
-    cy.get("#customize-control-logo_component_padding .left-input").should(
-      "have.value",
-      "0"
-    );
+		// Check if Reset button works
+		cy.get('#customize-control-logo_component_padding .top-input').should('have.value', '10');
+		cy.get('#customize-control-logo_component_padding .right-input').should('have.value', '0');
+		cy.get('#customize-control-logo_component_padding .bottom-input').should('have.value', '10');
+		cy.get('#customize-control-logo_component_padding .left-input').should('have.value', '0');
 
-    // Unlink values
-    cy.get("#customize-control-logo_component_padding button.link").click();
+		// Unlink values
+		cy.get('#customize-control-logo_component_padding button.link').click();
 
-    // Increase Top Padding value
-    cy.get("#customize-control-logo_component_padding .top-input")
-      .click()
-      .type("{uparrow}")
-      .trigger("change");
+		// Increase Top Padding value
+		cy.get('#customize-control-logo_component_padding .top-input')
+			.click()
+			.type('{uparrow}')
+			.trigger('change');
 
-    // Decrease Bottom Padding value
-    cy.get("#customize-control-logo_component_padding .bottom-input")
-      .click()
-      .type("{downarrow}")
-      .trigger("change");
+		// Decrease Bottom Padding value
+		cy.get('#customize-control-logo_component_padding .bottom-input')
+			.click()
+			.type('{downarrow}')
+			.trigger('change');
 
-    cy.get("#save").click();
-  });
+		cy.get('#save').click();
+	});
 
-  it("Sets up Padding for the Logo Component", () => {
-    cy.wait("@customizerSave").then((interception) => {
-      expect(interception.response.body.success).to.be.true;
-      expect(interception.response.statusCode).to.equal(200);
-      cy.checkPaddingFrontEnd();
-    });
-  });
+	it('Sets up Padding for the Logo Component', () => {
+		cy.wait('@customizerSave').then((interception) => {
+			expect(interception.response.body.success).to.be.true;
+			expect(interception.response.statusCode).to.equal(200);
+			cy.checkPaddingFrontEnd();
+		});
+	});
 });
--- cypress/integration/editor/modern/post-meta-sidebar.spec.js
+++ cypress/integration/editor/modern/post-meta-sidebar.spec.js
@@ -1,250 +1,222 @@
-describe("Single post meta sidebar", () => {
-  const postSetup = {
-    title: "Test Post",
-    content: "The Post Content",
-    url: null,
-  };
+describe('Single post meta sidebar', () => {
+	const postSetup = {
+		title: 'Test Post',
+		content: 'The Post Content',
+		url: null,
+	};
 
-  it('Create new post named "' + postSetup.title + '".', () => {
-    cy.insertPost(postSetup.title, postSetup.content, "post", true, true);
-    cy.get(".post-publish-panel__postpublish-header a")
-      .contains(postSetup.title)
-      .should("have.attr", "href")
-      .then((href) => {
-        postSetup.url = href;
-      });
-  });
+	it('Create new post named "' + postSetup.title + '".', () => {
+		cy.insertPost(postSetup.title, postSetup.content, 'post', true, true);
+		cy.get('.post-publish-panel__postpublish-header a')
+			.contains(postSetup.title)
+			.should('have.attr', 'href')
+			.then((href) => {
+				postSetup.url = href;
+			});
+	});
 
-  it("Default meta box settings on front end.", () => {
-    cy.visit(postSetup.url);
+	it('Default meta box settings on front end.', () => {
+		cy.visit(postSetup.url);
 
-    cy.get(".nv-sidebar-wrap")
-      .should("have.class", "nv-right")
-      .and("be.visible");
-    cy.get(".single-post-container")
-      .should("not.have.class", "container-fluid")
-      .and("be.visible");
-    cy.get(".nv-single-post-wrap")
-      .should("have.css", "max-width")
-      .and("eq", "70%");
-    cy.get(".entry-title").should("exist").and("contain", postSetup.title);
-    cy.get(".hfg_header").should("exist");
-    cy.get("footer.site-footer").should("exist");
-    cy.get(".nv-thumb-wrap > img")
-      .should("exist")
-      .and("be.visible")
-      .and("have.attr", "src")
-      .then(($href) => {
-        expect($href).to.contain("/wp-content/uploads/");
-      });
-    cy.get(".nv-content-wrap").should("contain", postSetup.content);
-  });
+		cy.get('.nv-sidebar-wrap').should('have.class', 'nv-right').and('be.visible');
+		cy.get('.single-post-container').should('not.have.class', 'container-fluid').and('be.visible');
+		cy.get('.nv-single-post-wrap').should('have.css', 'max-width').and('eq', '70%');
+		cy.get('.entry-title').should('exist').and('contain', postSetup.title);
+		cy.get('.hfg_header').should('exist');
+		cy.get('footer.site-footer').should('exist');
+		cy.get('.nv-thumb-wrap > img')
+			.should('exist')
+			.and('be.visible')
+			.and('have.attr', 'src')
+			.then(($href) => {
+				expect($href).to.contain('/wp-content/uploads/');
+			});
+		cy.get('.nv-content-wrap').should('contain', postSetup.content);
+	});
 
-  it("Check sidebar layout", () => {
-    cy.login(postSetup.url);
-    cy.get("#wp-admin-bar-edit a").click();
-    cy.clearWelcome();
+	it('Check sidebar layout', () => {
+		cy.login(postSetup.url);
+		cy.get('#wp-admin-bar-edit a').click();
+		cy.clearWelcome();
 
-    cy.openNeveSidebar();
+		cy.openNeveSidebar();
 
-    cy.getControl("neve_meta_sidebar")
-      .find('.components-radio-control__input[value="full-width"]')
-      .parent()
-      .click();
-    cy.updatePost();
-    cy.visit(postSetup.url);
-    cy.get(".nv-sidebar-wrap").should("not.exist");
-    cy.get("#wp-admin-bar-edit a").click();
+		cy.getControl('neve_meta_sidebar')
+			.find('.components-radio-control__input[value="full-width"]')
+			.parent()
+			.click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('.nv-sidebar-wrap').should('not.exist');
+		cy.get('#wp-admin-bar-edit a').click();
 
-    cy.getControl("neve_meta_sidebar")
-      .find('.components-radio-control__input[value="left"]')
-      .parent()
-      .click();
-    cy.updatePost();
-    cy.visit(postSetup.url);
-    cy.get(".nv-sidebar-wrap").should("exist").and("have.class", "nv-left");
-    cy.get("#wp-admin-bar-edit a").click();
+		cy.getControl('neve_meta_sidebar')
+			.find('.components-radio-control__input[value="left"]')
+			.parent()
+			.click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('.nv-sidebar-wrap').should('exist').and('have.class', 'nv-left');
+		cy.get('#wp-admin-bar-edit a').click();
 
-    cy.getControl("neve_meta_sidebar")
-      .find('.components-radio-control__input[value="right"]')
-      .parent()
-      .click();
-    cy.updatePost();
-    cy.visit(postSetup.url);
-    cy.get(".nv-sidebar-wrap").should("exist").and("have.class", "nv-right");
-  });
+		cy.getControl('neve_meta_sidebar')
+			.find('.components-radio-control__input[value="right"]')
+			.parent()
+			.click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('.nv-sidebar-wrap').should('exist').and('have.class', 'nv-right');
+	});
 
-  it("Check container layout", () => {
-    cy.login(postSetup.url);
-    cy.get("#wp-admin-bar-edit a").click();
-    cy.clearWelcome();
+	it('Check container layout', () => {
+		cy.login(postSetup.url);
+		cy.get('#wp-admin-bar-edit a').click();
+		cy.clearWelcome();
 
-    cy.openNeveSidebar();
+		cy.openNeveSidebar();
 
-    cy.getControl("neve_meta_container")
-      .find(".components-button")
-      .contains("Contained")
-      .click();
-    cy.updatePost();
-    cy.visit(postSetup.url);
-    cy.get(".single-post-container")
-      .should("have.class", "container")
-      .and("be.visible");
-    cy.get("#wp-admin-bar-edit a").click();
+		cy.getControl('neve_meta_container').find('.components-button').contains('Contained').click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('.single-post-container').should('have.class', 'container').and('be.visible');
+		cy.get('#wp-admin-bar-edit a').click();
 
-    cy.getControl("neve_meta_container")
-      .find(".components-button")
-      .contains("Full Width")
-      .click();
-    cy.updatePost();
-    cy.visit(postSetup.url);
-    cy.get(".single-post-container")
-      .should("not.have.class", "container")
-      .and("be.visible");
-  });
+		cy.getControl('neve_meta_container').find('.components-button').contains('Full Width').click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('.single-post-container').should('not.have.class', 'container').and('be.visible');
+	});
 
-  it("Check container width", () => {
-    cy.login(postSetup.url);
-    cy.get("#wp-admin-bar-edit a").click();
-    cy.clearWelcome();
+	it('Check container width', () => {
+		cy.login(postSetup.url);
+		cy.get('#wp-admin-bar-edit a').click();
+		cy.clearWelcome();
 
-    cy.openNeveSidebar();
+		cy.openNeveSidebar();
 
-    // const enableContentWidth = cy.get('.components-toggle-control__label').contains('Custom Content Width (%)');
-    cy.activateCheckbox(
-      ".components-toggle-control__label",
-      "Custom Content Width (%"
-    );
+		// const enableContentWidth = cy.get('.components-toggle-control__label').contains('Custom Content Width (%)');
+		cy.activateCheckbox('.components-toggle-control__label', 'Custom Content Width (%');
 
-    cy.get(".neve_meta_content_width")
-      .find("input[type=number]")
-      .type("{selectall}")
-      .type(60);
-    cy.updatePost();
-    cy.visit(postSetup.url);
+		cy.get('.neve_meta_content_width').find('input[type=number]').type('{selectall}').type(60);
+		cy.updatePost();
+		cy.visit(postSetup.url);
 
-    cy.get(".nv-single-post-wrap")
-      .should("have.css", "max-width")
-      .and("eq", "60%");
-  });
+		cy.get('.nv-single-post-wrap').should('have.css', 'max-width').and('eq', '60%');
+	});
 
-  it("Check title alignment", () => {
-    cy.login(postSetup.url);
-    cy.get("#wp-admin-bar-edit a").click();
-    cy.clearWelcome();
+	it('Check title alignment', () => {
+		cy.login(postSetup.url);
+		cy.get('#wp-admin-bar-edit a').click();
+		cy.clearWelcome();
 
-    cy.openNeveSidebar();
+		cy.openNeveSidebar();
 
-    cy.get(".neve_meta_title_alignment .nv-align-center").click();
-    cy.updatePost();
-    cy.visit(postSetup.url);
-    cy.get("h1.entry-title")
-      .should("have.class", "has-text-align-center")
-      .and("have.css", "text-align")
-      .and("eq", "center");
-    cy.get("#wp-admin-bar-edit a").click();
+		cy.get('.neve_meta_title_alignment .nv-align-center').click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('h1.entry-title')
+			.should('have.class', 'has-text-align-center')
+			.and('have.css', 'text-align')
+			.and('eq', 'center');
+		cy.get('#wp-admin-bar-edit a').click();
 
-    cy.get(".neve_meta_title_alignment .nv-align-right").click();
-    cy.updatePost();
-    cy.visit(postSetup.url);
-    cy.get("h1.entry-title")
-      .should("have.class", "has-text-align-right")
-      .and("have.css", "text-align")
-      .and("eq", "right");
-    cy.get("#wp-admin-bar-edit a").click();
+		cy.get('.neve_meta_title_alignment .nv-align-right').click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('h1.entry-title')
+			.should('have.class', 'has-text-align-right')
+			.and('have.css', 'text-align')
+			.and('eq', 'right');
+		cy.get('#wp-admin-bar-edit a').click();
 
-    cy.get(".neve_meta_title_alignment .nv-align-left").click();
-    cy.updatePost();
-    cy.visit(postSetup.url);
-    cy.get("h1.entry-title")
-      .should("have.class", "has-text-align-left")
-      .and("have.css", "text-align")
-      .and("eq", "left");
-    cy.get("#wp-admin-bar-edit a").click();
-  });
+		cy.get('.neve_meta_title_alignment .nv-align-left').click();
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('h1.entry-title')
+			.should('have.class', 'has-text-align-left')
+			.and('have.css', 'text-align')
+			.and('eq', 'left');
+		cy.get('#wp-admin-bar-edit a').click();
+	});
 
-  it("Check author avatar", () => {
-    cy.login(postSetup.url);
-    cy.get("#wp-admin-bar-edit a").click();
-    cy.clearWelcome();
+	it('Check author avatar', () => {
+		cy.login(postSetup.url);
+		cy.get('#wp-admin-bar-edit a').click();
+		cy.clearWelcome();
 
-    cy.openNeveSidebar();
+		cy.openNeveSidebar();
 
-    cy.activateCheckbox(".components-toggle-control__label", "Author Avatar");
-    cy.updatePost();
+		cy.activateCheckbox('.components-toggle-control__label', 'Author Avatar');
+		cy.updatePost();
 
-    cy.visit(postSetup.url);
-    cy.get(".nv-meta-list .author .photo").should("exist");
-  });
+		cy.visit(postSetup.url);
+		cy.get('.nv-meta-list .author .photo').should('exist');
+	});
 
-  it("Check post elements", () => {
-    cy.login(postSetup.url);
-    cy.get("#wp-admin-bar-edit a").click();
-    cy.clearWelcome();
+	it('Check post elements', () => {
+		cy.login(postSetup.url);
+		cy.get('#wp-admin-bar-edit a').click();
+		cy.clearWelcome();
 
-    cy.openNeveSidebar();
+		cy.openNeveSidebar();
 
-    cy.get(".ti-sortable-item-label").each((el, index) => {
-      const shouldContain = [
-        "Post Title",
-        "Post Meta",
-        "Featured Image",
-        "Content",
-        "Tags",
-        "Comments",
-        "Post Navigation",
-      ];
-      expect(el).to.contain(shouldContain[index]);
-      if (index === 6) {
-        cy.get(el).parent().should("have.class", "hidden");
-      } else {
-        cy.get(el).parent().should("not.have.class", "hidden");
-      }
-    });
+		cy.get('.ti-sortable-item-label').each((el, index) => {
+			const shouldContain = [
+				'Post Title',
+				'Post Meta',
+				'Featured Image',
+				'Content',
+				'Tags',
+				'Comments',
+				'Post Navigation',
+			];
+			expect(el).to.contain(shouldContain[index]);
+			if (index === 6) {
+				cy.get(el).parent().should('have.class', 'hidden');
+			} else {
+				cy.get(el).parent().should('not.have.class', 'hidden');
+			}
+		});
 
-    cy.toggleElements(false);
-    cy.get(".components-toggle-control__label")
-      .contains("Author Avatar")
-      .should("not.exist");
-    cy.updatePost();
-    cy.visit(postSetup.url);
+		cy.toggleElements(false);
+		cy.get('.components-toggle-control__label').contains('Author Avatar').should('not.exist');
+		cy.updatePost();
+		cy.visit(postSetup.url);
 
-    cy.get("h1.title").should("not.exist");
-    cy.get(".nv-thumb-wrap").should("not.exist");
-    cy.get(".nv-tags-list").should("not.exist");
-    cy.get(".nv-meta-list").should("not.exist");
-    cy.get(".nv-content-wrap").should("not.exist");
-    cy.get("#comments").should("not.exist");
-    cy.get(".nv-post-navigation").should("not.exist");
+		cy.get('h1.title').should('not.exist');
+		cy.get('.nv-thumb-wrap').should('not.exist');
+		cy.get('.nv-tags-list').should('not.exist');
+		cy.get('.nv-meta-list').should('not.exist');
+		cy.get('.nv-content-wrap').should('not.exist');
+		cy.get('#comments').should('not.exist');
+		cy.get('.nv-post-navigation').should('not.exist');
 
-    cy.get("#wp-admin-bar-edit a").click();
-    cy.toggleElements(true);
-    cy.get(".components-toggle-control__label")
-      .contains("Author Avatar")
-      .should("exist");
-    cy.updatePost();
-    cy.visit(postSetup.url);
+		cy.get('#wp-admin-bar-edit a').click();
+		cy.toggleElements(true);
+		cy.get('.components-toggle-control__label').contains('Author Avatar').should('exist');
+		cy.updatePost();
+		cy.visit(postSetup.url);
 
-    cy.get("h1.title").should("exist");
-    cy.get(".nv-thumb-wrap").should("exist");
-    cy.get(".nv-tags-list").should("exist");
-    cy.get(".nv-meta-list").should("exist");
-    cy.get(".nv-content-wrap").should("exist");
-    cy.get("#comments").should("exist");
-    cy.get(".nv-post-navigation").should("exist");
-    cy.visit(postSetup.url);
+		cy.get('h1.title').should('exist');
+		cy.get('.nv-thumb-wrap').should('exist');
+		cy.get('.nv-tags-list').should('exist');
+		cy.get('.nv-meta-list').should('exist');
+		cy.get('.nv-content-wrap').should('exist');
+		cy.get('#comments').should('exist');
+		cy.get('.nv-post-navigation').should('exist');
+		cy.visit(postSetup.url);
 
-    cy.get("#wp-admin-bar-edit a").click();
-    // const headerToggle = cy.get('.components-toggle-control__label').contains('Disable Header');
-    cy.activateCheckbox(".components-toggle-control__label", "Disable Header");
-    cy.updatePost();
-    cy.visit(postSetup.url);
-    cy.get(".hfg_header").should("not.exist");
-    cy.get("#wp-admin-bar-edit a").click();
+		cy.get('#wp-admin-bar-edit a').click();
+		// const headerToggle = cy.get('.components-toggle-control__label').contains('Disable Header');
+		cy.activateCheckbox('.components-toggle-control__label', 'Disable Header');
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('.hfg_header').should('not.exist');
+		cy.get('#wp-admin-bar-edit a').click();
 
-    cy.activateCheckbox(".components-toggle-control__label", "Disable Footer");
-    cy.updatePost();
-    cy.visit(postSetup.url);
-    cy.get("footer.site-footer").should("not.exist");
-  });
+		cy.activateCheckbox('.components-toggle-control__label', 'Disable Footer');
+		cy.updatePost();
+		cy.visit(postSetup.url);
+		cy.get('footer.site-footer').should('not.exist');
+	});
 });
