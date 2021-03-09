1787861e948956cda1f1047034d53a8f10117501
selul <marius.cristea@vertistudio.com>
2021-03-09 09:41:15 +0200

    build: remove false positive changes on pot generation, add pot generation to lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -6,26 +6,27 @@ module.exports = {
 	"package.json": [
 		"yarn run lint:package-json"
 	],
-	"dashboard/src/*.js": () => ["yarn run format:dash", // We ignore here the staged restriction.
+	"dashboard/src/**/*.js": () => ["yarn run format:dash", // We ignore here the staged restriction.
 		"yarn run lint:dash",
 		"yarn run build:dash"],
-	"assets/js/src/*.js": () => [ // We ignore here the staged restriction.
+	"assets/js/src/**/*.js": () => [ // We ignore here the staged restriction.
 		"yarn run format:global",
 		"yarn run lint:global",
 		"yarn run build:rollup",
 		"yarn run size"
 	],
-	"assets/scss/*.scss": () => [ // We ignore here the staged restriction.
+	"assets/scss/**/*.scss": () => [ // We ignore here the staged restriction.
 		"yarn run build:grunt",
 		"yarn run size"
 	],
 	"!(*-rtl).css": () => [ // We ignore here the staged restriction.
 		"yarn run build:grunt"
 	],
-	"inc/customizer/controls/react/src/*.js": () => [ // We ignore here the staged restriction.
+	"inc/customizer/controls/react/src/**/*.js": () => [ // We ignore here the staged restriction.
 		"yarn run format:customizer",
 		"yarn run lint:customizer",
 		"yarn run build:customizer"
-	]
+	],
+	"*.(js|php)" : () => [" yarn run build:makepot"]
 
 }
