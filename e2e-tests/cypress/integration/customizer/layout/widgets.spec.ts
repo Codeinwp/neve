5608ac78724361c35926d03567e3c67664b52d14
Augusto Silva <augusto@themeisle.com>
2021-03-29 11:48:08 -0300

    test: adds customizer layout widget spec

---
+++ e2e-tests/cypress/integration/customizer/layout/widgets.spec.ts
@@ -0,0 +1,13 @@
+describe('Widgets', function () {
+	before(function () {
+		cy.loginWithRequest();
+	});
+
+	it('Adds a new Widget', function () {
+		/** TODO */
+	});
+
+	it('Reorders widgets', function () {
+		/** todo */
+	});
+});
