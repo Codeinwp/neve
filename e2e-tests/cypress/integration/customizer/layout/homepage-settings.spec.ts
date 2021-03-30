2ad121618273d08713e8da9026ee6778ea44b91d
Irinel Enache <irinelenache@Irinels-MacBook-Pro.local>
2021-03-30 16:45:38 +0300

    test: first part of homepage

---
+++ e2e-tests/cypress/integration/customizer/layout/homepage-settings.spec.ts
@@ -0,0 +1,24 @@
+describe('Homepage-settings', function () {
+	beforeEach(function () {
+		cy.loginWithRequest('/wp-admin/customize.php');
+		cy.get('#accordion-panel-neve_layout > .accordion-section-title').click()
+		cy.get('#accordion-section-static_front_page > .accordion-section-title').click()
+		cy.get('#_customize-input-show_on_front-radio-page').click()
+		
+	});
+
+	it('Sets up a page to be the homepage', function () {
+		/** TODO */
+		
+		cy.get('#_customize-input-page_on_front').select('Sample Page')
+		cy.get('#save').click({force:true});
+
+	});
+
+	it('Sets up a page to be the posts page', function () {
+		/** todo */
+		cy.get('#_customize-input-page_for_posts').select('Blog')
+		cy.get('#save').click({force:true});
+
+	});
+});
