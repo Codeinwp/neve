4021558031237f2d54371b514e0d68b45c76e6bb
Tudor-Dev <tudor.simion@redboxdesign.ro>
2021-04-01 17:07:24 +0300

    style: fix homepage-setting.spec style

--- e2e-tests/cypress/integration/customizer/layout/homepage-settings.spec.ts
+++ e2e-tests/cypress/integration/customizer/layout/homepage-settings.spec.ts
@@ -1,31 +1,28 @@
 describe('Homepage-settings', function () {
 	beforeEach(function () {
 		cy.loginWithRequest('/wp-admin/customize.php');
-		cy.get('#accordion-panel-neve_layout > .accordion-section-title').click()
-		cy.get('#accordion-section-static_front_page > .accordion-section-title').click()
-		cy.get('#_customize-input-show_on_front-radio-page').click()
-		
+		cy.get('#accordion-panel-neve_layout > .accordion-section-title').click();
+		cy.get('#accordion-section-static_front_page > .accordion-section-title').click();
+		cy.get('#_customize-input-show_on_front-radio-page').click();
 	});
 
 	it('Sets up a page to be the homepage', function () {
 		/** TODO */
-		
-		cy.get('#_customize-input-page_on_front').select('Sample Page')
-		cy.get('#save').click({force:true});
+
+		cy.get('#_customize-input-page_on_front').select('Sample Page');
+		cy.get('#save').click({ force: true });
 		cy.visit('/');
-		cy.get('h1').should('contain.text','Sample Page');
-
-
+		cy.get('h1').should('contain.text', 'Sample Page');
 	});
 
 	it.only('Sets up a page to be the posts page', function () {
 		/** todo */
-		cy.get('#_customize-input-page_for_posts').select('Blog')
-		cy.get('#save').click({force:true});
+		cy.get('#_customize-input-page_for_posts').select('Blog');
+
+		cy.get('#save').click({ force: true });
+
 		cy.visit('/Blog');
+
 		cy.get('container archive-container');
-
-
-
 	});
 });
