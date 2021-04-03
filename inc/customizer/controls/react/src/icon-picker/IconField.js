8976fbaf68385dbd25349bb0fcbf8b560b333d86
Craciun Bianca <bianca_craciun@LAPTOP-0DSIDJFA.localdomain>
2021-04-03 19:44:39 +0300

    build: icon input

---
+++ inc/customizer/controls/react/src/icon-picker/IconField.js
@@ -0,0 +1,61 @@
+import { Icon } from '@wordpress/components';
+import { closeSmall } from '@wordpress/icons';
+import { useState } from '@wordpress/element';
+import IconPicker from '../common/IconPicker';
+import * as icons from '../common/icons';
+
+const IconField = ({ label = 'Icon' }) => {
+	const [isOpen, open] = useState(false);
+	const [icon, setIcon] = useState(icons.facebook.icon);
+	const [field, setField] = useState(icons.facebook.id);
+
+	const openPicker = () => {
+		open(!isOpen);
+	};
+
+	const selectIcon = (socialIcon, id) => {
+		setIcon(socialIcon);
+		setField(id);
+		open(false);
+	};
+
+	const closeIconsContainer = () => {
+		open(false);
+	};
+
+	return (
+		<div className="nv-repeater--field">
+			<label>{label}</label>
+			<div className="nv--icon-field-wrap">
+				<div className="form">
+					<button
+						className="nv--icon-selector button"
+						onClick={openPicker}
+					>
+						{icon}
+					</button>
+					<input
+						type="text"
+						className="nv-repeater-text-field has-value"
+						value={field}
+						readOnly
+						onClick={openPicker}
+					/>
+					<a
+						//
+						href="javascript: return false;"
+						className="button nv--remove-icon"
+						onClick={closeIconsContainer}
+					>
+						{/*<Icon icon={closeSmall} />*/}
+						<span className="dashicons dashicons-no" />
+					</a>
+				</div>
+
+				<IconPicker selectIcon={selectIcon} isOpen={isOpen} />
+			</div>
+		</div>
+	);
+};
+
+export default IconField;
