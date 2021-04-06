dc643b12f9fd4f96e4ce0252af2efef4468d7c10
Augusto Silva <augusto@themeisle.com>
2021-04-06 10:38:53 -0300

    chore: removes setup function for local use

--- setupDockerEnv.sh
+++
@@ -1,8 +0,0 @@
-  # installs wpReset
-  docker-compose -f docker-compose.ci.yml run --rm -u root cli wp --allow-root plugin install wp-reset --activate
-  # resets DB
-  docker-compose -f docker-compose.ci.yml run --rm -u root cli wp --allow-root reset reset --reactivate-theme --yes
-  # initial setup for docker env
-  bash bin/envs/init.sh
-  # setup for specific env
-  docker-compose -f docker-compose.ci.yml run --rm -u root cli sh "/var/www/html/bin/envs/$1/start.sh"
