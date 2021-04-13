c0cbc15d85e0333b7d94c98d5ae25a470d932a18
cristian-ungureanu <cristian@themeisle.com>
2021-04-13 12:21:43 +0300

    fix: fatal error for missing hooks functions [#2764]

---
+++ globals/hooks.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Main Hooks.
+ *
+ * @package Neve/Hooks
+ */
+
+/* Header ( Navigation ) area */
+/**
+ * Hook just before the header ( navigation ) area
+ */
+function neve_before_header_trigger() {
+	do_action( 'neve_before_header_hook' );
+}
+
+/**
+ * Hook at the beginning of header ( navigation ) area
+ */
+function neve_before_header_wrapper_trigger() {
+	do_action( 'neve_before_header_wrapper_hook' );
+}
+
+/**
+ * Hook just after the header ( navigation ) area
+ */
+function neve_after_header_trigger() {
+	do_action( 'neve_after_header_hook' );
+}
+
+/**
+ * Hook just after the header content ( navigation ) area
+ */
+function neve_after_header_wrapper_trigger() {
+	do_action( 'neve_after_header_wrapper_hook' );
+}
+
+/**
+ * Hook just before the footer area
+ */
+function neve_before_footer_trigger() {
+	do_action( 'neve_before_footer_hook' );
+}
+
+/**
+ * Hook just after the footer area
+ *
+ * HTML context: after `footer`
+ */
+function neve_after_footer_trigger() {
+	do_action( 'neve_after_footer_hook' );
+}
+
+if ( ! function_exists( 'wp_body_open' ) ) {
+	/**
+	 * Body open hook.
+	 */
+	function wp_body_open() {
+		do_action( 'wp_body_open' );
+	}
+}
