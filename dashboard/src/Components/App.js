6e6037466161a391816534bea82b89b7dc0598b4
cristian-ungureanu <cristian@themeisle.com>
2021-04-15 14:01:39 +0300

    chore: update notification to display text and link [#1286]

--- dashboard/src/Components/App.js
+++ dashboard/src/Components/App.js
@@ -6,13 +6,11 @@ import Loading from './Loading';
 import Snackbar from './Snackbar';
 import { fetchOptions } from '../utils/rest';
 
-import { __ } from '@wordpress/i18n';
-import { Button } from '@wordpress/components';
 import { withDispatch, withSelect } from '@wordpress/data';
 import { compose } from '@wordpress/compose';
 import { useState, Fragment, useEffect } from '@wordpress/element';
 
-const App = ({ tier, setSettings, toast, currentTab, setTab }) => {
+const App = ({ setSettings, toast, currentTab, setTab }) => {
 	const [loading, setLoading] = useState(true);
 	useEffect(() => {
 		fetchOptions().then((r) => {
@@ -30,22 +28,6 @@ const App = ({ tier, setSettings, toast, currentTab, setTab }) => {
 				<div className="container content">
 					<div className="main">
 						{'starter-sites' !== currentTab && <Notifications />}
-						{tier === 3 && (
-							<div className="notification template-cloud">
-								<p>
-									{__(
-										'Great news! Now you can export your own custom designs to the cloud and then reuse them on other sites.',
-										'neve'
-									)}
-								</p>
-								<Button
-									isSecondary
-									href="themes.php?page=tiob-starter-sites"
-								>
-									{__('Open Templates Cloud', 'neve')}
-								</Button>
-							</div>
-						)}
 						<TabsContent currentTab={currentTab} setTab={setTab} />
 					</div>
 					{'starter-sites' !== currentTab && (
@@ -67,11 +49,10 @@ export default compose(
 		};
 	}),
 	withSelect((select) => {
-		const { getToast, getTab, getLicenseTier } = select('neve-dashboard');
+		const { getToast, getTab } = select('neve-dashboard');
 		return {
 			toast: getToast(),
 			currentTab: getTab(),
-			tier: getLicenseTier(),
 		};
 	})
 )(App);
