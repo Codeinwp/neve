08f4b3e045ca409baa4a182f8e2904af7fe99434
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2021-05-08 14:33:31 +0300

    refactor: a small ench on if condition

--- inc/core/styles/css_prop.php
+++ inc/core/styles/css_prop.php
@@ -179,7 +179,7 @@ class Css_Prop {
 					Font_Manager::add_google_font( $font, strval( $value ) );
 				}
 
-				$value = ( $value > 0 ) ? intval( $value ) : 'normal';
+				$value = ( intval($value) > 0 ) ? intval( $value ) : 'normal';
 
 				return sprintf( ' %s: %s;', $css_prop, $value );
 				break;
