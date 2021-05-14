ee6e77d24b42648fb818f7e3a7d5cb77c93a8079
Bogdan Preda <bogdan.preda@themeisle.com>
2021-05-14 16:33:44 +0300

    chore: cleanup code

--- inc/customizer/controls/react/src/inline-select/InlineSelectComponent.js
+++ inc/customizer/controls/react/src/inline-select/InlineSelectComponent.js
@@ -1,9 +1,8 @@
 import { useEffect, useState } from '@wordpress/element';
-//import { WPCustomizeControl } from '';
 import InlineSelect from '../common/InlineSelect';
 
 const InlineSelectComponent = ({ control }) => {
-	const { label, options } = control.params;
+	const { label, options, changesOn } = control.params;
 	const [value, setValue] = useState(control.setting());
 	const defaultSettings = Object.keys(options).map((key) => {
 		return { value: key, label: options[key] };
@@ -11,15 +10,32 @@ const InlineSelectComponent = ({ control }) => {
 	const [settings, setSettings] = useState(defaultSettings);
 
 	useEffect(() => {
+		if (
+			typeof changesOn !== 'string' ||
+			changesOn !== 'neve_global_colors'
+		) {
+			return;
+		}
+
 		window.wp.customize.bind('ready', () => {
-			// If `changesOn` param is set to `neve_global_colors` update select settings with current global colors
-			if (
-				control.params.changesOn &&
-				control.params.changesOn === 'neve_global_colors'
-			) {
-				const currentGlobalColors = window.wp.customize
-					.control(control.params.changesOn)
-					.setting.get().palettes;
+			// Update select settings with current global colors
+			const currentGlobalColors = window.wp.customize
+				.control(changesOn)
+				.setting.get().palettes;
+			setSettings(
+				Object.keys(currentGlobalColors).map((key) => {
+					return {
+						value: key,
+						label: currentGlobalColors[key].name,
+					};
+				})
+			);
+		});
+
+		// Listen on value change and update select settings with current global colors
+		window.wp.customize.control(changesOn, (customizeControl) => {
+			customizeControl.setting.bind((nextValue) => {
+				const currentGlobalColors = nextValue.palettes;
 				setSettings(
 					Object.keys(currentGlobalColors).map((key) => {
 						return {
@@ -28,31 +44,8 @@ const InlineSelectComponent = ({ control }) => {
 						};
 					})
 				);
-			}
+			});
 		});
-
-		if (
-			control.params.changesOn &&
-			control.params.changesOn === 'neve_global_colors'
-		) {
-			// If `changesOn` param is set to `neve_global_colors` listen on value change and update select settings with current global colors
-			window.wp.customize.control(
-				control.params.changesOn,
-				(customizeControl) => {
-					customizeControl.setting.bind((nextValue) => {
-						const currentGlobalColors = nextValue.palettes;
-						setSettings(
-							Object.keys(currentGlobalColors).map((key) => {
-								return {
-									value: key,
-									label: currentGlobalColors[key].name,
-								};
-							})
-						);
-					});
-				}
-			);
-		}
 	}, [settings]);
 
 	const updateValue = (newValue) => {
