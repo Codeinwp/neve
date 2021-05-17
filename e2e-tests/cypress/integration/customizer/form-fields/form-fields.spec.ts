911890aa214510985ba724308acb7697a40a343c
irinelenache <irinel@themeisle.com>
2021-05-17 16:50:04 +0300

    test: commited all changes

---
+++ e2e-tests/cypress/integration/customizer/form-fields/form-fields.spec.ts
@@ -0,0 +1,46 @@
+describe('Form fields', function () {
+	beforeEach('Sets up customizer', function () {
+		cy.fixture('customizer/form-fields/form-fields-setup').then((componentSetup) => {
+			cy.setCustomizeSettings(componentSetup);
+		});
+	});
+
+	it('Checks up the search field', function () {
+		cy.visit('/');
+		cy.get('.search-field')
+			.should('have.css', 'padding-top', '14px')
+			.and('have.css', 'padding-bottom', '14px')
+			.and('have.css', 'padding-left', '14px')
+			.and('have.css', 'color', 'rgb(3, 102, 214)')
+			.and('have.css', 'background-color', 'rgb(237, 237, 237)')
+			.and('have.css', 'border-color', 'rgb(20, 23, 28)')
+			.and('have.css', 'border-top-width', '3px')
+			.and('have.css', 'border-bottom-width', '3px')
+			.and('have.css', 'border-left-width', '3px')
+			.and('have.css', 'border-right-width', '3px')
+			.and('have.css', 'border-top-left-radius', '4px')
+			.and('have.css', 'border-top-right-radius', '4px')
+			.and('have.css', 'border-bottom-left-radius', '4px')
+			.and('have.css', 'border-bottom-right-radius', '4px')
+			//to do - text transform, works only in customizer for search
+			.and('have.css', 'font-size', '15px')
+			.and('have.css', 'line-height', '24px')
+			.and('have.css', 'letter-spacing', '1.5px')
+			.and('have.css', 'font-weight', '600');
+	});
+	it('Checks up the comment label', function () {
+		cy.visit('/hello-world');
+		cy.contains('Name')
+			.should('have.css', 'font-size', '15px')
+			.and('have.css', 'line-height', '24px')
+			.and('have.css', 'letter-spacing', '1.5px')
+			.and('have.css', 'font-weight', '100')
+			.and('have.css', 'text-transform', 'capitalize')
+			.and('have.css', 'margin-bottom', '20px');
+	});
+	it('Checks up the button', function () {
+		cy.visit('/hello-world');
+		cy.get('#submit');
+		//todo - check if the button is primary
+	});
+});
