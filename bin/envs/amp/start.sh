b0c7c02d8e8b8b037d3d8135e4988617f8e6ec47
Augusto Silva <augusto@themeisle.com>
2021-06-18 12:25:59 -0300

    chore: fix amp env script

--- bin/envs/amp/start.sh
+++ bin/envs/amp/start.sh
@@ -1,6 +1,6 @@
 wp --allow-root plugin install amp --activate
 wp menu create sample-menu --allow-root
 wp menu location assign sample-menu primary --allow-root
-wp menu item add-post sample-menu 1 --title='Parent menu' --allow-root
-wp menu item add-post sample-menu 1 --title='Submenu Item' --allow-root  --parent-id='$(wp menu item list sample-menu --allow-root --format=ids)'
+wp menu item add-post sample-menu 1 --title="Parent menu" --allow-root
+wp menu item add-post sample-menu 1 --title="Submenu Item" --allow-root  --parent-id="$(wp menu item list sample-menu --allow-root --format=ids)"
 wp --allow-root rewrite structure /%postname%/
