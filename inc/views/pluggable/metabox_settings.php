debb3240a228f98b2a542928d272f365193705a5
cristian-ungureanu <cristian@themeisle.com>
2021-06-29 12:14:17 +0300

    fix: single post author avatar setting [#2929]

--- inc/views/pluggable/metabox_settings.php
+++ inc/views/pluggable/metabox_settings.php
@@ -74,7 +74,7 @@ class Metabox_Settings {
 		);
 		add_filter( 'neve_layout_single_post_elements_order', array( $this, 'filter_post_elements' ) );
 		add_filter( 'neve_post_title_alignment', array( $this, 'filter_title_alignment' ) );
-		add_filter( 'neve_display_author_avatar', array( $this, 'filter_author_avatar_display' ) );
+		add_filter( 'neve_display_author_avatar', array( $this, 'filter_author_avatar_display' ), 15 );
 	}
 
 	/**
