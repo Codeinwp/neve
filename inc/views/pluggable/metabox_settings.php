3681e7672e711bd7d5e2e9cd875c72aa8fef3192
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2021-07-05 13:10:40 +0300

    refactor: unnecessary typehint was removed

--- inc/views/pluggable/metabox_settings.php
+++ inc/views/pluggable/metabox_settings.php
@@ -294,7 +294,7 @@ class Metabox_Settings {
 	/**
 	 * Get content width status.
 	 *
-	 * @return int|string
+	 * @return int
 	 */
 	private function get_content_width_default() {
 		if ( (int) $this->get_post_id() === (int) get_option( 'woocommerce_checkout_page_id' ) ) {
