3dc4edf1b5ddf03a4b42237af22924b851fe06a4
abaicus <baicusandrei@me.com>
2021-07-13 12:48:37 +0300

    chore: make sure name of global function makes sense chore: disable conditional header selector in incompatibility cases

--- globals/utilities.php
+++ globals/utilities.php
@@ -1480,5 +1480,5 @@ function neve_is_new_skin() {
  * @since 3.0.0
  */
 function neve_can_use_conditional_header() {
-	return defined( 'NEVE_PRO_VERSION' ) && version_compare( NEVE_PRO_VERSION, '3.0.0', '<' ) && neve_is_new_builder();
+	return defined( 'NEVE_PRO_VERSION' ) && version_compare( NEVE_PRO_VERSION, '3.0.0', '>=' ) && neve_is_new_builder();
 }
