a4b8ab2765e524b4750aa139f40dae726329f156
selul <marius.cristea@vertistudio.com>
2021-07-14 11:42:44 +0300

    fix theme check loaded package, ref https://github.com/anhskohbo/wp-cli-themecheck/issues/9

--- bin/envs/theme-check/start.sh
+++ bin/envs/theme-check/start.sh
@@ -1,3 +1,3 @@
-php -d memory_limit=1024M "$(which wp)" package install anhskohbo/wp-cli-themecheck --allow-root
+php -d memory_limit=1024M "$(which wp)" package install anhskohbo/wp-cli-themecheck:dev-master#a53732bf056ee446a4b975d20914d0469e16ea59 --allow-root
 wp plugin install theme-check --activate --allow-root
 wp themecheck --theme=neve --no-interactive --allow-root
