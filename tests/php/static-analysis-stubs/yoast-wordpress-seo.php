70597367383588e7d4d7c36e4f1d4d7f6b48800b
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2021-07-15 14:47:57 +0300

    chore: stub file created for yoast functions

---
+++ tests/php/static-analysis-stubs/yoast-wordpress-seo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @param string $before
+ * @param string $after
+ * @param bool $display
+ * 
+ * @return string
+ */
+function yoast_breadcrumb( $before = '', $after = '', $display = true ) {}
