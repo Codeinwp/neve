41c82f3c7c8aee0f72e961d5f64c8eb921970d4b
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2021-07-15 21:53:01 +0300

    refactor: stub class created for a yoast function

--- tests/php/static-analysis-stubs/yoast-wordpress-seo.php
+++ tests/php/static-analysis-stubs/yoast-wordpress-seo.php
@@ -7,3 +7,14 @@
  * @return string
  */
 function yoast_breadcrumb( $before = '', $after = '', $display = true ) {}
+
+class WPSEO_Options {
+    /**
+     * Get single field option
+     *
+     * @param  string $key
+     * @param  mixed $default
+     * @return mixed|null
+     */
+    public static function get( $key, $default = null ) {}
+}
