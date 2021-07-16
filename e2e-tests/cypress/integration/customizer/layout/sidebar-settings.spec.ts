13ac686d4fa140a75d08f6aa643a9e106c4a67d3
Augusto Silva <augusto@themeisle.com>
2021-07-15 23:36:08 -0300

    test: change sidebar layout test order

--- e2e-tests/cypress/integration/customizer/layout/sidebar-settings.spec.ts
+++ e2e-tests/cypress/integration/customizer/layout/sidebar-settings.spec.ts
@@ -6,16 +6,9 @@ describe('Sidebar/Content Settings', function () {
 			});
 		});
 
-		it('Index', function () {
-			cy.visit('/');
-			cy.wait(900);
-			cy.reload();
-			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '50%');
-			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
-			cy.get('.nv-index-posts').should('have.css', 'max-width', '50%');
-		});
 		it('Page', function () {
 			cy.visit('/sample-page');
+      cy.reload();
 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '50%');
 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
 			cy.get('.nv-single-page-wrap').should('have.css', 'max-width', '50%');
@@ -32,6 +25,12 @@ describe('Sidebar/Content Settings', function () {
 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
 			cy.get('.nv-single-post-wrap').should('have.css', 'max-width', '50%');
 		});
+    it('Index', function () {
+			cy.visit('/');
+			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '50%');
+			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
+			cy.get('.nv-index-posts').should('have.css', 'max-width', '50%');
+		});
 	});
 
 	context('Sidebar advanced on front end.', function () {
@@ -41,16 +40,9 @@ describe('Sidebar/Content Settings', function () {
 			});
 		});
 
-		it('Index', function () {
-			cy.visit('/');
-			cy.wait(1500);
-			cy.reload();
-			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '80%');
-			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
-			cy.get('.nv-index-posts').should('have.css', 'max-width', '20%');
-		});
 		it('Page', function () {
 			cy.visit('/sample-page');
+      cy.reload()
 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '30%');
 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
 			cy.get('.nv-single-page-wrap').should('have.css', 'max-width', '70%');
@@ -66,6 +58,12 @@ describe('Sidebar/Content Settings', function () {
 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '30%');
 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-right');
 			cy.get('.nv-single-post-wrap').should('have.css', 'max-width', '70%');
+		});
+    it('Index', function () {
+			cy.visit('/');
+			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '80%');
+			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
+			cy.get('.nv-index-posts').should('have.css', 'max-width', '20%');
 		});
 	});
 });
