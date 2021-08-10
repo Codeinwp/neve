70d5a3e5e541dedae2fb5d92ae6f2b294d11dc43
Augusto Silva <augusto@themeisle.com>
2021-08-10 10:37:48 -0300

    test: adds style tests for mobile devices

--- e2e-tests/cypress/integration/woocommerce/header-mini-cart.ts
+++ e2e-tests/cypress/integration/woocommerce/header-mini-cart.ts
@@ -64,6 +64,18 @@ describe('Header Mini Cart Functionality', function () {
 });
 
 describe('Header Mini Cart Style', function () {
+	const devices = [
+		{
+			name: 'Desktop',
+			preset: 'macbook-16',
+		},
+		{
+			name: 'Phone',
+			preset: 'iphone-xr',
+		},
+		{ name: 'Tablet', preset: 'ipad-mini' },
+	];
+
 	before(function () {
 		cy.fixture('woocommerce/header-mini-cart').then((setup) => {
 			cy.setCustomizeSettings(setup.style);
@@ -71,28 +83,49 @@ describe('Header Mini Cart Style', function () {
 		cy.visit('/shop');
 	});
 
-	context('Desktop', function () {
+	devices.forEach((device) => {
+		context(device.name, function () {
+			beforeEach(function () {
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				cy.viewport(device.preset);
+			});
+
+			it('Check style for the mini cart', function () {
+				cy.get('.nv-icon > svg')
+					.should('have.css', 'color', 'rgb(118, 5, 5)')
+					.and('have.css', 'fill', 'rgb(118, 5, 5)')
+					.and('have.css', 'width', '80px');
+			});
+
+			it('Check margin and padding', function () {
+				cy.get('[data-item-id="header_cart_icon"]')
+					.should('have.css', 'padding-bottom', '0px')
+					.and('have.css', 'padding-top', '0px')
+					.and('have.css', 'padding-left', '10px')
+					.and('have.css', 'padding-right', '10px')
+					.and('have.css', 'margin-bottom', '0px')
+					.and('have.css', 'margin-top', '0px')
+					.and('have.css', 'margin-left', '0px')
+					.and('have.css', 'margin-right', '0px');
+			});
+		});
+	});
+
+	context('Desktop-only', function () {
 		beforeEach(function () {
 			cy.viewport(1920, 1080);
 		});
 
-		it('Check style for the mini cart', function () {
-			cy.get('.nv-icon > svg')
-				.should('have.css', 'color', 'rgb(118, 5, 5)')
-				.and('have.css', 'fill', 'rgb(118, 5, 5)')
-				.and('have.css', 'width', '80px');
-
+		it('Check hover style', function () {
 			cy.get('.nv-icon > svg')
 				.realHover()
 				.should('have.css', 'color', 'rgb(18, 90, 50)')
 				.and('have.css', 'fill', 'rgb(18, 90, 50)')
 				.and('have.css', 'width', '80px');
 		});
-	});
-
-	context('Mobile', function () {
-		beforeEach(function () {
-			cy.viewport('iphone-xr');
+		it('Check alignment', function () {
+			cy.get('[data-item-id="header_cart_icon"]').should('have.css', 'text-align', 'center');
 		});
 	});
 });
