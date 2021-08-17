699fdd7d6c6422e5a64ad3a90b407949d036e735
Augusto Silva <augusto@themeisle.com>
2021-08-17 11:10:00 -0300

    test: adds tests for mobile functionality on mini cart

--- e2e-tests/cypress/integration/woocommerce/header-mini-cart.ts
+++ e2e-tests/cypress/integration/woocommerce/header-mini-cart.ts
@@ -1,65 +1,115 @@
 describe('Header Mini Cart Functionality', function () {
-	before(function () {
-		cy.fixture('woocommerce/header-mini-cart').then((setup) => {
-			cy.setCustomizeSettings(setup.functionality);
+	context('Desktop', function () {
+		beforeEach(function () {
+			cy.fixture('woocommerce/header-mini-cart').then((setup) => {
+				cy.setCustomizeSettings(setup.desktopFunctionality);
+			});
+			cy.viewport(1920, 1080);
+		});
+
+		it('Open the cart page when clicking on mini cart', function () {
+			cy.visit('/shop');
+			/* ==== Generated with Cypress Studio ==== */
+			cy.get('.nv-icon > svg > path').click();
+			/* ==== End Cypress Studio ==== */
+			cy.url().should('contain', '/cart');
+		});
+
+		it('Value of products is updated when adding or removing a product', function () {
+			let countBefore = 0;
+
+			// Add product to cart
+			cy.visit('/product/album');
+			cy.get('.cart-count')
+				.invoke('text')
+				.then((count) => {
+					countBefore = parseInt(count);
+					cy.findByRole('button', {
+						name: /add to cart/i,
+					}).click();
+					const countExpect = ++countBefore;
+					cy.get('.cart-count').should('have.text', countExpect);
+				});
+
+			// Removes the product from cart
+			cy.get('.nv-icon > svg')
+				.realHover({ pointer: 'mouse', position: 'center' })
+				.then(() => {
+					cy.findAllByRole('link', {
+						name: /remove this item/i,
+					}).click({ multiple: true });
+				});
+			cy.get('.cart-count').should('have.text', '0');
+		});
+
+		it('Enables Checkout page button on mini cart when at least one product is on cart', function () {
+			cy.visit('/product/album/');
+			cy.findByRole('button', {
+				name: /add to cart/i,
+			}).click();
+
+			cy.get('.nv-icon > svg')
+				.realHover({ pointer: 'mouse', position: 'center' })
+				.then(() => {
+					//Checks if the view cart exists
+					cy.findByRole('link', {
+						name: /view cart/i,
+					});
+
+					//Clicks on the Checkout button
+					cy.findByRole('link', {
+						name: /checkout/i,
+					}).click();
+				});
+			cy.url().should('contain', '/checkout');
 		});
 	});
 
-	it('Open the cart page when clicking on mini cart', function () {
-		cy.visit('/shop');
-		/* ==== Generated with Cypress Studio ==== */
-		cy.get('.nv-icon > svg > path').click();
-		/* ==== End Cypress Studio ==== */
-		cy.url().should('contain', '/cart');
-	});
-
-	it('Value of products is updated when adding or removing a product', function () {
-		let countBefore = 0;
-
-		// Add product to cart
-		cy.visit('/product/album');
-		cy.get('.cart-count')
-			.invoke('text')
-			.then((count) => {
-				countBefore = parseInt(count);
-				cy.findByRole('button', {
-					name: /add to cart/i,
-				}).click();
-				const countExpect = ++countBefore;
-				cy.get('.cart-count').should('have.text', countExpect);
+	context('Mobile', function () {
+		beforeEach(function () {
+			cy.fixture('woocommerce/header-mini-cart').then((setup) => {
+				cy.setCustomizeSettings(setup.mobileFunctionality);
 			});
+			cy.viewport('iphone-xr');
+		});
 
-		// Removes the product from cart
-		cy.get('.nv-icon > svg')
-			.realHover({ pointer: 'mouse', position: 'center' })
-			.then(() => {
-				cy.findAllByRole('link', {
-					name: /remove this item/i,
-				}).click({ multiple: true });
-			});
-		cy.get('.cart-count').should('have.text', '0');
-	});
+		it('Visit the cart page after clicking on the mini cart icon', function () {
+			cy.visit('/shop');
+			cy.findByRole('link', {
+				name: /cart/i,
+			}).click();
+			cy.url().should('contain', '/cart');
+		});
+		it('Update value of products in cart after adding a product', function () {
+			let countBefore = 0;
 
-	it('Enables Checkout page button on mini cart when at least one product is on cart', function () {
-		cy.visit('/product/sunglasses/');
-		cy.findByRole('button', {
-			name: /add to cart/i,
-		}).click();
-
-		cy.get('.nv-icon > svg')
-			.realHover({ pointer: 'mouse', position: 'center' })
-			.then(() => {
-				//Checks if the view cart exists
-				cy.findByRole('link', {
-					name: /view cart/i,
+			// Add product to cart
+			cy.visit('/product/album');
+			cy.get(
+				'.header-bottom.hide-on-desktop > .header--row-inner > .container > .row > .right > .builder-item > .item--inner > .component-wrap > .responsive-nav-cart > .cart-icon-wrapper > .cart-count',
+			)
+				.invoke('text')
+				.then((count) => {
+					countBefore = parseInt(count);
+					cy.findByRole('button', {
+						name: /add to cart/i,
+					}).click();
+					const countExpect = ++countBefore;
+					cy.get(
+						'.header-bottom.hide-on-desktop > .header--row-inner > .container > .row > .right > .builder-item > .item--inner > .component-wrap > .responsive-nav-cart > .cart-icon-wrapper > .cart-count',
+					).should('have.text', countExpect);
 				});
+		});
 
-				//Clicks on the Checkout button
-				cy.findByRole('link', {
-					name: /checkout/i,
-				}).click();
+		it('Update value of products in cart after clearing the cart', function () {
+			cy.visit('/cart');
+			cy.findByRole('link', {
+				name: /remove this item/i,
+			}).click({ multiple: true });
+			cy.findByRole('link', {
+				name: /cart 0/i,
 			});
-		cy.url().should('contain', '/checkout');
+		});
 	});
 });
 
