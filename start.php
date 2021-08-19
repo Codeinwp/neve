6f4d36c36b4b568e9637fcec93efe6fab2c7e4b2
arinaturcu <arinaturcu2@gmail.com>
2021-08-19 11:41:57 +0300

    refactor: repeater control [#2652]

--- start.php
+++ start.php
@@ -13,6 +13,7 @@ function neve_run() {
 		'NEVE_COMPATIBILITY_FEATURES',
 		[
 			'single_customizer' => true,
+			'repeater_control'  => true,
 		] 
 	);
 	$vendor_file = trailingslashit( get_template_directory() ) . 'vendor/autoload.php';
