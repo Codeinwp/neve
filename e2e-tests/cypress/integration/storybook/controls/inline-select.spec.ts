9c0a74b913d95e3551619618838a2bd27183f043
Augusto Silva <augusto@themeisle.com>
2021-08-23 22:29:10 -0300

    test: adds tests for inline select

--- e2e-tests/cypress/integration/storybook/controls/inline-select.spec.ts
+++ e2e-tests/cypress/integration/storybook/controls/inline-select.spec.ts
@@ -5,7 +5,17 @@ describe('Inline Select', function () {
 	context('Default', function () {
 		beforeEach(function () {
 			cy.loadStory('Customizer/Controls/Inline Select', 'Default');
-			cy.get('.value-previewer > pre').as('valuePreview');
+		});
+
+		it('Changes the value on control label', function () {
+			cy.findByRole('combobox', {
+				name: /control label/i,
+			})
+				.as('controlLabel')
+				.should('have.value', 'one');
+			cy.get('@controlLabel').select('two').should('have.value', 'two');
+			cy.get('@controlLabel').select('three').should('have.value', 'three');
+			cy.get('@controlLabel').select('four').should('have.value', 'four');
 		});
 	});
 });
