f48f8ad03455996831f1366916edfdebbb4c171a
Augusto Silva <augusto@themeisle.com>
2021-08-23 22:28:44 -0300

    test: adds tests for global colors picker

--- e2e-tests/cypress/integration/storybook/controls/global-colors-picker.spec.ts
+++ e2e-tests/cypress/integration/storybook/controls/global-colors-picker.spec.ts
@@ -8,5 +8,16 @@ describe('Global Colors Picker', function () {
 			cy.loadStory('Customizer/Controls/Global Colors Picker', 'Default');
 			cy.get('.value-previewer > pre').as('valuePreview');
 		});
+
+		it('Changes the color selected', function () {
+			cy.get('@valuePreview').should('have.text', 'var(--nv-primary-accent)');
+			cy.findByRole('button', {
+				name: /global colors/i,
+			}).click();
+			cy.findByRole('link', {
+				name: /extra color 1/i,
+			}).click();
+			cy.get('@valuePreview').should('have.text', 'var(--nv-c-1)');
+		});
 	});
 });
