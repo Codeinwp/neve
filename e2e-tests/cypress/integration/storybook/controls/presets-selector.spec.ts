675a91a7d94ef92977438cbf6e50ff7a40727c66
Augusto Silva <augusto@themeisle.com>
2021-08-26 23:11:47 -0300

    test: adds spec for component presets selector

--- e2e-tests/cypress/integration/storybook/controls/presets-selector.spec.ts
+++ e2e-tests/cypress/integration/storybook/controls/presets-selector.spec.ts
@@ -8,5 +8,43 @@ describe('Presets Selector', function () {
 			cy.loadStory('Customizer/Controls/Presets Selector', 'Default');
 			cy.get('.value-previewer > pre').as('valuePreview');
 		});
+
+		it('Search for a preset', function () {
+			cy.findByRole('img', {
+				name: /button item/i,
+			});
+			cy.findByRole('img', {
+				name: /classic/i,
+			});
+			cy.findByRole('img', {
+				name: /search field/i,
+			});
+			cy.findByRole('searchbox').type('button');
+			cy.findByRole('img', {
+				name: /button item/i,
+			});
+			cy.findByRole('img', {
+				name: /classic/i,
+			}).should('not.exist');
+			cy.findByRole('img', {
+				name: /search field/i,
+			}).should('not.exist');
+		});
+
+		it('Select a preset from the list', function () {
+			cy.get('@valuePreview').should('have.text', 'null');
+			cy.findByRole('img', {
+				name: /button item/i,
+			}).click();
+			cy.findByText(/"\{\\"theme_mod\\":\\"preset_value_1\\"\}"/i);
+			cy.findByRole('img', {
+				name: /classic/i,
+			}).click();
+			cy.findByText(/"\{\\"theme_mod\\":\\"preset_value_2\\"\}"/i);
+			cy.findByRole('img', {
+				name: /search field/i,
+			}).click();
+			cy.findByText(/"\{\\"theme_mod\\":\\"preset_value_3\\"\}"/i);
+		});
 	});
 });
