9ef013e2cd9bae533ed1e08983ece6197ed2f16a
Augusto Silva <augusto@themeisle.com>
2021-08-26 23:13:02 -0300

    test: adds spec for component toggle

--- e2e-tests/cypress/integration/storybook/controls/toggle.spec.ts
+++ e2e-tests/cypress/integration/storybook/controls/toggle.spec.ts
@@ -5,8 +5,24 @@ describe('Toggle', function () {
 
 	context('Default', function () {
 		beforeEach(function () {
-			cy.loadStory('Customizer/Controls/Togggle', 'Default');
+			cy.loadStory('Customizer/Controls/Toggle', 'Default');
 			cy.get('.value-previewer > pre').as('valuePreview');
+		});
+
+		it('Toggle on and off', function () {
+			cy.get('@valuePreview').should('have.text', 'false');
+			cy.findByRole('checkbox', {
+				name: /control label/i,
+			})
+				.click()
+				.should('be.checked');
+			cy.get('@valuePreview').should('have.text', 'true');
+			cy.findByRole('checkbox', {
+				name: /control label/i,
+			})
+				.click()
+				.should('not.be.checked');
+			cy.get('@valuePreview').should('have.text', 'false');
 		});
 	});
 });
