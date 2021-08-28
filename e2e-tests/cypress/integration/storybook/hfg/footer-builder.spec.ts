9f5aa2fd16417168590f24438e9ccee4fb8c36d3
Augusto Silva <augusto@themeisle.com>
2021-08-28 16:41:07 -0300

    test: adds specs for hfg on storybook
    Does not test the drag and drop functions, since Cypress doesn't handle this tests

---
+++ e2e-tests/cypress/integration/storybook/hfg/footer-builder.spec.ts
@@ -0,0 +1,20 @@
+describe('Footer Builder', function () {
+	before(function () {
+		cy.visitStorybook();
+	});
+	context('Default', function () {
+		beforeEach(function () {
+			cy.loadStory('Customizer/Hfg/Footer Builder', 'Default');
+		});
+
+		it('Adds a component on builder and remove it', function () {
+			cy.get('.left > .droppable > .builder-item').should('not.exist');
+			cy.get('.bottom > .inner-row > .slots-wrap > .left > .components-button').click({
+				force: true,
+			});
+			cy.get(':nth-child(8) > .name').click();
+			cy.get('.left > .droppable > .builder-item > .actions > .remove > svg').click();
+			cy.get('.left > .droppable > .builder-item').should('not.exist');
+		});
+	});
+});
