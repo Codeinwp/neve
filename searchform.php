bc733ab5d7757d5a06b97fa18b54052ff65135cb
cristian-ungureanu <cristian@themeisle.com>
2021-09-01 15:44:17 +0300

    fix: polylang search on another language [#2999]

--- searchform.php
+++ searchform.php
@@ -5,14 +5,18 @@
  * @package Neve.
  */
 
-$placeholder = apply_filters( 'nv_search_placeholder', __( 'Search for...', 'neve' ) );
-$aria_label  = __( 'Search', 'neve' );
+$placeholder     = apply_filters( 'nv_search_placeholder', __( 'Search for...', 'neve' ) );
+$aria_label      = __( 'Search', 'neve' );
+$current_lang    = function_exists( 'pll_current_language' ) ? pll_current_language() : '';
+$default_lang    = function_exists( 'pll_default_language' ) ? pll_default_language() : '';
+$language_subdir = $current_lang !== $default_lang ? $current_lang . '/' : '';
+
 ?>
 
 <form role="search"
 	method="get"
 	class="search-form"
-	action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	action="<?php echo esc_url( home_url( '/' . $language_subdir ) ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php echo esc_html__( 'Search for...', 'neve' ); ?></span>
 	</label>
