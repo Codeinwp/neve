2a6f2e833c80150aa4de2f1655312ddab5de8d2a
selul <marius.cristea@vertistudio.com>
2021-09-03 21:58:07 +0300

    build: fix podiff install [skip e2e]

--- bin/pot-diff.sh
+++ bin/pot-diff.sh
@@ -6,7 +6,6 @@ file2="$2"
 if [[ $(podiff $file1 $file2) ]]; then
 		podiff $file1 $file2
 		echo "::set-output name=has_pot_diff::failure"
-    exit 1
 else
 		echo "::set-output name=has_pot_diff::success"
     echo "No differences found"
