dc78a71988530e75ce7c821129cc9d937a0ff318
selul <marius.cristea@vertistudio.com>
2021-09-03 20:59:29 +0300

    build: fix translation diff, now using podiff

--- bin/pot-diff.sh
+++ bin/pot-diff.sh
@@ -2,23 +2,10 @@
 # Set Arguments
 file1="$1"
 file2="$2"
-PERCENT_TRESHOLD=${3:-2}
 
-# Get new lines between the 2 files
-new=$(diff -u $file1 $file2 | grep -E "^\+msgid" | sed -E 's/^\+//' <<< $(wc -l))
-
-# Get the total lines from the base file
-total=$(cat $file1 | grep -E "^\msgid" | sed -E 's/^\+//' <<< $(wc -l))
-
-# Compute the difference
-pdiff=$(awk '{print $1*100/$2}' <<<"${new} ${total}")
-
-#if [[ $pdiff > $PERCENT_TRESHOLD ]]
-if awk "BEGIN {exit !($pdiff >= $PERCENT_TRESHOLD)}";
-then
-	echo "Greater then $PERCENT_TRESHOLD% difference $pdiff% | new lines $new from total $total"
-	exit 1;
+if [[ $(podiff $file1 $file2) ]]; then
+		podiff $file1 $file2
+    exit 1
 else
-	echo "Less than $PERCENT_TRESHOLD% difference $pdiff% | new lines $new from total $total"
-	exit 0;
+    echo "No differences found"
 fi
