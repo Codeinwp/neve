1c18520fe8cdce34c6cdbe5ac0e47dbc81be5ec4
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2021-09-14 12:10:19 +0300

    refactor: searchform.php updated to able to get params and set form class, input placeholder etc

--- searchform.php
+++ searchform.php
@@ -5,7 +5,17 @@
  * @package Neve.
  */
 
-$placeholder = apply_filters( 'nv_search_placeholder', __( 'Search for...', 'neve' ) );
+$form_classes = [ 'search-form' ];
+
+$placeholder = array_key_exists( 'placeholder', $args ) ? $args['placeholder'] : __( 'Search for...', 'neve' );
+
+if ( array_key_exists( 'additional_form_classes', $args ) && is_array( $args['additional_form_classes'] ) ) {
+	$form_classes = array_merge( $form_classes, $args['additional_form_classes'] );
+}
+
+$value = array_key_exists( 'value', $args ) ? $args['value'] : '';
+
+$placeholder = apply_filters( 'nv_search_placeholder', $placeholder );
 $aria_label  = __( 'Search', 'neve' );
 $home_url    = home_url( '/' );
 if ( function_exists( 'PLL' ) ) {
@@ -19,7 +29,7 @@ if ( function_exists( 'PLL' ) ) {
 
 <form role="search"
 	method="get"
-	class="search-form"
+	class="<?php echo esc_html( implode( ' ', $form_classes ) ); ?>"
 	action="<?php echo esc_url( $home_url ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php echo esc_html__( 'Search for...', 'neve' ); ?></span>
@@ -27,7 +37,7 @@ if ( function_exists( 'PLL' ) ) {
 	<input type="search"
 		class="search-field"
 		placeholder="<?php echo esc_attr( $placeholder ); ?>"
-		value=""
+		value="<?php echo esc_html( $value ); ?>"
 		name="s"/>
 	<button type="submit"
 			class="search-submit"
@@ -36,4 +46,9 @@ if ( function_exists( 'PLL' ) ) {
 			<?php neve_search_icon( false, true ); ?>
 		</span>
 	</button>
+	<?php
+	if ( array_key_exists( 'post_type', $args ) ) {
+		echo '<input type="hidden" name="post_type" value="' . esc_attr( $args['post_type'] ) . '"/>';
+	}
+	?>
 </form>
