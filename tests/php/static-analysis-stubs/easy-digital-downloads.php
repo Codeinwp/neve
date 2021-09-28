9984593634811b9075e9544ccdb680865e59af79
UVLabs <uriahs.victor@gmail.com>
2021-09-27 21:50:24 -0400

    chore: add phpstan stubs

--- tests/php/static-analysis-stubs/easy-digital-downloads.php
+++ tests/php/static-analysis-stubs/easy-digital-downloads.php
@@ -1,1 +1,28 @@
 <?php
+/**
+ * Displays a formatted price for a download
+ *
+ * @since 1.0
+ * @param int $download_id ID of the download price to show
+ * @param bool $echo Whether to echo or return the results
+ * @param int $price_id Optional price id for variable pricing
+ */
+function edd_price( $download_id = 0, $echo = true, $price_id = false ) {}
+
+/**
+ * Checks to see if a download has variable prices enabled.
+ *
+ * @since 1.0.7
+ * @param int $download_id ID number of the download to check
+ * @return bool true if has variable prices, false otherwise
+ */
+function edd_has_variable_prices( $download_id = 0 ) {}
+
+/**
+ * Retrieves a price from from low to high of a variable priced download
+ *
+ * @since 1.4.4
+ * @param int $download_id ID of the download
+ * @return string $range A fully formatted price range
+ */
+function edd_price_range( $download_id = 0 ) {}
