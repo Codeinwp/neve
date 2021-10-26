d48fbf640db76bc62ec233bb347cce1993021fd8
Bogdan Preda <bogdan.preda@themeisle.com>
2021-10-26 12:52:26 +0300

    fix: solved post title alignment and cover defaults #3158

--- inc/core/styles/frontend.php
+++ inc/core/styles/frontend.php
@@ -1141,20 +1141,30 @@ class Frontend extends Generator {
 		}
 
 		$cover_rules = [
-			'--height'  => [
+			'--height'    => [
 				Dynamic_Selector::META_KEY           => Config::MODS_POST_COVER_HEIGHT,
 				Dynamic_Selector::META_IS_RESPONSIVE => true,
 				Dynamic_Selector::META_AS_JSON       => true,
 				Dynamic_Selector::META_SUFFIX        => 'responsive_suffix',
 				Dynamic_Selector::META_DEFAULT       => '{ "mobile": "400", "tablet": "400", "desktop": "400" }',
 			],
-			'--padding' => [
+			'--padding'   => [
 				Dynamic_Selector::META_KEY           => Config::MODS_POST_COVER_PADDING,
 				Dynamic_Selector::META_IS_RESPONSIVE => true,
 				Dynamic_Selector::META_DEFAULT       => $this->padding_default( 'cover' ),
 				'directional-prop'                   => Config::CSS_PROP_PADDING,
 			],
-			'--vAlign'  => [
+			'--justify'   => [
+				Dynamic_Selector::META_KEY           => Config::MODS_POST_COVER_TITLE_ALIGNMENT,
+				Dynamic_Selector::META_IS_RESPONSIVE => true,
+				Dynamic_Selector::META_DEFAULT       => $this->post_title_alignment(),
+			],
+			'--textAlign' => [
+				Dynamic_Selector::META_KEY           => Config::MODS_POST_COVER_TITLE_ALIGNMENT,
+				Dynamic_Selector::META_IS_RESPONSIVE => true,
+				Dynamic_Selector::META_DEFAULT       => $this->post_title_alignment(),
+			],
+			'--vAlign'    => [
 				Dynamic_Selector::META_KEY           => Config::MODS_POST_COVER_TITLE_POSITION,
 				Dynamic_Selector::META_IS_RESPONSIVE => true,
 				Dynamic_Selector::META_DEFAULT       => [
