0da1c2475ec10dcc0ac42a4d1986c5323c2c7feb
Bogdan Preda <bogdan.preda@themeisle.com>
2021-10-27 16:29:41 +0300

    fix: additional check for global wp object

--- assets/js/src/frontend/blog.js
+++ assets/js/src/frontend/blog.js
@@ -105,6 +105,7 @@ const requestMorePosts = () => {
  * @return {*} Sanitized URL.
  */
 const maybeParseUrlForCustomizer = (url) => {
+	if (typeof wp === 'undefined') return url;
 	//Add change-set uuid.
 	if (typeof wp.customize === 'undefined') return url;
 	url +=
