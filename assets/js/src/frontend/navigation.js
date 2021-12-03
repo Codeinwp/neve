a98fa4484a2d112d43b9f79a48bfbb7a8baed980
abaicus <baicusandrei@me.com>
2021-12-03 13:57:29 +0200

    fix: shift+tab on the caret caused double toggle [ref: #3154]

--- assets/js/src/frontend/navigation.js
+++ assets/js/src/frontend/navigation.js
@@ -80,6 +80,9 @@ function handleMobileDropdowns() {
 function openCarrets(e, caret) {
 	e.preventDefault();
 	e.stopPropagation();
+	if (e.keyCode && e.keyCode !== 9) {
+		return;
+	}
 	const subMenu = caret.parentNode.parentNode.querySelector('.sub-menu');
 	toggleClass(caret, strings[0]);
 	toggleClass(subMenu, strings[0]);
