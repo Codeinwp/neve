9f5903f41933068e634e2fd4cc1687d65239f774
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2021-12-07 14:34:51 +0300

    chore: translatable strings added to prevent translations diffs

--- globals/orphaned-i18n-strings.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * The file keeps the orphaned translatable strings to prevent translations diff.
- *
- * @package Neve\Globals
- */
-__( 'Page Layout', 'neve' );
-__( 'Page Title', 'neve' );
