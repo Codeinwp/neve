37c46ba4c016f33993e638ab7f92d0116b84b3f6
UVLabs <uriahs.victor@gmail.com>
2021-12-15 10:22:00 -0400

    chore: change text of starter sites back to original

--- inc/admin/dashboard/main.php
+++ inc/admin/dashboard/main.php
@@ -204,7 +204,7 @@ class Main {
 				/* translators: %s - Theme name */
 				'header'                        => sprintf( __( '%s Options', 'neve' ), wp_kses_post( $theme_name ) ),
 				/* translators: %s - Theme name */
-				'starterSitesCardDescription'   => sprintf( __( '%s comes with a sites library allowing you to import a ready-made website in no time. New sites are constantly being added.', 'neve' ), wp_kses_post( $theme_name ) ),
+				'starterSitesCardDescription'   => sprintf( __( '%s now comes with a sites library with various designs to pick from. Visit our collection of demos that are constantly being added.', 'neve' ), wp_kses_post( $theme_name ) ),
 				/* translators: %s - Theme name */
 				'starterSitesTabDescription'    => sprintf( __( 'With %s, you can choose from multiple unique demos, specially designed for you, that can be installed with a single click. You just need to choose your favorite, and we will take care of everything else.', 'neve' ), wp_kses_post( $theme_name ) ),
 				/* translators: 1 - Theme name, 2 - Cloud Templates & Patterns Collection */
