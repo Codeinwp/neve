a5ccd165d3404ec6bea29ba20db95899006cf98a
cristian-ungureanu <cristian@themeisle.com>
2022-02-14 09:30:46 +0200

    chore: implement featured post [Codeinwp/neve-pro-addon#1105]

--- views/archive-post.php
+++ views/archive-post.php
@@ -4,12 +4,10 @@
  *
  * @package Neve
  */
-
-$featured_post_classes = apply_filters( 'neve_featured_post_classes', '' );
 ?>
 <article id="<?php echo esc_attr( $args['post_id'] ); ?>" class="<?php echo esc_attr( $args['post_class'] ); ?>">
 	<div class="article-content-col">
-		<div class="content <?php echo esc_attr( $featured_post_classes ); ?>">
+		<div class="content">
 			<?php echo $args['content'];  // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 		</div>
 	</div>
