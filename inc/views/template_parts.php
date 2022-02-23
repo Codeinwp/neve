067852d498d8c53ca210f8563dfd6b65d7f76849
Bogdan Preda <bogdan.preda@themeisle.com>
2022-02-23 18:28:27 +0200

    fix: read more button display when columns are too small

--- inc/views/template_parts.php
+++ inc/views/template_parts.php
@@ -282,7 +282,7 @@ class Template_Parts extends Base_View {
 		$markup .= '</a>';
 
 		if ( ! empty( $read_more_args['classes'] ) ) {
-			$style  = neve_is_new_skin() ? '' : 'padding: 10px 0 0;';
+			$style  = neve_is_new_skin() ? 'display: flex;' : 'padding: 10px 0 0;';
 			$markup = '<div class="read-more-wrapper" style="' . esc_attr( $style ) . '">' . $markup . '</div>';
 		}
 
