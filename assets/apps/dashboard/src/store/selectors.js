0d41b1461f4cad7be72f6efd0f9fc3c6c44bf3f1
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2022-03-02 00:56:45 +0300

    refactor: show pro settings in dashboard by auto module config if auto module config is enabled

--- assets/apps/dashboard/src/store/selectors.js
+++ assets/apps/dashboard/src/store/selectors.js
@@ -1,9 +1,25 @@
 export default {
-	getModuleStatus: (state, slug) =>
-		state.settings['nv_pro_' + slug + '_status'],
+	getModuleStatus: (state, slug) => {
+		if( neveDash.autoModuleConfigAvailable ) {
+			return neveDash.autoModuleConfig.activeModules.includes(slug);
+		}
+
+		return state.settings['nv_pro_' + slug + '_status']
+	},
 	getOption: (state, slug) => state.settings[slug],
 	getPlugins: (state) => state.plugins,
-	getProOption: (state, slug) => state.settings['nv_pro_' + slug],
+	getProOption: (state, slug) => {
+		if( neveDash.autoModuleConfigAvailable ) {
+			if(  neveDash.autoModuleConfig.options[slug] ) {
+				return neveDash.autoModuleConfig.options[slug];
+			}
+
+			// fallback to default option values if the option not in the array.
+			return neveDash.autoModuleConfig.defaultOptionVals[slug];
+		}
+
+		return state.settings['nv_pro_' + slug];
+	},
 	getLicenseTier: (state) => (state.license ? state.license.tier : 1),
 	getLicense: (state) => state.license,
 	getToast: (state) => state.toast,
