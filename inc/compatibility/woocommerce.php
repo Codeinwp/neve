dfdbb8f5291d45139fba06838452827a625de480
cristian-ungureanu <cristian@themeisle.com>
2022-04-14 18:17:09 +0300

    fix: check if class exists

--- inc/compatibility/woocommerce.php
+++ inc/compatibility/woocommerce.php
@@ -163,7 +163,8 @@ class Woocommerce {
 		$payment_method = WC()->session->get( 'chosen_payment_method' );
 		if ( ! $payment_method ) {
 			// If payment method is null, see if there is only one option;
-			$available_payment_methods = WC()->payment_gateways->get_available_payment_gateways();
+			$payment_gateways          = new WC_Payment_Gateways();
+			$available_payment_methods = $payment_gateways->get_available_payment_gateways();
 			if ( is_array( $available_payment_methods ) && count( $available_payment_methods ) === 1 ) {
 				return array_keys( $available_payment_methods )[0];
 			}
