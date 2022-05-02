cb5b0a1f8bb121cbd2464837e41b865e4f314e30
cristian-ungureanu <cristian@themeisle.com>
2022-05-02 17:31:37 +0300

    chore: small change

--- inc/views/page_header.php
+++ inc/views/page_header.php
@@ -58,7 +58,7 @@ class Page_Header extends Base_View {
 		}
 		$header_layout = get_theme_mod( 'neve_page_header_layout', 'normal' );
 		if ( $header_layout !== 'normal' && $context === 'single-page' ) {
-			return false;
+			return;
 		}
 
 		$title_args['category_description'] = $this->get_archive_description();
