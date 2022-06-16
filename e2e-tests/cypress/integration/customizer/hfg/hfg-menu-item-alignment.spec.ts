f574d1070898d79af17da016dcaff8546633ce0d
Bogdan Preda <bogdan.preda@themeisle.com>
2022-06-16 12:44:44 +0300

    chore: e2e test update

--- e2e-tests/cypress/integration/customizer/hfg/hfg-menu-item-alignment.spec.ts
+++ e2e-tests/cypress/integration/customizer/hfg/hfg-menu-item-alignment.spec.ts
@@ -13,7 +13,7 @@ describe('Menu item alignment', function () {
 			.contains('About The Tests')
 			.should('have.css', 'text-align', 'left');
 		cy.get(
-			'#nv-primary-navigation-sidebar > .menu-item-1643 > [href="http://localhost:8080/level-1/"] > .caret-wrap',
+			'#nv-primary-navigation-sidebar > .menu-item-1643 > .wrap > [href="http://localhost:8080/level-1/"] > .caret-wrap',
 		).click();
 		cy.get('.menu-item-title-wrap').contains('Level 2').should('have.css', 'text-align', 'left');
 	});
