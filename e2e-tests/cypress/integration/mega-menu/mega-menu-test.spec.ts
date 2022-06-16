1547e7091e247f4aa3bb5d9c3fe44026fa20ff0f
Bogdan Preda <bogdan.preda@themeisle.com>
2022-06-16 12:15:57 +0300

    chore: e2e test update

--- e2e-tests/cypress/integration/mega-menu/mega-menu-test.spec.ts
+++ e2e-tests/cypress/integration/mega-menu/mega-menu-test.spec.ts
@@ -32,7 +32,7 @@ describe('Mega Menu Check', () => {
 
 		cy.get('.header-menu-sidebar .neve-mega-menu').as('mm');
 
-		cy.get('@mm').find(' > a > .caret-wrap').click();
+		cy.get('@mm').find(' > .wrap > .caret-wrap').click();
 
 		cy.get('@mm').find('> .sub-menu').should('be.visible');
 		cy.get('@mm').find('.neve-mm-col').should('be.visible').and('have.length', 4);
