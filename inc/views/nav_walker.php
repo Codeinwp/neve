fbf2dd9e841d8879f5d45eb9466adff55cc2c4a6
Bogdan Preda <bogdan.preda@themeisle.com>
2022-06-16 15:25:55 +0300

    chore: debug e2e

--- inc/views/nav_walker.php
+++ inc/views/nav_walker.php
@@ -65,8 +65,7 @@ class Nav_Walker extends \Walker_Nav_Menu {
 		$expanded = strpos( $args->menu_id, 'sidebar' ) !== false ? 'tabindex="0"' : '';
 
 		if ( in_array( 'menu-item-has-children', $item->classes, true ) ) {
-			$title = '<span class="menu-item-title-wrap dd-title">' . $title . '</span>';
-
+			$title  = '<span class="menu-item-title-wrap dd-title">' . $title . '</span>';
 			$title .= '<div ' . $expanded . ' class="caret-wrap ' . $item->menu_order . '">';
 			$title .= '<span class="caret"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"/></svg></span>';
 			$title .= '</div>';
