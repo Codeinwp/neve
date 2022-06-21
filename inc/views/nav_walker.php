c2b7ecb7524ab587990725524d9767393505f9f9
Bogdan Preda <bogdan.preda@themeisle.com>
2022-06-21 12:35:11 +0300

    fix: double span wrap

--- inc/views/nav_walker.php
+++ inc/views/nav_walker.php
@@ -69,7 +69,9 @@ class Nav_Walker extends \Walker_Nav_Menu {
 		$expanded = $is_sidebar_item ? 'tabindex="0"' : 'tabindex="-1"';
 
 		if ( in_array( 'menu-item-has-children', $item->classes, true ) ) {
-			$title     = '<span class="menu-item-title-wrap dd-title">' . $title . '</span>';
+			if ( strpos( $title, 'menu-item-title-wrap' ) === false ) {
+				$title = '<span class="menu-item-title-wrap dd-title">' . $title . '</span>';
+			}
 			$caret_svg = '<span class="caret"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"/></svg></span>';
 
 			if ( $is_sidebar_item ) {
