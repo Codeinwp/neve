461ac504db1ad127f489266f461742f252509947
cristian-ungureanu <cristian@themeisle.com>
2022-09-21 19:41:44 +0300

    fix: upside down carets on safari

--- assets/apps/components/src/Common/svg.js
+++ assets/apps/components/src/Common/svg.js
@@ -1,3 +1,7 @@
+const rotateStyle = {
+	transform: `rotate(180deg)`,
+};
+
 const SVG = {
 	logoOnly: (
 		<svg
@@ -483,7 +487,7 @@ const SVG = {
 			width="15"
 			height="15"
 			viewBox="0 0 15 15"
-			transform="rotate(180)"
+			style={rotateStyle}
 		>
 			<rect width="15" height="15" fill="none" />
 			<path d="M14,12a1,1,0,0,1-.73-.32L7.5,5.47,1.76,11.65a1,1,0,0,1-1.4,0A1,1,0,0,1,.3,10.3l6.47-7a1,1,0,0,1,1.46,0l6.47,7a1,1,0,0,1-.06,1.4A1,1,0,0,1,14,12Z" />
@@ -497,7 +501,7 @@ const SVG = {
 			width="15"
 			height="15"
 			viewBox="0 0 15 15"
-			transform="rotate(180)"
+			style={rotateStyle}
 		>
 			<rect width="15" height="15" fill="none" />
 			<path d="M2,8.48l-.65-.65a.71.71,0,0,1,0-1L7,1.14a.72.72,0,0,1,1,0l5.69,5.7a.71.71,0,0,1,0,1L13,8.48a.71.71,0,0,1-1,0L8.67,4.94v8.42a.7.7,0,0,1-.7.7H7a.7.7,0,0,1-.7-.7V4.94L3,8.47a.7.7,0,0,1-1,0Z" />
@@ -511,7 +515,7 @@ const SVG = {
 			width="15"
 			height="15"
 			viewBox="0 0 15 15"
-			transform="rotate(180)"
+			style={rotateStyle}
 		>
 			<rect width="15" height="15" fill="none" />
 			<path d="M14.71,10.3l-6.48-7a1,1,0,0,0-1.46,0l-6.48,7A1,1,0,0,0,1,12H14a1,1,0,0,0,.73-1.68Z" />
@@ -525,7 +529,7 @@ const SVG = {
 			width="15"
 			height="15"
 			viewBox="0 0 15 15"
-			transform="rotate(180)"
+			style={rotateStyle}
 		>
 			<rect width="15" height="15" fill="none" />
 			<path d="M13,15a1,1,0,0,1-.74-.32L7.5,9.46l-4.76,5.2A1,1,0,1,1,1.26,13.3l5.5-6a1,1,0,0,1,1.48,0l5.5,6a1,1,0,0,1-.06,1.42A1.05,1.05,0,0,1,13,15Z" />
@@ -540,7 +544,7 @@ const SVG = {
 			width="15"
 			height="15"
 			viewBox="0 0 15 15"
-			transform="rotate(180)"
+			style={rotateStyle}
 		>
 			<rect width="15" height="15" fill="none" />
 			<path d="M2,10.91l-.65-.65a.69.69,0,0,1,0-1L7,3.57a.72.72,0,0,1,1,0l5.69,5.7a.71.71,0,0,1,0,1l-.65.65a.71.71,0,0,1-1,0L8.67,7.37v6.56a.7.7,0,0,1-.7.7H7a.7.7,0,0,1-.7-.7V7.37L3,10.9A.69.69,0,0,1,2,10.91Z" />
@@ -555,7 +559,7 @@ const SVG = {
 			width="15"
 			height="15"
 			viewBox="0 0 15 15"
-			transform="rotate(180)"
+			style={rotateStyle}
 		>
 			<rect width="15" height="15" fill="none" />
 			<path d="M7.86,1.93l5.83,10.2a.8.8,0,0,1-1.08,1.1L8,10.65a.83.83,0,0,0-.78,0L2.39,13.36a.79.79,0,0,1-1.1-1L6.45,2A.8.8,0,0,1,7.86,1.93Z" />
