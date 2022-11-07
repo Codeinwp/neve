8a7df8e5ac4ab0b099bb3e1abd2ce94c4ed9a391
Bogdan Preda <bogdan.preda@themeisle.com>
2022-11-07 16:35:26 +0200

    chore: test otter already present

--- bin/envs/starter-sites/start.sh
+++ bin/envs/starter-sites/start.sh
@@ -1,2 +1,4 @@
-wp --allow-root plugin is-installed templates-patterns-collection  && wp --allow-root plugin update templates-patterns-collection  ||  wp --allow-root plugin install templates-patterns-collection 
-wp --allow-root plugin activate templates-patterns-collection 
+wp --allow-root plugin is-installed templates-patterns-collection  && wp --allow-root plugin update templates-patterns-collection  ||  wp --allow-root plugin install templates-patterns-collection
+wp --allow-root plugin activate templates-patterns-collection
+wp --allow-root plugin is-installed otter-blocks  && wp --allow-root plugin update otter-blocks  ||  wp --allow-root plugin install otter-blocks
+wp --allow-root plugin activate otter-blocks
