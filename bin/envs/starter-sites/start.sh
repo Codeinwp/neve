f979ebb3ae1bf86177fd2d2c2a3a32084547c31f
Bogdan Preda <bogdan.preda@themeisle.com>
2022-11-08 23:38:10 +0200

    chore: add optimole by default and ss test back

--- bin/envs/starter-sites/start.sh
+++ bin/envs/starter-sites/start.sh
@@ -2,3 +2,5 @@ wp --allow-root plugin is-installed templates-patterns-collection  && wp --allow
 wp --allow-root plugin activate templates-patterns-collection
 wp --allow-root plugin is-installed otter-blocks  && wp --allow-root plugin update otter-blocks  ||  wp --allow-root plugin install otter-blocks
 wp --allow-root plugin activate otter-blocks
+wp --allow-root plugin is-installed optimole-wp  && wp --allow-root plugin update optimole-wp  ||  wp --allow-root plugin install optimole-wp
+wp --allow-root plugin activate optimole-wp
