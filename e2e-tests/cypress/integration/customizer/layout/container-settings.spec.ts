397a8361086e839ca140148923d4043613854cfa
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2022-11-09 17:27:03 +0300

    fix: revert container-settings spec changes

--- e2e-tests/cypress/integration/customizer/layout/container-settings.spec.ts
+++ e2e-tests/cypress/integration/customizer/layout/container-settings.spec.ts
@@ -22,7 +22,7 @@ describe('Container Settings', function () {
 				.then((href) => {
 					setup.pageUrl = href;
 				});
-	
+
 			cy.insertPost(setup.postTitle, setup.content, 'post', true);
 			cy.get('.post-publish-panel__postpublish-header a')
 				.contains(setup.postTitle)
@@ -38,27 +38,27 @@ describe('Container Settings', function () {
 				cy.setCustomizeSettings(containerSetup.width);
 			});
 		});
-	
+
 		it('Container width on front end.', function () {
 			cy.visit(setup.pageUrl);
 			cy.get('.single-page-container').as('container');
 			cy.get('@container').should('have.css', 'max-width').and('eq', '1200px');
-	
+
 			cy.viewport(768, 1024); //iPad
 			cy.get('@container').should('have.css', 'max-width').and('eq', '960px');
-	
+
 			cy.viewport(375, 812); //iPhoneX
 			cy.get('@container').should('have.css', 'max-width').and('eq', '680px');
 		});
 	});
-	
+
 	context('Container Style', function () {
 		before('Setup customizer container style.', function () {
 			cy.fixture('customizer/layout/container-setting-setup').then((containerSetup) => {
 				cy.setCustomizeSettings(containerSetup.style);
 			});
 		});
-	
+
 		it('Container style on front end.', function () {
 			cy.visit(setup.pageUrl);
 			cy.get('.single-page-container').as('container');
