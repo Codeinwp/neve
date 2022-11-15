7359d0d8925cfc569ee077e745aa23dda7d0c4eb
cristian-ungureanu <cristian@themeisle.com>
2022-11-15 13:44:39 +0200

    chore: initial playwright setup

---
+++ test/e2e/specs/customizer/hfg/hfg-row-background.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Header Row Background Control', function () {
+	test('Background image control on front end.', function () {
+
+	});
+});
+// describe('Header Row Background Control', function () {
+// 	before('Setup Customizer Control', function () {
+// 		cy.fixture('customizer/hfg/row-background-setup').then((rowSetup) => {
+// 			cy.setCustomizeSettings(rowSetup);
+// 		});
+// 		cy.visit('/');
+// 	});
+//
+// 	it('Background image control on front end.', function () {
+// 		cy.get('.header-main[data-show-on="desktop"] .header--row-inner').as('row');
+//
+// 		cy.get('@row').should('be.visible');
+// 		cy.get('@row').should('have.css', 'background-attachment').and('contain', 'fixed');
+// 		cy.get('@row')
+// 			.should('have.css', 'background-image')
+// 			.and('contain', 'url(')
+// 			.and('contain', 'placeholder.com');
+// 	});
+// });
