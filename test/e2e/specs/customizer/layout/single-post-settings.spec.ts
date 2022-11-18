bc61a23f43167644e29847e1bdab749b12ddbce4
cristian-ungureanu <cristian@themeisle.com>
2022-11-18 18:33:40 +0200

    chore: add single post test

--- test/e2e/specs/customizer/layout/single-post-settings.spec.ts
+++ test/e2e/specs/customizer/layout/single-post-settings.spec.ts
@@ -1,15 +1,21 @@
-import { test, expect } from '@playwright/test';
-import { setCustomizeSettings, loginWithRequest } from '../../utils';
+import { test, expect, Page } from '@playwright/test';
+import { setCustomizeSettings, loginWithRequest, loadData } from '../../utils';
 
 test.describe('Single Post Check', function () {
-	test.beforeAll(async ({ request, baseURL }) => {
-		await setCustomizeSettings(JSON.stringify({ neve_new_skin: 'new' }), {
-			request,
-			baseURL,
-		});
+	/**
+	 * Declare local variables.
+	 */
+	let customizerData;
+	let page: Page;
+
+	test.beforeAll(async ({ browser }) => {
+		page = await browser.newPage();
+		customizerData = await loadData(
+			'./customizer/layout/data/single-post-settings.json'
+		);
 	});
 
-	test('Shows all options on customizer.', async ({ page }) => {
+	test('Shows all options on customizer.', async () => {
 		await loginWithRequest('/wp-admin/customize.php', page);
 		await page
 			.locator('#accordion-panel-neve_layout > .accordion-section-title')
@@ -20,35 +26,12 @@ test.describe('Single Post Check', function () {
 			)
 			.click();
 
-		const visibleSelectors = [
+		const selectors = [
 			'customize-control-neve_post_header_layout_heading',
 			'customize-control-neve_post_header_layout',
-			'customize-control-neve_post_title_alignment',
-			'customize-control-neve_post_page_elements_heading',
-			'customize-control-neve_layout_single_post_elements_order',
-			'customize-control-neve_single_post_elements_spacing',
-			'customize-control-neve_post_meta_heading',
-			'customize-control-neve_single_post_meta_fields',
-			'customize-control-neve_single_post_metadata_separator',
-			'customize-control-neve_single_post_author_avatar',
-			'customize-control-neve_single_post_show_last_updated_date',
-			'customize-control-neve_comments_heading',
-			'customize-control-neve_comment_section_style',
-			'customize-control-neve_post_comments_section_heading',
-			'customize-control-neve_post_comment_section_title',
-			'customize-control-neve_comments_boxed_layout',
-			'customize-control-neve_post_comments_form_heading',
-			'customize-control-neve_post_comment_form_title',
-			'customize-control-neve_post_comment_form_button_style',
-			'customize-control-neve_post_comment_form_button_text',
-			'customize-control-neve_comments_form_boxed_layout',
-			'customize-control-neve_comments_form_boxed_padding',
-			'customize-control-neve_comments_form_boxed_background_color',
-			'customize-control-neve_comments_form_boxed_text_color',
-		];
-		const hiddenSelectors = [
 			'customize-control-neve_post_cover_height',
 			'customize-control-neve_post_cover_padding',
+			'customize-control-neve_post_title_alignment',
 			'customize-control-neve_post_title_position',
 			'customize-control-neve_post_cover_meta_before_title',
 			'customize-control-neve_post_cover_background_color',
@@ -60,60 +43,96 @@ test.describe('Single Post Check', function () {
 			'customize-control-neve_post_cover_title_boxed_layout',
 			'customize-control-neve_post_cover_title_boxed_padding',
 			'customize-control-neve_post_cover_title_boxed_background_color',
+			'customize-control-neve_post_page_elements_heading',
+			'customize-control-neve_layout_single_post_elements_order',
+			'customize-control-neve_single_post_elements_spacing',
+			'customize-control-neve_post_meta_heading',
+			'customize-control-neve_single_post_meta_fields',
+			'customize-control-neve_single_post_metadata_separator',
+			'customize-control-neve_single_post_author_avatar',
 			'customize-control-neve_single_post_avatar_size',
+			'customize-control-neve_single_post_show_last_updated_date',
+			'customize-control-neve_post_comments_section_heading',
+			'customize-control-neve_post_comment_section_title',
+			'customize-control-neve_comments_boxed_layout',
 			'customize-control-neve_comments_boxed_padding',
 			'customize-control-neve_comments_boxed_background_color',
 			'customize-control-neve_comments_boxed_text_color',
-			'customize-control-neve_post_sharing_heading',
-			'customize-control-neve_sharing_icon_style',
-			'customize-control-neve_sharing_enable_custom_color',
-			'customize-control-neve_sharing_icon_color',
-			'customize-control-neve_sharing_custom_color',
-			'customize-control-neve_sharing_icon_size',
-			'customize-control-neve_sharing_icon_padding',
-			'customize-control-neve_sharing_enable_text_label',
-			'customize-control-neve_sharing_label',
-			'customize-control-neve_sharing_label_tag',
-			'customize-control-neve_sharing_label_position',
-			'customize-control-neve_sharing_icons_alignment',
-			'customize-control-neve_sharing_icon_spacing',
-			'customize-control-neve_sharing_icons',
-			'customize-control-neve_post_author_box_heading',
-			'customize-control-neve_author_box_enable_avatar',
-			'customize-control-neve_author_box_avatar_size',
-			'customize-control-neve_author_box_avatar_position',
-			'customize-control-neve_author_box_avatar_border_radius',
-			'customize-control-neve_author_box_enable_archive_link',
-			'customize-control-neve_author_box_content_alignment',
-			'customize-control-neve_author_box_boxed_layout',
-			'customize-control-neve_author_box_boxed_padding',
-			'customize-control-neve_author_box_boxed_background_color',
-			'customize-control-neve_author_box_boxed_text_color',
-			'customize-control-neve_post_related_posts_heading',
-			'customize-control-neve_related_posts_title',
-			'customize-control-neve_related_posts_taxonomy',
-			'customize-control-neve_related_posts_columns',
-			'customize-control-neve_related_posts_number',
-			'customize-control-neve_related_posts_content_alignment',
-			'customize-control-neve_related_posts_excerpt_length',
-			'customize-control-neve_related_posts_boxed_layout',
-			'customize-control-neve_related_posts_boxed_padding',
-			'customize-control-neve_related_posts_boxed_background_color',
-			'customize-control-neve_related_posts_boxed_text_color',
-			'customize-control-neve_related_posts_box_layout_width',
-			'customize-control-neve_related_posts_card',
-			'customize-control-neve_related_posts_post_meta_ordering',
-			'customize-control-neve_related_typography_shortcut',
-			'customize-control-neve_post_post_nav_heading',
-			'customize-control-neve_post_nav_infinite',
+			'customize-control-neve_post_comments_form_heading',
+			'customize-control-neve_post_comment_form_title',
+			'customize-control-neve_post_comment_form_button_style',
+			'customize-control-neve_post_comment_form_button_text',
+			'customize-control-neve_comments_form_boxed_layout',
+			'customize-control-neve_comments_form_boxed_padding',
+			'customize-control-neve_comments_form_boxed_background_color',
+			'customize-control-neve_comments_form_boxed_text_color',
+			'customize-control-neve_single_post_upsell',
 		];
-
-		for (const elementId of visibleSelectors) {
-			await expect(page.locator('#' + elementId)).toBeVisible();
+		for (const elementId of selectors) {
+			await expect(page.locator('#' + elementId)).not.toHaveCount(0);
 		}
+	});
 
-		for (const elementId of hiddenSelectors) {
-			await expect(page.locator('#' + elementId)).not.toBeVisible();
+	test('All page elements are hidden.', async ({ request, baseURL }) => {
+		const data = JSON.stringify(JSON.parse(customizerData).hidden);
+		await setCustomizeSettings(data, { request, baseURL });
+		const HIDDEN = [
+			'.entry-header',
+			'.nv-thumb-wrap',
+			'.entry-content',
+			'.nv-tags-list',
+			'.comments-area',
+			'.nv-post-navigation',
+		];
+		await page.goto('/template-comments/');
+		for (const className of HIDDEN) {
+			await expect(
+				page.locator('.nv-single-post-wrap', {
+					has: page.locator(className),
+				})
+			).toHaveCount(0);
+		}
+	});
+
+	test('All elements enabled and reordered.', async ({
+		request,
+		baseURL,
+	}) => {
+		const data = JSON.stringify(JSON.parse(customizerData).enabled);
+		await setCustomizeSettings(data, { request, baseURL });
+		const ORDER = [
+			'nv-post-navigation',
+			'nv-tags-list',
+			'nv-content-wrap',
+			'comments-area',
+			'entry-header',
+			'nv-thumb-wrap',
+		];
+		await page.goto('/template-comments/');
+		const postElements = await page.locator('.nv-single-post-wrap > *');
+		for (let i = 0; i < (await postElements.count()); i++) {
+			await expect(postElements.nth(i)).toHaveClass(
+				new RegExp(`${ORDER[i]}`)
+			);
+		}
+	});
+
+	test('Header Cover', async ({ request, baseURL }) => {
+		const data = JSON.stringify(JSON.parse(customizerData).cover);
+		await setCustomizeSettings(data, { request, baseURL });
+		const urls = ['/hello-world', '/markup-text-alignment', '/scheduled'];
+		for (const url of urls) {
+			await page.goto(url);
+			const overlay = await page.locator('.nv-overlay');
+			await expect(overlay).toHaveCSS('height', '505px');
+			await expect(overlay).toHaveCSS(
+				'background-color',
+				'rgb(198, 53, 53)'
+			);
+			await expect(page.locator('.nv-title-meta-wrap')).toHaveCSS(
+				'color',
+				'rgb(255, 255, 255)'
+			);
 		}
 	});
 });
