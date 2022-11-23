d0cf9be63875726c1b5a2271552c92f4f4062bbe
cristian-ungureanu <cristian@themeisle.com>
2022-11-23 20:28:00 +0200

    chore: add layout tests

---
+++ test/e2e/specs/customizer/layout/sidebar-settings.spec.ts
@@ -0,0 +1,69 @@
+// describe('Sidebar/Content Settings', function () {
+// 	context('Sidebar site wide on front end.', function () {
+// 		beforeEach(function () {
+// 			cy.fixture('customizer/layout/sidebar-settings').then((sidebarSetup) => {
+// 				cy.setCustomizeSettings(sidebarSetup.site_wide);
+// 			});
+// 		});
+//
+// 		it('Page', function () {
+// 			cy.visit('/sample-page');
+// 			cy.reload();
+// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '50%');
+// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
+// 			cy.get('.nv-single-page-wrap').should('have.css', 'max-width', '50%');
+// 		});
+// 		it('Author archive', function () {
+// 			cy.visit('/?author=1');
+// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '50%');
+// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
+// 			cy.get('.nv-index-posts').should('have.css', 'max-width', '50%');
+// 		});
+// 		it('Single Post', function () {
+// 			cy.visit('/hello-world');
+// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '50%');
+// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
+// 			cy.get('.nv-single-post-wrap').should('have.css', 'max-width', '50%');
+// 		});
+// 		it.skip('Index', function () {
+// 			cy.visit('/');
+// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '50%');
+// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
+// 			cy.get('.nv-index-posts').should('have.css', 'max-width', '50%');
+// 		});
+// 	});
+//
+// 	context('Sidebar advanced on front end.', function () {
+// 		beforeEach(function () {
+// 			cy.fixture('customizer/layout/sidebar-settings').then((sidebarSetup) => {
+// 				cy.setCustomizeSettings(sidebarSetup.advanced);
+// 			});
+// 		});
+//
+// 		it('Page', function () {
+// 			cy.visit('/sample-page');
+// 			cy.reload();
+// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '30%');
+// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
+// 			cy.get('.nv-single-page-wrap').should('have.css', 'max-width', '70%');
+// 		});
+// 		it('Author archive', function () {
+// 			cy.visit('/?author=1');
+// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '80%');
+// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
+// 			cy.get('.nv-index-posts').should('have.css', 'max-width', '20%');
+// 		});
+// 		it('Single Post', function () {
+// 			cy.visit('/hello-world');
+// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '30%');
+// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-right');
+// 			cy.get('.nv-single-post-wrap').should('have.css', 'max-width', '70%');
+// 		});
+// 		it.skip('Index', function () {
+// 			cy.visit('/');
+// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '80%');
+// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
+// 			cy.get('.nv-index-posts').should('have.css', 'max-width', '20%');
+// 		});
+// 	});
+// });
--- test/e2e/utils.ts
+++ test/e2e/utils.ts
@@ -1,11 +1,12 @@
 const { promises: fsPromise } = require('fs');
 const path = require('path');
-import { test, expect } from '@playwright/test';
+import { expect } from '@playwright/test';
+import { addQueryArgs } from '@wordpress/url';
 
 /**
  * Load data form file.
  *
- * @param filePath
+ * @param {string} filePath Path of the file to read.
  */
 export const loadData = async (filePath) => {
 	const jsonPath = path.join(__dirname, filePath);
@@ -15,12 +16,19 @@ export const loadData = async (filePath) => {
 /**
  * Set customizer settings.
  *
+ * @param testname
  * @param data
  * @param root0
  * @param root0.request
  * @param root0.baseURL
+ * @param root0.request.request
+ * @param root0.request.baseURL
  */
-export const setCustomizeSettings = async (testname, data, { request, baseURL }) => {
+export const setCustomizeSettings = async (
+	testname,
+	data,
+	{ request, baseURL }
+) => {
 	const customizerData = JSON.parse(data);
 	const response = await request.post(
 		baseURL + '/wp-json/wpthememods/v1/settings/' + testname,
@@ -48,3 +56,62 @@ export const loginWithRequest = async (nextRoute = '/wp-admin', page) => {
 	}
 	await page.goto(nextRoute);
 };
+
+/**
+ * Creates new post.
+ *
+ * @param {Object}  object                    Object to create new post, along with tips enabling option.
+ * @param {string}  [object.postType]         Post type of the new post.
+ * @param {string}  [object.title]            Title of the new post.
+ * @param {string}  [object.content]          Content of the new post.
+ * @param {string}  [object.excerpt]          Excerpt of the new post.
+ * @param {boolean} [object.showWelcomeGuide] Whether to show the welcome guide.
+ */
+export async function createNewPost({
+	postType,
+	title,
+	content,
+	excerpt,
+	showWelcomeGuide = false,
+} = {}) {
+	const query = addQueryArgs('', {
+		post_type: postType,
+		post_title: title,
+		content,
+		excerpt,
+	}).slice(1);
+
+	await this.visitAdminPage('post-new.php', query);
+
+	await this.page.waitForSelector('.edit-post-layout');
+
+	const isWelcomeGuideActive = await this.page.evaluate(() =>
+		window.wp.data.select('core/edit-post').isFeatureActive('welcomeGuide')
+	);
+	const isFullscreenMode = await this.page.evaluate(() =>
+		window.wp.data
+			.select('core/edit-post')
+			.isFeatureActive('fullscreenMode')
+	);
+
+	if (showWelcomeGuide !== isWelcomeGuideActive) {
+		await this.page.evaluate(() =>
+			window.wp.data
+				.dispatch('core/edit-post')
+				.toggleFeature('welcomeGuide')
+		);
+
+		await this.page.reload();
+		await this.page.waitForSelector('.edit-post-layout');
+	}
+
+	if (isFullscreenMode) {
+		await this.page.evaluate(() =>
+			window.wp.data
+				.dispatch('core/edit-post')
+				.toggleFeature('fullscreenMode')
+		);
+
+		await this.page.waitForSelector('body:not(.is-fullscreen-mode)');
+	}
+}
