f26e6afcb986ee48bd15fcb6675ae6071da79e52
cristian-ungureanu <cristian@themeisle.com>
2022-11-24 17:02:29 +0200

    chore: add sidebar settings and post settings

--- test/e2e/specs/customizer/layout/sidebar-settings.spec.ts
+++ test/e2e/specs/customizer/layout/sidebar-settings.spec.ts
@@ -1,69 +1,124 @@
-// describe('Sidebar/Content Settings', function () {
-// 	context('Sidebar site wide on front end.', function () {
-// 		beforeEach(function () {
-// 			cy.fixture('customizer/layout/sidebar-settings').then((sidebarSetup) => {
-// 				cy.setCustomizeSettings(sidebarSetup.site_wide);
-// 			});
-// 		});
-//
-// 		it('Page', function () {
-// 			cy.visit('/sample-page');
-// 			cy.reload();
-// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '50%');
-// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
-// 			cy.get('.nv-single-page-wrap').should('have.css', 'max-width', '50%');
-// 		});
-// 		it('Author archive', function () {
-// 			cy.visit('/?author=1');
-// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '50%');
-// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
-// 			cy.get('.nv-index-posts').should('have.css', 'max-width', '50%');
-// 		});
-// 		it('Single Post', function () {
-// 			cy.visit('/hello-world');
-// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '50%');
-// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
-// 			cy.get('.nv-single-post-wrap').should('have.css', 'max-width', '50%');
-// 		});
-// 		it.skip('Index', function () {
-// 			cy.visit('/');
-// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '50%');
-// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
-// 			cy.get('.nv-index-posts').should('have.css', 'max-width', '50%');
-// 		});
-// 	});
-//
-// 	context('Sidebar advanced on front end.', function () {
-// 		beforeEach(function () {
-// 			cy.fixture('customizer/layout/sidebar-settings').then((sidebarSetup) => {
-// 				cy.setCustomizeSettings(sidebarSetup.advanced);
-// 			});
-// 		});
-//
-// 		it('Page', function () {
-// 			cy.visit('/sample-page');
-// 			cy.reload();
-// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '30%');
-// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
-// 			cy.get('.nv-single-page-wrap').should('have.css', 'max-width', '70%');
-// 		});
-// 		it('Author archive', function () {
-// 			cy.visit('/?author=1');
-// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '80%');
-// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
-// 			cy.get('.nv-index-posts').should('have.css', 'max-width', '20%');
-// 		});
-// 		it('Single Post', function () {
-// 			cy.visit('/hello-world');
-// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '30%');
-// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-right');
-// 			cy.get('.nv-single-post-wrap').should('have.css', 'max-width', '70%');
-// 		});
-// 		it.skip('Index', function () {
-// 			cy.visit('/');
-// 			cy.get('.nv-sidebar-wrap').should('have.css', 'max-width', '80%');
-// 			cy.get('.nv-sidebar-wrap').should('have.class', 'nv-left');
-// 			cy.get('.nv-index-posts').should('have.css', 'max-width', '20%');
-// 		});
-// 	});
-// });
+import { test, expect } from '@playwright/test';
+import { setCustomizeSettings, loadData } from '../../../utils';
+
+test.describe('Sidebar/Content Settings', () => {
+	let customizerData;
+
+	test.beforeAll(async () => {
+		customizerData = await loadData(
+			'./fixtures/customizer/layout/sidebar-settings.json'
+		);
+	});
+
+	test('Sidebar site wide on front end.', async ({
+		page,
+		request,
+		baseURL,
+	}) => {
+		const data = JSON.stringify(JSON.parse(customizerData).site_wide);
+		await setCustomizeSettings('sidebarSitewide', data, {
+			request,
+			baseURL,
+		});
+
+		await test.step('Page', async () => {
+			await page.goto('/sample-page/?test_name=sidebarSitewide');
+			const sidebarWrap = await page.locator('.nv-sidebar-wrap');
+			await expect(sidebarWrap).toHaveCSS('max-width', '50%');
+			await expect(sidebarWrap).toHaveClass(/nv-left/);
+			await expect(await page.locator('.nv-single-page-wrap')).toHaveCSS(
+				'max-width',
+				'50%'
+			);
+		});
+
+		await test.step('Author archive', async () => {
+			await page.goto('/?author=1&test_name=sidebarSitewide');
+			const sidebarWrap = await page.locator('.nv-sidebar-wrap');
+			await expect(sidebarWrap).toHaveCSS('max-width', '50%');
+			await expect(sidebarWrap).toHaveClass(/nv-left/);
+			await expect(await page.locator('.nv-index-posts')).toHaveCSS(
+				'max-width',
+				'50%'
+			);
+		});
+
+		await test.step('Single Post', async () => {
+			await page.goto('/hello-world/?test_name=sidebarSitewide');
+			const sidebarWrap = await page.locator('.nv-sidebar-wrap');
+			await expect(sidebarWrap).toHaveCSS('max-width', '50%');
+			await expect(sidebarWrap).toHaveClass(/nv-left/);
+			await expect(await page.locator('.nv-single-post-wrap')).toHaveCSS(
+				'max-width',
+				'50%'
+			);
+		});
+
+		await test.step('Index', async () => {
+			await page.goto('/?test_name=sidebarSitewide');
+			const sidebarWrap = await page.locator('.nv-sidebar-wrap');
+			await expect(sidebarWrap).toHaveCSS('max-width', '50%');
+			await expect(sidebarWrap).toHaveClass(/nv-left/);
+			await expect(await page.locator('.nv-index-posts')).toHaveCSS(
+				'max-width',
+				'50%'
+			);
+		});
+	});
+
+	test('Sidebar advanced on front end.', async ({
+		page,
+		request,
+		baseURL,
+	}) => {
+		const data = JSON.stringify(JSON.parse(customizerData).advanced);
+		await setCustomizeSettings('sidebarAdvanced', data, {
+			request,
+			baseURL,
+		});
+
+		await test.step('Page', async () => {
+			await page.goto('/sample-page/?test_name=sidebarAdvanced');
+			const sidebarWrap = await page.locator('.nv-sidebar-wrap');
+			await expect(sidebarWrap).toHaveCSS('max-width', '30%');
+			await expect(sidebarWrap).toHaveClass(/nv-left/);
+			await expect(await page.locator('.nv-single-page-wrap')).toHaveCSS(
+				'max-width',
+				'70%'
+			);
+		});
+
+		await test.step('Author archive', async () => {
+			await page.goto('/?author=1&test_name=sidebarAdvanced');
+			const sidebarWrap = await page.locator('.nv-sidebar-wrap');
+			await expect(sidebarWrap).toHaveCSS('max-width', '80%');
+			await expect(sidebarWrap).toHaveClass(/nv-left/);
+			await expect(await page.locator('.nv-index-posts')).toHaveCSS(
+				'max-width',
+				'20%'
+			);
+		});
+
+		await test.step('Single Post', async () => {
+			await page.goto('/hello-world/?test_name=sidebarAdvanced');
+			const sidebarWrap = await page.locator('.nv-sidebar-wrap');
+			await expect(sidebarWrap).toHaveCSS('max-width', '30%');
+			await expect(sidebarWrap).toHaveClass(/nv-right/);
+			await expect(await page.locator('.nv-single-post-wrap')).toHaveCSS(
+				'max-width',
+				'70%'
+			);
+		});
+
+		await test.step('Index', async () => {
+			await page.goto('/?test_name=sidebarAdvanced');
+			const sidebarWrap = await page.locator('.nv-sidebar-wrap');
+			await expect(sidebarWrap).toHaveCSS('max-width', '80%');
+			await expect(sidebarWrap).toHaveClass(/nv-left/);
+			await expect(await page.locator('.nv-index-posts')).toHaveCSS(
+				'max-width',
+				'20%'
+			);
+		});
+	});
+});
