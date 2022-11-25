eac60e5646d105c9baaac9fabbb719957a8ee6c8
cristian-ungureanu <cristian@themeisle.com>
2022-11-25 14:01:48 +0200

    chore: add font family check

--- test/e2e/specs/customizer/typography/font-family.spec.ts
+++ test/e2e/specs/customizer/typography/font-family.spec.ts
@@ -1,37 +1,84 @@
-// const fonts = {
-// 	general: 'Arapey',
-// 	headings: 'Allerta Stencil',
-// };
-//
-// describe('Font Family', function () {
-// 	before(function () {
-// 		cy.loginWithRequest();
-// 		cy.fixture('customizer/typography/font-family').then((fontSetup) => {
-// 			cy.setCustomizeSettings(fontSetup);
-// 		});
-// 	});
-//
-// 	it('Font Family on Front End', function () {
-// 		cy.visit('/markup-html-tags-and-formatting/');
-//
-// 		cy.get('body').should('have.css', 'font-family').and('match', new RegExp(fonts.general, 'g'));
-//
-// 		cy.get('h1, h2, h3, h4, h5, h6')
-// 			.should('have.css', 'font-family')
-// 			.and('match', new RegExp(fonts.headings, 'g'));
-// 	});
-//
-// 	it('Test Font Family inside the Editor', function () {
-// 		cy.visit('/markup-html-tags-and-formatting/');
-// 		cy.get('#wp-admin-bar-edit > a').click();
-// 		cy.clearWelcome();
-// 		cy.get('.editor-styles-wrapper.block-editor-writing-flow p').as('body');
-// 		cy.get('@body').should('have.css', 'font-family').and('match', new RegExp(fonts.general, 'g'));
-//
-// 		['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].forEach((heading) => {
-// 			cy.get(`.editor-styles-wrapper ${heading}`)
-// 				.should('have.css', 'font-family')
-// 				.and('match', new RegExp(fonts.headings, 'g'));
-// 		});
-// 	});
-// });
+import { test, expect } from '@playwright/test';
+import {
+	loadData,
+	setCustomizeSettings,
+	loginWithRequest,
+	clearWelcome,
+} from '../../../utils';
+
+const fonts = {
+	general: 'Arapey',
+	headings: 'Allerta Stencil',
+};
+
+test.describe('Font Family', () => {
+
+	const headingSelectors = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+
+	test.beforeAll(async ({ request, baseURL }) => {
+		await loadData(
+			'./fixtures/customizer/typography/font-family.json'
+		).then(async (data) => {
+			await setCustomizeSettings('fontFamily', data, {
+				request,
+				baseURL,
+			});
+		});
+	});
+
+	test('Font Family on Front End', async ({ page }) => {
+		await page.goto(
+			'/markup-html-tags-and-formatting/?test_name=fontFamily'
+		);
+
+		await expect(await page.locator('body')).toHaveCSS(
+			'font-family',
+			new RegExp(`${fonts.general}`)
+		);
+
+		for (const heading of headingSelectors) {
+			const headings = await page.locator(heading);
+			for (let index = 0; index < (await headings.count()); index++) {
+				await expect(await headings.nth(index)).toHaveCSS(
+					'font-family',
+					new RegExp(`${fonts.headings}`)
+				);
+			}
+		}
+	});
+
+	test('Test Font Family inside the Editor', async ({ page }) => {
+		await loginWithRequest(
+			'/markup-html-tags-and-formatting/?test_name=fontFamily',
+			page
+		);
+		const editPageLink = await page
+			.locator('#wp-admin-bar-edit > a')
+			.getAttribute('href');
+		await page.goto(editPageLink + '&test_name=fontFamily');
+
+		await clearWelcome(page);
+
+		const pElements = await page.locator(
+			'.editor-styles-wrapper.block-editor-writing-flow p'
+		);
+		for (let index = 0; index < (await pElements.count()); index++) {
+			await expect(await pElements.nth(index)).toHaveCSS(
+				'font-family',
+				new RegExp(`${fonts.general}`)
+			);
+		}
+
+		for (const heading of headingSelectors) {
+			const headings = await page.locator(
+				'.editor-styles-wrapper ' + heading
+			);
+			for (let index = 0; index < (await headings.count()); index++) {
+				await expect(await headings.nth(index)).toHaveCSS(
+					'font-family',
+					new RegExp(`${fonts.headings}`)
+				);
+			}
+		}
+	});
+});
--- test/e2e/utils.ts
+++ test/e2e/utils.ts
@@ -57,61 +57,75 @@ export const loginWithRequest = async (nextRoute = '/wp-admin', page) => {
 	await page.goto(nextRoute);
 };
 
-/**
- * Creates new post.
- *
- * @param {Object}  object                    Object to create new post, along with tips enabling option.
- * @param {string}  [object.postType]         Post type of the new post.
- * @param {string}  [object.title]            Title of the new post.
- * @param {string}  [object.content]          Content of the new post.
- * @param {string}  [object.excerpt]          Excerpt of the new post.
- * @param {boolean} [object.showWelcomeGuide] Whether to show the welcome guide.
- */
-export async function createNewPost({
-	postType,
-	title,
-	content,
-	excerpt,
-	showWelcomeGuide = false,
-} = {}) {
-	const query = addQueryArgs('', {
-		post_type: postType,
-		post_title: title,
-		content,
-		excerpt,
-	}).slice(1);
-
-	await this.visitAdminPage('post-new.php', query);
-
-	await this.page.waitForSelector('.edit-post-layout');
-
-	const isWelcomeGuideActive = await this.page.evaluate(() =>
-		window.wp.data.select('core/edit-post').isFeatureActive('welcomeGuide')
-	);
-	const isFullscreenMode = await this.page.evaluate(() =>
-		window.wp.data
-			.select('core/edit-post')
-			.isFeatureActive('fullscreenMode')
-	);
-
-	if (showWelcomeGuide !== isWelcomeGuideActive) {
-		await this.page.evaluate(() =>
+export const clearWelcome = async (page) => {
+	await page.evaluate(() => {
+		// eslint-disable-next-line no-unused-expressions
+		window.wp &&
+			window.wp.data &&
+			window.wp.data
+				.select('core/edit-post')
+				.isFeatureActive('welcomeGuide') &&
 			window.wp.data
 				.dispatch('core/edit-post')
-				.toggleFeature('welcomeGuide')
-		);
-
-		await this.page.reload();
-		await this.page.waitForSelector('.edit-post-layout');
-	}
-
-	if (isFullscreenMode) {
-		await this.page.evaluate(() =>
-			window.wp.data
-				.dispatch('core/edit-post')
-				.toggleFeature('fullscreenMode')
-		);
-
-		await this.page.waitForSelector('body:not(.is-fullscreen-mode)');
-	}
-}
+				.toggleFeature('welcomeGuide');
+	});
+};
+//
+// /**
+//  * Creates new post.
+//  *
+//  * @param {Object}  object                    Object to create new post, along with tips enabling option.
+//  * @param {string}  [object.postType]         Post type of the new post.
+//  * @param {string}  [object.title]            Title of the new post.
+//  * @param {string}  [object.content]          Content of the new post.
+//  * @param {string}  [object.excerpt]          Excerpt of the new post.
+//  * @param {boolean} [object.showWelcomeGuide] Whether to show the welcome guide.
+//  */
+// export async function createNewPost({
+// 	postType,
+// 	title,
+// 	content,
+// 	excerpt,
+// 	showWelcomeGuide = false,
+// } = {}) {
+// 	const query = addQueryArgs('', {
+// 		post_type: postType,
+// 		post_title: title,
+// 		content,
+// 		excerpt,
+// 	}).slice(1);
+//
+// 	await this.visitAdminPage('post-new.php', query);
+//
+// 	await this.page.waitForSelector('.edit-post-layout');
+//
+// 	const isWelcomeGuideActive = await this.page.evaluate(() =>
+// 		window.wp.data.select('core/edit-post').isFeatureActive('welcomeGuide')
+// 	);
+// 	const isFullscreenMode = await this.page.evaluate(() =>
+// 		window.wp.data
+// 			.select('core/edit-post')
+// 			.isFeatureActive('fullscreenMode')
+// 	);
+//
+// 	if (showWelcomeGuide !== isWelcomeGuideActive) {
+// 		await this.page.evaluate(() =>
+// 			window.wp.data
+// 				.dispatch('core/edit-post')
+// 				.toggleFeature('welcomeGuide')
+// 		);
+//
+// 		await this.page.reload();
+// 		await this.page.waitForSelector('.edit-post-layout');
+// 	}
+//
+// 	if (isFullscreenMode) {
+// 		await this.page.evaluate(() =>
+// 			window.wp.data
+// 				.dispatch('core/edit-post')
+// 				.toggleFeature('fullscreenMode')
+// 		);
+//
+// 		await this.page.waitForSelector('body:not(.is-fullscreen-mode)');
+// 	}
+// }
