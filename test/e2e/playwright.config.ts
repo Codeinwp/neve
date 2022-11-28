c4c932259704b37729c930dc0c3a9e3363fc34b8
cristian-ungureanu <cristian@themeisle.com>
2022-11-28 11:49:50 +0200

    chore: edit config files

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -1,43 +1,21 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-	testDir: './specs',
-	/* Maximum time one test can run for. */
-	timeout: 30 * 1000,
-	expect: {
-		/**
-		 * Maximum time expect() should wait for the condition to be met.
-		 * For example in `await expect(locator).toHaveText();`
-		 */
-		timeout: 5000,
-	},
-	/* Run tests in files in parallel */
-	// fullyParallel: true,
-	/* Fail the build on CI if you accidentally left test.only in the source code. */
+	reporter: process.env.CI ? 'github' : 'list',
 	forbidOnly: !!process.env.CI,
-	/* Retry on CI only */
+	workers: process.env.CI ? 6 : undefined,
 	retries: process.env.CI ? 2 : 0,
-	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
-	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: 'html',
-	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+	timeout: parseInt(process.env.TIMEOUT || '', 10) || 100_000, // Defaults to 100 seconds.
+	testDir: './specs',
+	fullyParallel: true,
 	use: {
-		// browserName: 'chromium',
-		// headless: true,
-		ignoreHTTPSErrors: true,
 		baseURL: process.env.baseURL || 'http://localhost:8080',
-		// trace: 'on-first-retry',
-		trace: 'on',
+		headless: true,
+		ignoreHTTPSErrors: true,
+		trace: 'retain-on-failure',
 		screenshot: 'only-on-failure',
 		video: 'on-first-retry',
 	},
