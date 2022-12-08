3a1e39a538f9e900af721bdc8d3597148deaed21
cristian-ungureanu <cristian@themeisle.com>
2022-12-08 10:37:46 +0200

    fix: phpstan

--- inc/compatibility/woocommerce.php
+++ inc/compatibility/woocommerce.php
@@ -266,7 +266,7 @@ class Woocommerce {
 		if ( is_checkout() ) {
 			$page_id        = get_the_ID();
 			$elementor_data = get_post_meta( $page_id, '_elementor_data', true );
-			if ( ! empty( $elementor_data ) && is_string( $elementor_data ) && strpos( $elementor_data, 'woocommerce-checkout-page' ) !== false ) {
+			if ( ! empty( $elementor_data ) && is_string( $elementor_data ) && strpos( $elementor_data, 'woocommerce-checkout-page' ) ) {
 				$is_elementor_checkout = true;
 			}
 		}
