2d059abbf0f0ff06f7e365c67ee7b0c62dea4d5a
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2022-12-15 16:07:25 +0300

    fix: wip - some fixes for pr#3743

--- inc/admin/dashboard/main.php
+++ inc/admin/dashboard/main.php
@@ -236,7 +236,7 @@ class Main {
 			'tpcPath'                 => defined( 'TIOB_PATH' ) ? TIOB_PATH . 'template-patterns-collection.php' : 'template-patterns-collection/template-patterns-collection.php',
 			'tpcAdminURL'             => admin_url( 'themes.php?page=tiob-starter-sites' ),
 			'pluginsURL'              => esc_url( admin_url( 'plugins.php' ) ),
-			'getPluginStateBaseURL'   => get_rest_url( null, '/nv/v1/dashboard/plugin-state/' ),
+			'getPluginStateBaseURL'   => esc_url( rest_url( null, '/nv/v1/dashboard/plugin-state/' ) ),
 		];
 
 		if ( defined( 'NEVE_PRO_PATH' ) ) {
