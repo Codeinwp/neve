44214f41ee5173d25f8a1b3f968a5b3f5c444043
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2022-12-28 21:49:36 +0300

    Revert "chore: chore: wip - custom global colors - update slug too when custom color label has changed"
    This reverts commit ed271ab7285f1c328b8f009aa479a23137c5b814.

--- assets/apps/customizer-controls/src/global-custom-colors/PaletteColors.js
+++ assets/apps/customizer-controls/src/global-custom-colors/PaletteColors.js
@@ -9,7 +9,6 @@ import { __ } from '@wordpress/i18n';
 import { debounce } from 'lodash';
 import { Accordion, ColorControl } from '@neve-wp/components';
 import { useState } from '@wordpress/element';
-import { cleanForSlug } from '@wordpress/url';
 import EditColorLabel from './EditColorLabel';
 
 const PaletteColors = ({ values, save }) => {
@@ -74,23 +73,13 @@ const PaletteColors = ({ values, save }) => {
 		closeDeleteModal();
 	};
 
-	// updates label&slug
 	const editLabel = (slug, newLabel) => {
 		const nextValues = { ...values };
 		if (nextValues[slug].label === newLabel) {
 			setWillEdit('');
 			return false;
 		}
-
-		// TODO: duplicate check can be added.
-
-		nextValues[cleanForSlug(newLabel)] = {
-			label: newLabel,
-			val: nextValues[slug].val,
-		};
-
-		delete nextValues[slug];
-
+		nextValues[slug].label = newLabel;
 		save(nextValues);
 		setWillEdit('');
 	};
