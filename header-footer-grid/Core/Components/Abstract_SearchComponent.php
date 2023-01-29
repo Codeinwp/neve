39954a63b79d47d9bd87c4930f81f3009e0a90d8
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2023-01-29 13:38:41 +0300

    chore: wip - customizable HFG search icons / optional button

--- header-footer-grid/Core/Components/Abstract_SearchComponent.php
+++ header-footer-grid/Core/Components/Abstract_SearchComponent.php
@@ -18,6 +18,7 @@ use HFG\Core\Settings\Manager as SettingsManager;
 abstract class Abstract_SearchComponent extends Abstract_Component {
 	const ACTION_TYPE = 'action_type';
 	const ICON_TYPE   = 'icon_type';
+	const CUSTOM_SVG  = 'icon_c_svg';
 
 	/**
 	 * Has support for the search button instead of icon?
@@ -41,7 +42,7 @@ abstract class Abstract_SearchComponent extends Abstract_Component {
 				'id'                 => self::ACTION_TYPE,
 				'group'              => $this->get_class_const( 'COMPONENT_ID' ),
 				'tab'                => SettingsManager::TAB_STYLE,
-				'transport'          => 'postMessage',
+				'transport'          => 'refresh',
 				'label'              => __( 'Action Type', 'neve' ),
 				'default'            => 'icon',
 				'type'               => '\Neve\Customizer\Controls\React\Radio_Buttons',
@@ -69,21 +70,49 @@ abstract class Abstract_SearchComponent extends Abstract_Component {
 	 *
 	 * @return void
 	 */
-	private function add_customize_icon_control() {
-		SettingsManager::get_instance()->add(
+	private function add_customize_icon_controls() {
+		$sm = SettingsManager::get_instance();
+		$sm::get_instance()->add(
 			[
 				'id'                 => self::ICON_TYPE,
 				'group'              => $this->get_class_const( 'COMPONENT_ID' ),
 				'tab'                => SettingsManager::TAB_STYLE,
-				'transport'          => 'postMessage',
+				'transport'          => 'refresh',
 				'label'              => __( 'Icon Type', 'neve' ),
-				'default'            => 'icon',
+				'default'            => 'hfgs-icon-style-1',
 				'type'               => '\Neve\Customizer\Controls\React\Radio_Buttons',
 				'section'            => $this->section,
 				'conditional_header' => true,
 				'options'            => [
-					'is_for'      => 'search_icon',
-					'show_labels' => false,
+					'is_for'          => 'search_icon',
+					'show_labels'     => false,
+					'active_callback' => function() use ( $sm ) {
+						$action_type = $this->get_class_const( 'COMPONENT_ID' ) . '_' . self::ACTION_TYPE;
+						return ( ! $this->has_button_support ) || $sm->get( $action_type, 'icon' ) === 'icon';
+					},
+				],
+			]
+		);
+
+		$sm::get_instance()->add(
+			[
+				'id'                 => self::CUSTOM_SVG,
+				'group'              => $this->get_class_const( 'COMPONENT_ID' ),
+				'tab'                => SettingsManager::TAB_STYLE,
+				'transport'          => 'postMessage',
+				'label'              => __( 'Custom SVG Content', 'neve' ),
+				'default'            => 'icon',
+				'type'               => '\Neve\Customizer\Controls\React\Textarea',
+				'section'            => $this->section,
+				'conditional_header' => true,
+				'options'            => [
+					'active_callback' => function() use ( $sm ) {
+						$icon_type = $this->get_class_const( 'COMPONENT_ID' ) . '_' . self::ICON_TYPE;
+						return $sm->get( $icon_type, 'hfgs-icon-style-1' ) === 'hfgs-icon-custom';
+					},
+					'input_attrs'     => [
+						'rows' => 8,
+					],
 				],
 			]
 		);
@@ -97,6 +126,6 @@ abstract class Abstract_SearchComponent extends Abstract_Component {
 	public function define_settings() {
 		parent::define_settings();
 		$this->add_action_type_control();
-		$this->add_customize_icon_control();
+		$this->add_customize_icon_controls();
 	}
 }
