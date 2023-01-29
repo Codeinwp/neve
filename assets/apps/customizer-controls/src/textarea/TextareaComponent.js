208c5115f8003f2599c3ccdb0f08fc3798f711c9
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2023-01-29 13:36:45 +0300

    feat: new customizer textarea control was added

---
+++ assets/apps/customizer-controls/src/textarea/TextareaComponent.js
@@ -0,0 +1,39 @@
+import { Textarea } from '@neve-wp/components';
+import { useState } from '@wordpress/element';
+import PropTypes from 'prop-types';
+
+const TextareaComponent = ({ control }) => {
+	const defaultParams = {
+		rows: 10,
+	};
+
+	const controlParams = control.params.input_attrs
+		? {
+				...defaultParams,
+				...control.params.input_attrs,
+		  }
+		: defaultParams;
+
+	const [value, setValue] = useState(control.setting.get());
+	const { label } = control.params;
+
+	const updateValues = (newVal) => {
+		setValue(newVal);
+		control.setting.set(newVal);
+	};
+
+	return (
+		<Textarea
+			value={value}
+			label={label}
+			onChange={updateValues}
+			rows={controlParams.rows}
+		/>
+	);
+};
+
+TextareaComponent.propTypes = {
+	control: PropTypes.object.isRequired,
+};
+
+export default TextareaComponent;
