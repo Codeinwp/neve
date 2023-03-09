672f841236bbc28884af05fc06f8c3dadad6860e
Bogdan Preda <bogdan.preda@themeisle.com>
2023-03-09 12:10:14 +0200

    fix: solve woo loop links when hooks is displayed

--- inc/admin/hooks_upsells.php
+++ inc/admin/hooks_upsells.php
@@ -131,6 +131,7 @@ class Hooks_Upsells {
 		 }
 		.nv-hook-placeholder a .nv-hook-upsell {
 			font-size: 14px;
+			line-height: 16px;
 			font-weight: 600;
 			padding: 3px 2px;
 			margin-left: -2px;
@@ -162,17 +163,27 @@ class Hooks_Upsells {
 		echo esc_attr( Dynamic_Css::minify_css( $this->get_css() ) );
 		echo '</style>';
 		$upsell_label = __( 'Neve PRO Features', 'neve' ) . ' / ' . __( 'Learn more', 'neve' );
+
+		// These hooks have to be removed as to not have nested links that will break the layout.
+		// We don't need them when the hooks are displayed, as the action will be replaced by the displayed hook action.
+		remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+		remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
+
 		foreach ( $hooks as $hooks_in_category ) {
 			foreach ( $hooks_in_category as $hook_value ) {
 				$hook_label = self::beautify_hook( $hook_value );
 				add_action(
 					$hook_value,
-					function () use ( $hook_label, $upsell_label ) {
+					function () use ( $hook_value, $hook_label, $upsell_label ) {
+						$style = '';
+						if ( 'woocommerce_before_shop_loop_item' === $hook_value ) {
+							$style = 'max-width: 200px;';
+						}
 						$upsell_url = tsdk_utmify( 'https://themeisle.com/themes/neve/upgrade/', 'viewhooks' );
 						echo '<div class="nv-hook-wrapper nv-hook-upsell-wrapper">';
 						echo '<div class="nv-hook-placeholder">';
 						echo '<a href="' . esc_url( $upsell_url ) . '" title="' . esc_attr( $upsell_label ) . '" target="_blank">';
-						echo '<span class="nv-hook-upsell">' . esc_html( $upsell_label ) . '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" class="components-external-link__icon" role="img" aria-hidden="true" focusable="false" style="fill: #0073AA"><path d="M18.2 17c0 .7-.6 1.2-1.2 1.2H7c-.7 0-1.2-.6-1.2-1.2V7c0-.7.6-1.2 1.2-1.2h3.2V4.2H7C5.5 4.2 4.2 5.5 4.2 7v10c0 1.5 1.2 2.8 2.8 2.8h10c1.5 0 2.8-1.2 2.8-2.8v-3.6h-1.5V17zM14.9 3v1.5h3.7l-6.4 6.4 1.1 1.1 6.4-6.4v3.7h1.5V3h-6.3z"></path></svg></span>';
+						echo '<span class="nv-hook-upsell" style="' . esc_attr( $style ) . '">' . esc_html( $upsell_label ) . '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" class="components-external-link__icon" role="img" aria-hidden="true" focusable="false" style="fill: #0073AA"><path d="M18.2 17c0 .7-.6 1.2-1.2 1.2H7c-.7 0-1.2-.6-1.2-1.2V7c0-.7.6-1.2 1.2-1.2h3.2V4.2H7C5.5 4.2 4.2 5.5 4.2 7v10c0 1.5 1.2 2.8 2.8 2.8h10c1.5 0 2.8-1.2 2.8-2.8v-3.6h-1.5V17zM14.9 3v1.5h3.7l-6.4 6.4 1.1 1.1 6.4-6.4v3.7h1.5V3h-6.3z"></path></svg></span>';
 						echo '<span class="nv-hook-name"><span class="dashicons dashicons-lock"></span>' . esc_html( $hook_label ) . '</span>';
 						echo '</a>';
 						echo '</div>';
