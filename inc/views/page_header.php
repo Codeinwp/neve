Codeinwp/neve

commit 1c906f388ac31c13b87599faf7a820ceb4793ab7

fix: do not render page header for woocommerce pages too if the header layout is not normal

inc/views/page_header.php
@@ -57,7 +57,7 @@ class Page_Header extends Base_View {
 			return;
 		}
 		$header_layout = get_theme_mod( 'neve_page_header_layout', 'normal' );
-		if ( $header_layout !== 'normal' && $context === 'single-page' ) {
+		if ( $header_layout !== 'normal' ) {
 			return;
 		}
 

commit 246da997113e812ee24ba11dce9498b98b84ee06

fix: regression is caused by 1c906f3 was fixed (preserve post category pages title behavior)

inc/views/page_header.php
@@ -57,7 +57,7 @@ class Page_Header extends Base_View {
 			return;
 		}
 		$header_layout = get_theme_mod( 'neve_page_header_layout', 'normal' );
-		if ( $header_layout !== 'normal' ) {
+		if ( $header_layout !== 'normal' && in_array( $context, [ 'single-page', 'woo-page' ] ) ) {
 			return;
 		}
 