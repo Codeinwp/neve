30b6bac6ea2c3945808cdaee7022b715e49da0bd
cristian-ungureanu <cristian@themeisle.com>
2023-03-28 19:00:05 +0300

    chore: improve test coverage

--- e2e-tests/specs/woocommerce/header-mini-cart.spec.ts
+++ e2e-tests/specs/woocommerce/header-mini-cart.spec.ts
@@ -41,43 +41,43 @@ const testForViewport = async (
 	}
 };
 
-// test.describe('Header Mini Cart Functionality', function () {
-// 	test.beforeAll(async ({ request, baseURL }) => {
-// 		await setCustomizeSettings(
-// 			'mini-cart-functionality',
-// 			data.functionality,
-// 			{
-// 				request,
-// 				baseURL,
-// 			}
-// 		);
-// 	});
-//
-// 	test('Open the cart page when clicking on mini cart', async ({ page }) => {
-// 		await page.goto('/shop/?test_name=mini-cart-functionality');
-// 		await page.locator('.nv-icon > svg > path').click();
-// 		await expect(page).toHaveURL(/cart/);
-// 	});
-//
-// 	test('Value of products is updated when adding or removing a product', async ({
-// 		page,
-// 	}) => {
-// 		// Add product to cart
-// 		await page.goto('/product/album/?test_name=mini-cart-functionality');
-// 		await expect(await page.locator('.cart-count').innerText()).toBe('0');
-// 		await page.locator('.single_add_to_cart_button').click();
-// 		await page.goto('/product/album/?test_name=mini-cart-functionality');
-// 		await expect(await page.locator('.cart-count').innerText()).toBe('1');
-//
-// 		// Removes the product from cart
-// 		await page.goto('/product/album/?test_name=mini-cart-functionality');
-// 		await page.hover('.nv-icon > svg');
-// 		await page.waitForSelector('.nv-nav-cart');
-// 		await page.locator('.nv-nav-cart .remove').click();
-// 		await page.waitForTimeout(500);
-// 		await expect(await page.locator('.cart-count').innerText()).toBe('0');
-// 	});
-// });
+test.describe('Header Mini Cart Functionality', function () {
+	test.beforeAll(async ({ request, baseURL }) => {
+		await setCustomizeSettings(
+			'mini-cart-functionality',
+			data.functionality,
+			{
+				request,
+				baseURL,
+			}
+		);
+	});
+
+	test('Open the cart page when clicking on mini cart', async ({ page }) => {
+		await page.goto('/shop/?test_name=mini-cart-functionality');
+		await page.locator('.nv-icon > svg > path').click();
+		await expect(page).toHaveURL(/cart/);
+	});
+
+	test('Value of products is updated when adding or removing a product', async ({
+		page,
+	}) => {
+		// Add product to cart
+		await page.goto('/product/album/?test_name=mini-cart-functionality');
+		await expect(await page.locator('.cart-count').innerText()).toBe('0');
+		await page.locator('.single_add_to_cart_button').click();
+		await page.goto('/product/album/?test_name=mini-cart-functionality');
+		await expect(await page.locator('.cart-count').innerText()).toBe('1');
+
+		// Removes the product from cart
+		await page.goto('/product/album/?test_name=mini-cart-functionality');
+		await page.hover('.nv-icon > svg');
+		await page.waitForSelector('.nv-nav-cart');
+		await page.locator('.nv-nav-cart .remove').click();
+		await page.waitForTimeout(500);
+		await expect(await page.locator('.cart-count').innerText()).toBe('0');
+	});
+});
 
 test.describe('Header Mini Cart Style', function () {
 	test('Check the mini cart style', async ({ page, request, baseURL }) => {
@@ -87,29 +87,45 @@ test.describe('Header Mini Cart Style', function () {
 		});
 
 		await page.goto('/shop/?test_name=mini-cart-style');
-		const viewports = [
+		await testForViewport(
+			page,
+			'.nv-icon > svg',
 			{ width: 1536, height: 960 },
-			{ width: 768, height: 960 },
-			{ width: 375, height: 960 },
-		];
-		for (const viewport of viewports) {
-			await testForViewport(page, '.nv-icon > svg', viewport, {
+			{
 				cssProperties: [
 					{ property: 'color', value: 'rgb(118, 5, 5)' },
 					{ property: 'fill', value: 'rgb(118, 5, 5)' },
 					{ property: 'width', value: '80px' },
 				],
-			});
+			}
+		);
 
-			await testForViewport(page, '.nv-icon > svg', viewport, {
+		await testForViewport(
+			page,
+			'[data-item-id="header_cart_icon"]',
+			{ width: 1536, height: 960 },
+			{
 				cssProperties: [
-					{ property: 'color', value: 'rgb(118, 5, 5)' },
-					{ property: 'fill', value: 'rgb(118, 5, 5)' },
-					{ property: 'width', value: '80px' },
+					{ property: 'padding-bottom', value: '0px' },
+					{ property: 'padding-top', value: '0px' },
+					{ property: 'padding-left', value: '10px' },
+					{ property: 'padding-right', value: '10px' },
+					{ property: 'margin-bottom', value: '0px' },
+					{ property: 'margin-top', value: '0px' },
+					{ property: 'margin-left', value: '0px' },
+					{ property: 'margin-right', value: '0px' },
 				],
-			});
+			}
+		);
 
-
-		}
+		await page.hover('.nv-icon > svg');
+		await expect(await page.locator('.nv-icon > svg').first()).toHaveCSS(
+			'color',
+			'rgb(18, 90, 50)'
+		);
+		await expect(await page.locator('.nv-icon > svg').first()).toHaveCSS(
+			'fill',
+			'rgb(18, 90, 50)'
+		);
 	});
 });
