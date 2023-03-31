07791e7037cc0965dbf32618d2b9874235be51d0
cristian-ungureanu <cristian@themeisle.com>
2023-03-31 17:30:24 +0300

    chore: add e2e tests for the starter sites

---
+++ e2e-tests/specs/visual-regression/starter-sites.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Starter sites check', () => {
+	test('Check frontpage', async ({ page }) => {
+		const starterSites = {
+			'Web Agency': ['https://staging.demosites.io/web-agency-gb/'],
+			Blogger: ['https://staging.demosites.io/blogger-gb/'],
+			Shop: ['https://staging.demosites.io/shop-gb/'],
+			'Neve 3': ['https://staging.demosites.io/neve-3-gb/'],
+			Photography: ['https://staging.demosites.io/rokophoto-gb/'],
+			Fitness: ['https://staging.demosites.io/fitness-gb/'],
+			'Pet Shop': ['https://staging.demosites.io/pet-shop-gb/'],
+			Restaurant: ['https://staging.demosites.io/restaurant-gb/'],
+			'Vacation Rental': [
+				'https://staging.demosites.io/vacation-rental-gb/',
+			],
+		};
+
+		for ( const [name, urls] of Object.entries(starterSites)) {
+			await test.step(`Check ${name} starter site.`, async () => {
+				for (const url of urls) {
+					await page.goto(url + '?optml_off=true');
+					await page.waitForTimeout(500);
+					await expect(page).toHaveScreenshot({
+						maxDiffPixelRatio: 0.05,
+						fullPage: true,
+					});
+				}
+			});
+		}
+	});
+});
