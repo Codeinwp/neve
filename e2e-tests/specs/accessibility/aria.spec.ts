26b148934439f13082f16b9548beb72c852645b8
Bogdan Preda <bogdan.preda@themeisle.com>
2023-04-19 14:34:11 +0300

    chore: e2e test per device

--- e2e-tests/specs/accessibility/aria.spec.ts
+++ e2e-tests/specs/accessibility/aria.spec.ts
@@ -1,48 +1,50 @@
 import { test, expect, devices } from '@playwright/test';
 
-const runMenuARIATest = () => {
-	test('Test ARIA Menu Expanded', async ({page}) => {
-		await page.goto('/');
+const runMenuARIATest = (deviceType = 'mobile') => {
+	test('Test ARIA Menu Expanded', async ({ page }) => {
+		await page.goto('/?device=' + deviceType);
 
 		// Check ARIA attribute for expanded state is false when menu is closed.
 		await expect(
-			page.getByRole('button', {name: 'Navigation Menu'})
+			page.getByRole('button', { name: 'Navigation Menu' })
 		).toHaveAttribute('aria-expanded', 'false');
-		await page.getByRole('button', {name: 'Navigation Menu'}).click();
+		await page.getByRole('button', { name: 'Navigation Menu' }).click();
 
 		// Check ARIA attribute for expanded state is true when menu is open.
 		await expect(
-			page.getByRole('button', {name: 'Navigation Menu'})
+			page.getByRole('button', { name: 'Navigation Menu' })
 		).toHaveAttribute('aria-expanded', 'true');
 
 		// Close the menu from the overlay. Check ARIA attribute for expanded state is false when menu is closed.
-		await page.locator('.header-menu-sidebar-overlay').click({force: true});
+		await page
+			.locator('.header-menu-sidebar-overlay')
+			.click({ force: true });
 		await expect(
-			page.getByRole('button', {name: 'Navigation Menu'})
+			page.getByRole('button', { name: 'Navigation Menu' })
 		).toHaveAttribute('aria-expanded', 'false');
 
 		// Open the menu from the button. Check ARIA attribute for expanded state is true when menu is open.
-		await page.getByRole('button', {name: 'Navigation Menu'}).click();
+		await page.getByRole('button', { name: 'Navigation Menu' }).click();
 		await expect(
-			page.getByRole('button', {name: 'Navigation Menu'})
+			page.getByRole('button', { name: 'Navigation Menu' })
 		).toHaveAttribute('aria-expanded', 'true');
 
 		// Close the menu from the button. Check ARIA attribute for expanded state is false when menu is closed.
-		await page.getByRole('button', {name: 'Navigation Menu'}).click();
+		await page.getByRole('button', { name: 'Navigation Menu' }).click();
 		await expect(
-			page.getByRole('button', {name: 'Navigation Menu'})
+			page.getByRole('button', { name: 'Navigation Menu' })
 		).toHaveAttribute('aria-expanded', 'false');
 
 		// Open the menu from the button. Check ARIA attribute for expanded state is true when menu is open.
-		await page.getByRole('button', {name: 'Navigation Menu'}).click();
+		await page.getByRole('button', { name: 'Navigation Menu' }).click();
 		await expect(
-			page.getByRole('button', {name: 'Navigation Menu'})
+			page.getByRole('button', { name: 'Navigation Menu' })
 		).toHaveAttribute('aria-expanded', 'true');
 
 		// Close the menu from the escape key. Check ARIA attribute for expanded state is false when menu is closed.
 		await page.keyboard.press('Escape');
 		await expect(
-			page.getByRole('button', {name: 'Navigation Menu'})
+			page.getByRole('button', { name: 'Navigation Menu' })
 		).toHaveAttribute('aria-expanded', 'false');
 	});
 };
@@ -52,7 +54,7 @@ test.use({
 });
 
 test.describe('Menu ARIA iPhone 13', () => {
-	runMenuARIATest();
+	runMenuARIATest('iPhone13');
 });
 
 test.use({
@@ -60,5 +62,5 @@ test.use({
 });
 
 test.describe('Menu ARIA Pixel 5', () => {
-	runMenuARIATest();
+	runMenuARIATest('pixel5');
 });
