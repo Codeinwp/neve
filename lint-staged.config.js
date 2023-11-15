937f9417de639b8dcc3c6c040f434bf2947601b6
Soare Robert Daniel (Mac 2023) <soare.robert.daniel@protonmail.com>
2023-11-15 17:08:31 +0200

    Revert "chore: go back to yarn"
    This reverts commit ecd35901bba9e42654fcce84910116a80ca567e5.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,32 +4,32 @@ module.exports = {
 		"composer run lint"
 	],
 	"package.json": [
-		"yarn run lint:package-json"
+		"npm run lint:package-json"
 	],
 	"/assets/apps/dashboard/src/**/*.js": [
-		"yarn run format:dash",
-		"yarn run lint:dash"
+		"npm run format:dash",
+		"npm run lint:dash"
 	],
 	"assets/apps/metabox/src/**/*.js": [
-		"yarn run format:dash",
-		"yarn run lint:metabox"
+		"npm run format:dash",
+		"npm run lint:metabox"
 	],
 	"assets/scss/components/**/*.scss": [
-		"yarn run lint:scss --fix"
+		"npm run lint:scss --fix"
 	],
 	"assets/js/src/**/*.js": () => [ // We ignore here the staged restriction.
-		"yarn run format:global",
-		"yarn run lint:global",
-		"yarn run build:rollup",
-		"yarn run size"
+		"npm run format:global",
+		"npm run lint:global",
+		"npm run build:rollup",
+		"npm run size"
 	],
 	"assets/scss/**/*.scss": () => [ // We ignore here the staged restriction.
-		"yarn run build:grunt",
-		"yarn run size"
+		"npm run build:grunt",
+		"npm run size"
 	],
 	"assets/apps/customizer-controls/src/**/*.js": [ // We ignore here the staged restriction.
-		"yarn run format:customizer",
-		"yarn run lint:customizer",
+		"npm run format:customizer",
+		"npm run lint:customizer",
 	],
 
 }
