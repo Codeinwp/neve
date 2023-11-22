a0b59002c15430743f80f54e864ac667e3806a07
Bogdan Preda <bogdan.preda@themeisle.com>
2023-11-22 17:13:35 +0200

    chore: updated woo visual regression test

--- e2e-tests/specs/woo-visual-regression/checkout.spec.ts
+++ e2e-tests/specs/woo-visual-regression/checkout.spec.ts
@@ -7,12 +7,19 @@ test.describe('Checkout page check', () => {
 		await page.goto('/checkout/');
 		await page.getByRole('button', { name: 'Place Order' }).click();
 		await page.waitForTimeout(1000);
-		await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.2 });
+		await page
+			.getByRole('heading', { name: 'Checkout' })
+			.scrollIntoViewIfNeeded();
+		await expect(page).toHaveScreenshot({
+			maxDiffPixelRatio: 0.2,
+		});
 	});
 
 	test('Check MyAccount errors', async ({ page }) => {
 		await page.goto('/my-account/edit-account/');
 		await page.locator('.woocommerce-Button').first().click();
-		await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.2 });
+		await expect(page).toHaveScreenshot({
+			maxDiffPixelRatio: 0.2,
+		});
 	});
 });
