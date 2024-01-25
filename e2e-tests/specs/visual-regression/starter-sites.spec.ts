d4892e93769977998e7fd0800441a046380da7a0
Bogdan Preda <bogdan.preda@themeisle.com>
2024-01-25 17:57:56 +0200

    chore: update mexDiffPixelRatio and e2e visual regression

--- e2e-tests/specs/visual-regression/starter-sites.spec.ts
+++ e2e-tests/specs/visual-regression/starter-sites.spec.ts
@@ -26,7 +26,7 @@ test.describe('Starter sites check', () => {
 					await page.evaluate(() => window.scrollTo(0, 0));
 					await page.waitForTimeout(500);
 					await expect(page).toHaveScreenshot({
-						maxDiffPixelRatio: 0.15,
+						maxDiffPixelRatio: 0.18,
 						fullPage: true,
 					});
 				}
