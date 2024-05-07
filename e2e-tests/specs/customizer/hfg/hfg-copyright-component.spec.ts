bf8fc9d1b1b6f5de42a152064b22ee73225bc2a4
Bogdan Preda <bogdan.preda@themeisle.com>
2024-05-07 14:45:53 +0300

    chore: add e2e test

--- e2e-tests/specs/customizer/hfg/hfg-copyright-component.spec.ts
+++ e2e-tests/specs/customizer/hfg/hfg-copyright-component.spec.ts
@@ -6,8 +6,30 @@ test('Checks the copyright in front-end', async ({ page }) => {
 	await expect(
 		page.locator('.hfg_footer .footer--row[data-show-on="desktop"]')
 	).toHaveText('Neve | Powered by WordPress');
+	await expect(
+		page.locator(
+			'.hfg_footer .footer--row[data-show-on="desktop"] a:first-child'
+		)
+	).toHaveAttribute('href', 'https://themeisle.com/themes/neve/');
+	await expect(
+		page.locator(
+			'.hfg_footer .footer--row[data-show-on="desktop"] a:nth-child[1]'
+		)
+	).toHaveAttribute('href', 'https://wordpress.org');
+
 	// Check that the text is also present for the Mobile footer
 	await expect(
 		page.locator('.hfg_footer .footer--row[data-show-on="mobile"]')
 	).toHaveText('Neve | Powered by WordPress');
+
+	await expect(
+		page.locator(
+			'.hfg_footer .footer--row[data-show-on="mobile"] a:first-child'
+		)
+	).toHaveAttribute('href', 'https://themeisle.com/themes/neve/');
+	await expect(
+		page.locator(
+			'.hfg_footer .footer--row[data-show-on="mobile"] a:nth-child[1]'
+		)
+	).toHaveAttribute('href', 'https://wordpress.org');
 });
