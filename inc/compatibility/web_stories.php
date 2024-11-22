32231279032ecccb323fb4dbcf046d7e75cc25d0
girishpanchal30 <girish@krishaweb.com>
2024-11-22 16:24:31 +0530

    fix: shop page sidebar issue with latest release

--- inc/compatibility/web_stories.php
+++ inc/compatibility/web_stories.php
@@ -37,7 +37,7 @@ class Web_Stories {
 	 * Load hooks.
 	 */
 	private function load_hooks() {
-		add_action( 'init', array( $this, 'setup' ) );
+		$this->setup();
 		add_action( 'wp_body_open', array( $this, 'embed' ) );
 	}
 
