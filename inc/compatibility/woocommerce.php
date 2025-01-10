04d14ca69c7f7928c54cacb835b5848f7421795b
girishpanchal30 <girish@krishaweb.com>
2025-01-10 16:39:47 +0530

    fix: wrap elements issue with EL custom header

--- inc/compatibility/woocommerce.php
+++ inc/compatibility/woocommerce.php
@@ -284,8 +284,13 @@ class Woocommerce {
 		add_action( 'admin_footer', array( $this, 'update_woo_width' ) );
 
 		// Wrap content.
-		add_action( 'neve_after_primary_start', array( $this, 'wrap_pages_start' ) );
-		add_action( 'neve_before_primary_end', array( $this, 'wrap_pages_end' ) );
+		if ( function_exists( 'elementor_theme_do_location' ) && elementor_theme_do_location( 'header' ) ) {
+			add_action( 'woocommerce_before_main_content', array( $this, 'wrap_pages_start' ) );
+			add_action( 'woocommerce_after_main_content', array( $this, 'wrap_pages_end' ) );
+		} else {
+			add_action( 'neve_after_primary_start', array( $this, 'wrap_pages_start' ) );
+			add_action( 'neve_before_primary_end', array( $this, 'wrap_pages_end' ) );
+		}
 
 		add_action( 'woocommerce_before_main_content', array( $this, 'wrap_main_content_start' ), 15 );
 		add_action( 'woocommerce_after_main_content', array( $this, 'close_div' ), 15 );
