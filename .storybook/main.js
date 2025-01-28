02cd4221eb12a5d6be18e63908e274b567909410
abaicus <baicusandrei@me.com>
2025-01-29 01:03:05 +0200

    chore: attempt to fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,56 +1,72 @@
-const path = require('path');
-const modulesDir = path.join( __dirname, '../node_modules' );
-const componentsFile = path.join(__dirname, '../assets/apps/components/src/components.js')
+const path = require("path");
+const modulesDir = path.join(__dirname, "../node_modules");
+const componentsFile = path.join(
+	__dirname,
+	"../assets/apps/components/src/components.js"
+);
 const stories = [
 	"../stories/**/*.stories.mdx",
-	"../stories/**/*.stories.@(js|jsx|ts|tsx)"
+	"../stories/**/*.stories.@(js|jsx|ts|tsx)",
 ];
 const customEnvVariables = {};
 
 // Workaround for Emotion 11
 // https://github.com/storybookjs/storybook/pull/13300#issuecomment-783268111
-const updateEmotionAliases = ( config ) => ( {
+const updateEmotionAliases = (config) => ({
 	...config,
 	resolve: {
 		...config.resolve,
 		alias: {
 			...config.resolve.alias,
-			'@neve-wp/components': componentsFile,
-			'@emotion/core': path.join( modulesDir, '@emotion/react' ),
-			'@emotion/styled': path.join( modulesDir, '@emotion/styled' ),
-			'@emotion/styled-base': path.join( modulesDir, '@emotion/styled' ),
-			'emotion-theming': path.join( modulesDir, '@emotion/react' ),
+			"@neve-wp/components": componentsFile,
+			"@emotion/core": path.join(modulesDir, "@emotion/react"),
+			"@emotion/styled": path.join(modulesDir, "@emotion/styled"),
+			"@emotion/styled-base": path.join(modulesDir, "@emotion/styled"),
+			"emotion-theming": path.join(modulesDir, "@emotion/react"),
 		},
 	},
-} );
-
-
+});
 module.exports = {
 	core: {
-		builder: 'webpack5',
+		builder: "webpack5",
 	},
 	stories,
-	"addons": [
+	addons: [
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
 		"@storybook/preset-scss",
-		'@storybook/addon-a11y',
+		"@storybook/addon-a11y",
 	],
 	managerWebpack: updateEmotionAliases,
 	// Workaround:
 	// https://github.com/storybookjs/storybook/issues/12270
-	webpackFinal: async ( config ) => {
+	webpackFinal: async (config) => {
 		// Find the DefinePlugin
-		const plugin = config.plugins.find( ( p ) => {
-			return p.definitions && p.definitions[ 'process.env' ];
-		} );
+		const plugin = config.plugins.find((p) => {
+			console.log(p);
+			return p.definitions && p.definitions["process.env"];
+		});
 		// Add custom env variables
-		Object.keys( customEnvVariables ).forEach( ( key ) => {
-			plugin.definitions[ 'process.env' ][ key ] = JSON.stringify(
-				customEnvVariables[ key ]
+		Object.keys(customEnvVariables).forEach((key) => {
+			plugin.definitions["process.env"][key] = JSON.stringify(
+				customEnvVariables[key]
 			);
-		} );
+		});
 
-		return updateEmotionAliases( config );
+		// Add TypeScript support
+		config.module.rules.push({
+			test: /\.(ts|tsx)$/,
+			loader: require.resolve("ts-loader"),
+		});
+		config.resolve.extensions.push(".ts", ".tsx");
+
+		return updateEmotionAliases(config);
 	},
-}
+	framework: {
+		name: "@storybook/react-webpack5",
+		builder: "webpack5",
+		options: {
+			legacyRootApi: true,
+		},
+	},
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,30 +1,33 @@
-import './style.css'
+import "./style.css";
 
-try {
-	require('../e2e-tests/node_modules/cypress-storybook/react');
-} catch (ex) {
-	console.log(ex);
-}
-import '@wordpress/components/build-style/style.css'
-import '@icon/dashicons/dashicons.css'
-import {FONTS} from '../stories/utils/values'
-import {BuildersData} from "./dummy-data";
+// try {
+// 	require('../e2e-tests/node_modules/cypress-storybook/react');
+// } catch (ex) {
+// 	console.log(ex);
+// }
+import "@wordpress/components/build-style/style.css";
+import "@icon/dashicons/dashicons.css";
+import { FONTS } from "../stories/utils/values";
+import { BuildersData } from "./dummy-data";
 
-window.NeveReactCustomize = {}
-window.nvComponents = {}
-window.NeveReactCustomize.HFG = BuildersData
+window.NeveReactCustomize = {};
+window.nvComponents = {};
+window.NeveReactCustomize.HFG = BuildersData;
 
 const wpObject = {
 	customize: {
-		bind: (event, callback) => {
-		},
+		bind: (event, callback) => {},
 		previewedDevice: (device) => {
-			const wrap = document.querySelector('.mock-customize');
+			const wrap = document.querySelector(".mock-customize");
 
 			if (wrap.classList.contains(`preview-${device}`)) {
 				return false;
 			}
-			wrap.classList.remove('preview-desktop', 'preview-mobile', 'preview-tablet')
+			wrap.classList.remove(
+				"preview-desktop",
+				"preview-mobile",
+				"preview-tablet"
+			);
 			wrap.classList.add(`preview-${device}`);
 		},
 		section: (slug) => ({
@@ -32,70 +35,69 @@ const wpObject = {
 			expanded: (value) => console.log(`Set Expanded for ${slug} to ${value}.`),
 			params: {
 				title: `Title: ${slug}`,
-			}
+			},
 		}),
 		state: (slug) => ({
 			get: () => {
-				return 'title_tagline'
+				return "title_tagline";
 			},
 			bind: (a) => {
-				console.log('State Bounded and returns:', a());
+				console.log("State Bounded and returns:", a());
 			},
 			unbind: (a) => {
-				console.log('State Unbounded and returns:', a());
-
-			}
+				console.log("State Unbounded and returns:", a());
+			},
 		}),
 		previewer: {
 			bind: (e) => {
-				console.log('Event Bound:', e);
+				console.log("Event Bound:", e);
 			},
 			send: (e) => {
-				console.log(e)
+				console.log(e);
 			},
 			refresh: () => {
-				console.log('REFRESH PREVIEW');
-			}
+				console.log("REFRESH PREVIEW");
+			},
 		},
 		control: (slug, callback) => {
 			if (callback) {
-				callback(window.wp.customize.control(slug))
+				callback(window.wp.customize.control(slug));
 			}
 
-			if (slug.indexOf('_columns_layout') > -1) {
+			if (slug.indexOf("_columns_layout") > -1) {
 				return {
 					setting: {
-						value: 'left-third',
+						value: "left-third",
 						get: () => window.wp.customize.control(slug).setting.value,
-						set: (value) => {
-						},
-						bind: (value) => window.wp.customize.control(slug).setting.value
-					}
-				}
+						set: (value) => {},
+						bind: (value) => window.wp.customize.control(slug).setting.value,
+					},
+				};
 			}
-			if (slug.indexOf('_columns_number') > -1) {
+			if (slug.indexOf("_columns_number") > -1) {
 				return {
 					setting: {
 						value: 2,
 						get: () => window.wp.customize.control(slug).setting.value,
-						set: (value) => {
-						},
-						bind: (value) => window.wp.customize.control(slug).setting.value
-					}
-				}
+						set: (value) => {},
+						bind: (value) => window.wp.customize.control(slug).setting.value,
+					},
+				};
 			}
-		}
-	}
+		},
+	},
 };
 
 window.wp = wpObject;
 
-const head = document.querySelector('#nv-google-fonts');
-const families = FONTS.Google.join('|');
-const link = document.createElement('link');
-link.setAttribute('rel', 'stylesheet');
-link.setAttribute('id', 'google-fonts-css');
-link.setAttribute('href', `https://fonts.googleapis.com/css?family=${families}&text=Abc&display=swap&ver=2.9.5`);
-link.setAttribute('media', 'all');
-head.appendChild(link)
-
+const head = document.querySelector("#nv-google-fonts");
+const families = FONTS.Google.join("|");
+const link = document.createElement("link");
+link.setAttribute("rel", "stylesheet");
+link.setAttribute("id", "google-fonts-css");
+link.setAttribute(
+	"href",
+	`https://fonts.googleapis.com/css?family=${families}&text=Abc&display=swap&ver=2.9.5`
+);
+link.setAttribute("media", "all");
+head.appendChild(link);
