e3d226c1f0bad3bd7a9949dda9f97aca9f659470
Soare Robert Daniel <soare.robert.daniel@protonmail.com>
2025-01-29 18:44:40 +0200

    fix: use 2.9 for required in compatibility with NevePro (#4369)

--- functions.php
+++ functions.php
@@ -112,7 +112,7 @@ add_filter(
 
 		$compatibilities['NevePro'] = [
 			'basefile'  => defined( 'NEVE_PRO_BASEFILE' ) ? NEVE_PRO_BASEFILE : '',
-			'required'  => '3.0',
+			'required'  => '2.9',
 			'tested_up' => '3.0',
 		];
 
