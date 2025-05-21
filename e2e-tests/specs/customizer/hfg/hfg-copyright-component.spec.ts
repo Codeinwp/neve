c82dcac754e4487f0cf5a74ff5661c6a6d22b751
abaicus <baicusandrei@me.com>
2025-05-21 18:32:05 +0300

    fix: copyright e2e test

--- e2e-tests/specs/customizer/hfg/hfg-copyright-component.spec.ts
+++ e2e-tests/specs/customizer/hfg/hfg-copyright-component.spec.ts
@@ -2,30 +2,29 @@ import { test, expect } from '@playwright/test';
 
 test('Checks the copyright in front-end', async ({ page }) => {
 	await page.goto('/');
+
+	const checkCopyrightVisible = async () => {
+		await expect(
+			page.locator('.hfg_footer .footer--row[data-show-on="desktop"]')
+		).toHaveText('Neve | Powered by WordPress');
+		await expect(
+			page
+				.locator('.hfg_footer .footer--row[data-show-on="desktop"] a')
+				.nth(0)
+		).toHaveAttribute('href', 'https://themeisle.com/themes/neve/');
+		await expect(
+			page
+				.locator('.hfg_footer .footer--row[data-show-on="desktop"] a')
+				.nth(1)
+		).toHaveAttribute('href', 'https://wordpress.org');
+	};
+
 	// Check that the text is present for the Desktop footer
-	await expect(
-		page.locator('.hfg_footer .footer--row[data-show-on="desktop"]')
-	).toHaveText('Neve | Powered by WordPress');
-	await expect(
-		page
-			.locator('.hfg_footer .footer--row[data-show-on="desktop"] a')
-			.nth(0)
-	).toHaveAttribute('href', 'https://themeisle.com/themes/neve/');
-	await expect(
-		page
-			.locator('.hfg_footer .footer--row[data-show-on="desktop"] a')
-			.nth(1)
-	).toHaveAttribute('href', 'https://wordpress.org');
+	await checkCopyrightVisible();
 
-	// Check that the text is also present for the Mobile footer
-	await expect(
-		page.locator('.hfg_footer .footer--row[data-show-on="mobile"]')
-	).toHaveText('Neve | Powered by WordPress');
+	// set viewport to mobile
+	await page.setViewportSize({ width: 400, height: 800 });
 
-	await expect(
-		page.locator('.hfg_footer .footer--row[data-show-on="mobile"] a').nth(0)
-	).toHaveAttribute('href', 'https://themeisle.com/themes/neve/');
-	await expect(
-		page.locator('.hfg_footer .footer--row[data-show-on="mobile"] a').nth(1)
-	).toHaveAttribute('href', 'https://wordpress.org');
+	// Check that the text is present for the Mobile footer
+	await checkCopyrightVisible();
 });
