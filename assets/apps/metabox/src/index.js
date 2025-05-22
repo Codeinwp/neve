b8d80a9ec26b3083de72de5929dc36d45aa7d8e1
abaicus <baicusandrei@me.com>
2025-05-22 17:56:43 +0300

    fix: clicking the more menu in the editor throws an error [closes #4420]

--- assets/apps/metabox/src/index.js
+++ assets/apps/metabox/src/index.js
@@ -11,11 +11,10 @@ import Sidebar from './components/Sidebar';
 import { neveIcon } from './helpers/icons.js';
 
 registerPlugin('meta-sidebar', {
-	icon: () =>
-		metaSidebar.whiteLabeled ? (
-			<span dangerouslySetInnerHTML={{ __html: metaSidebar.icon }} />
-		) : (
-			<Icon icon={neveIcon} />
-		),
+	icon: metaSidebar.whiteLabeled ? (
+		<span dangerouslySetInnerHTML={{ __html: metaSidebar.icon }} />
+	) : (
+		<Icon icon={neveIcon} />
+	),
 	render: Sidebar,
 });
