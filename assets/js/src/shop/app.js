3a949ccd73036b6255cac51d545a552bcb16b05d
girishpanchal30 <girish@krishaweb.com>
2025-10-30 19:22:42 +0530

    feat: add slider on gallery images

--- assets/js/src/shop/app.js
+++ assets/js/src/shop/app.js
@@ -20,6 +20,53 @@ function initShop() {
 	) {
 		handleExclusiveSlider();
 	}
+
+	if (
+		document.body.classList.contains('single-product') &&
+		!document.body.classList.contains('sp-slider-gallery')
+	) {
+		handleGallerySlider();
+	}
+}
+
+const svgs = {
+	vertical: {
+		prev: '<svg width="30px" height="25px" viewBox="0 0 50 80" xml:space="preserve" style="transform: rotate(90deg)"><polyline fill="none" stroke="currentColor" stroke-width="7" points="25,76 10,38 25,0"/></svg>',
+		next: '<svg width="30px" height="25px" viewBox="0 0 50 80" xml:space="preserve" style="transform: rotate(90deg)"><polyline fill="none" stroke="currentColor" stroke-width="7" points="25,0 40,38 25,75"/></svg>',
+	},
+	horizontal: {
+		prev: '<svg width="25px" height="30px" viewBox="0 0 50 80" xml:space="preserve"><polyline fill="none" stroke="currentColor" stroke-width="7" points="25,76 10,38 25,0"/></svg>',
+		next: '<svg width="25px" height="30px" viewBox="0 0 50 80" xml:space="preserve"><polyline fill="none" stroke="currentColor" stroke-width="7" points="25,0 40,38 25,75"/></svg>',
+	},
+};
+
+/**
+ * Add prev and next
+ *
+ * @param {Node} targetNode
+ * @param {Node} slider
+ * @param {string} vertical
+ */
+function addNextPrev(targetNode, slider, vertical = false) {
+	const next = document.createElement('span');
+	const prev = document.createElement('span');
+
+	next.classList.add('neve-slider-control', 'next');
+	prev.classList.add('neve-slider-control', 'prev');
+
+	prev.innerHTML = vertical ? svgs.vertical.prev : svgs.horizontal.prev;
+	next.innerHTML = vertical ? svgs.vertical.next : svgs.horizontal.next;
+
+	prev.addEventListener('click', function () {
+		slider.goTo('prev');
+	});
+
+	next.addEventListener('click', function () {
+		slider.goTo('next');
+	});
+
+	targetNode.parentNode.insertBefore(prev, targetNode);
+	targetNode.parentNode.appendChild(next);
 }
 
 /**
@@ -87,6 +134,36 @@ function handleExclusiveSlider() {
 			);
 		});
 	}
+}
+
+/**
+ * Handle Gallery Image Slider
+ */
+function handleGallerySlider() {
+	const galleryNav = document.querySelector('ol.flex-control-nav');
+
+	if (!galleryNav) return;
+
+	const isDesktop = window.innerWidth >= 992;
+
+	const slider = tns({
+		container: 'ol.flex-control-nav',
+		items: isDesktop ? 4 : 4,
+		axis: isDesktop ? 'vertical' : 'horizontal',
+		slideBy: 'page',
+		rewind: true,
+		loop: false,
+		nav: false,
+		controls: false,
+		mouseDrag: true,
+		speed: 400,
+	});
+
+	addNextPrev(
+		document.querySelector('.woocommerce-product-gallery .tns-inner'),
+		slider,
+		isDesktop
+	);
 }
 
 /**
